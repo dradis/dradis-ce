cd31b69763b676381b8287824466037094ae930a
Daniel Martin <etd-github@nomejortu.com>
2016-01-22 10:31:54 +0100

    Configurator class to store app settings in the DB

---
+++ app/controllers/configurations_controller.rb
@@ -0,0 +1,109 @@
+# Internal application Configuration settings are handled through this
+# REST-enabled controller.
+class ConfigurationsController < ProjectScopedController
+  before_filter :find_or_initialize_config, except: [ :index ]
+
+  # Get all the Configuration objects.
+  def index
+    @configs = all_configurations
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
+  # Update the value of a Configuration object or gemified plugin setting.
+  def update
+    respond_to do |format|
+      format.html { head :method_not_allowed }
+
+      if @config && @config.update_attributes(params[:config])
+        format.js { render json: @config.to_json }
+      elsif @plugin
+        @plugin.settings.update_settings(params[:setting])
+        @is_default = @plugin.settings.is_default?(params[:setting].keys.first, params[:setting].values.first)
+        format.js { render json: { setting_is_default: @is_default }.to_json }
+      else
+        format.js { render json: @config.errors.to_json, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DEPRECATED
+  # TODO: Remove when all plugins have been gemified.
+  # Create a new Configuration object and store it in the database.
+  def create
+    respond_to do |format|
+      format.html { head :method_not_allowed }
+      if @config.save
+        headers['Location'] = configuration_url(@config)
+        format.js { render json: @config.to_json, status: :created }
+      else
+        format.js { render json: @config.errors.to_json, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DEPRECATED
+  # TODO: Remove when all plugins have been gemified.
+  # Retrieve a Configuration object. Only supports XML format.
+  def show
+    respond_to do |format|
+      format.html { head :method_not_allowed }
+    end
+  end
+
+  private
+  def all_configurations
+    vendorized = Core::Configurator.configurables.map do |configurable|
+      {
+        klass:    configurable.name.to_s,
+        name:     configurable.name.gsub(/::Configuration$/, ''),
+        type:     'vendorized',
+        settings: configurable.settings.sort_by(&:name)
+      }
+    end
+    gemified = Dradis::Plugins.list.map do |plugin|
+      {
+        klass:    plugin.name.to_s,
+        name:     plugin.name.gsub(/^Dradis::Plugins::/, '').gsub(/::Engine$/, ''),
+        type:     'gemified',
+        settings: plugin.settings.all
+      }
+    end
+    configurations = vendorized.zip(gemified).flatten
+    configurations = configurations.reject{ |c| c[:settings].empty? }
+    configurations = configurations.sort_by{ |c| c[:name] }
+  end
+
+  # This filter locates the Dradis::Plugins subclass
+  # for which we're updating the settings using the :id param.
+  # If no plugin is found, an error page is rendered.
+  def find_plugin
+    if params[:id]
+      class_name = "Dradis::Plugins::#{ params[:id].camelcase }::Engine"
+      @plugin = class_name.constantize if all_configurations.map{ |c| c[:klass] }.include?(class_name)
+    end
+    if @plugin.nil?
+      raise ActionController::RoutingError.new('Not Found')
+    else
+      return true
+    end
+  end
+
+  # DEPRECATED
+  # TODO: Remove when all plugins have been gemified.
+  # This filter locates a Configuration object based on the :id passed as a
+  # parameter in the request. If the :id is invalid, it delegates to find_plugin
+  def find_or_initialize_config
+    if params[:id]
+      if @config = params[:id].to_s =~ /\A[0-9]+\z/ ? ::Configuration.find(params[:id]) : ::Configuration.find_by_name(params[:id])
+        return true
+      else
+        find_plugin
+      end
+    else
+      @config = ::Configuration.new(params[:config])
+    end
+  end
+
+end
---
+++ lib/tasks/thorfile.rb
@@ -0,0 +1,284 @@
+module Core
+  module Pro
+    module ProjectScopedTask
+      def detect_and_set_project_scope
+        if ENV.key?('PROJECT_ID')
+
+          project_id = ENV['PROJECT_ID'].to_i
+          if Project.where(id: project_id).first
+            shell.say("Project found. Scoping all models to Project #{project_id}.")
+            Evidence.set_project_scope(project_id)
+            Issue.set_project_scope(project_id)
+            Node.set_project_scope(project_id)
+            Note.set_project_scope(project_id)
+            Tag.set_project_scope(project_id)
+          else
+            shell.error("*"*80)
+            shell.error("Couldn't find Project with id=#{project_id}")
+            shell.error("*"*80)
+            exit(2)
+          end
+        else
+          shell.error("*"*80)
+          shell.error('This task requires you to pass a project ID as a parameter. Like this:')
+          shell.error("\tPROJECT_ID=1234 RAILS_ENV=production bundle exec thor #{ARGV.join(' ')}")
+          shell.error("*"*80)
+          exit(1)
+        end
+      end
+    end
+  end
+end
+
+class DradisTasks < Thor
+  namespace       "dradis"
+  
+  desc      "backup", "creates a backup of your current repository"
+  long_desc "Creates a backup of the current repository, including all nodes, notes and " +
+            "attachments as a zipped archive. The backup can be imported into another " +
+            "dradis instance using the 'Project Package Upload' option."
+  method_option   :file, :type => :string, :desc => "the backup file to create, or directory to create it in"
+  def backup
+    require 'config/environment'
+
+    backup_path   = options.file || Rails.root.join('backup')
+
+    unless backup_path.to_s =~ /\.zip\z/
+      date        = DateTime.now.strftime("%Y-%m-%d")
+      sequence    = Dir.glob(File.join(backup_path, "dradis_#{date}_*.zip")).collect { |a| a.match(/_([0-9]+)\.zip\z/)[1].to_i }.max || 0
+      backup_path = File.join(backup_path, "dradis_#{date}_#{sequence + 1}.zip")
+    end
+
+    if ActiveRecord::Migrator::current_version > 0
+      print "** Saving backup...                                                   "
+
+      begin
+        FileUtils.mkdir_p File.dirname(backup_path)
+        
+        ProjectExport::Processor.full_project( :filename => backup_path )
+
+        puts "[  DONE  ]"
+        puts "** Backup Saved as: #{backup_path}"
+      rescue RuntimeError => e
+        puts "[ FAILED ]"
+        puts "** #{e.message}"
+        exit(-1)
+      end
+    else
+      puts "** Nothing to Backup."
+    end
+  end
+
+  desc      "reset", "resets your local dradis repository"
+  long_desc "Resets your dradis repository, removing all nodes, notes and attachments and log files " +
+            "so it is ready to start a new project.\n\nA backup of the current repository "+
+            "will be taken before anything is removed."
+  method_option   :file, :type => :string, :desc => "the backup file to create, or directory to create it in"
+  method_option   :no_backup, :type => :boolean, :desc => "do not create a backup of the current repository"
+  def reset
+    invoke "dradis:setup:configure"
+    invoke "dradis:setup:migrate"
+
+    invoke "dradis:backup", options             unless options.no_backup
+    
+    invoke "dradis:reset:attachments", options
+    invoke "dradis:reset:database", options
+    invoke "dradis:setup:seed"
+    invoke "dradis:reset:logs", options
+  end
+
+  desc "server", "start dradis server"
+  method_option   :p, :type => :string, :desc => "specify the port to listen to (default 3004)"
+  method_option   :b, :type => :string, :desc => "bind to a specific IP address (default 0.0.0.0)"
+  method_option   :d, :type => :boolean, :desc => "run in the background"
+  method_option   :P, :type => :string, :desc => "specify the location of the PID file (default tmp/pids)"
+  def server
+    require 'rubygems'
+
+    ARGV.shift        # remove dradis:server from the command-line arguments
+    ARGV.unshift 's'  # add 's' to the beginning of the command-line arguments,
+                      # because we want a server
+
+    gem 'rails', ">= 0"
+    # now that we've massaged the arguments a little, we let Rails take over and
+    # do its magic (this is essentially invoking 'rails s' with all the options
+    # passed into Thor
+    load Gem.bin_path('rails', 'rails', ">= 0")
+  end
+
+  desc "settings [NAMESPACE]", "list dradis settings, with an optional namespace to filter the results"
+  def settings(namespace=nil)
+    require 'config/environment'
+
+    settings = Core::Configurator.configurables.collect(&:settings).flatten.sort_by(&:name)
+    width = settings.collect { |s| s.name.length + 1 }.max
+
+    settings.each do |setting|
+      puts "%-#{width}s %s" % [setting.name, setting.value] if namespace.nil? || setting.name.include?(namespace)
+    end
+  end
+
+  desc "version", "displays the version of the dradis server"
+  def version
+    require 'lib/core/version'
+    puts Core::VERSION::string
+    puts Core::Pro::VERSION::string
+  end
+  
+
+  class Import < Thor; end
+  class Export < Thor; end
+  class Upload < Thor; end
+
+
+  class Setup < Thor
+    namespace     "dradis:setup"
+
+    desc "configure", "Creates the Dradis configuration files from their templates (see config/*.yml.template)"
+    def configure
+      # init the config files
+      init_all = false
+      Dir['config/*.template'].each do |template|
+        config = File.join( 'config', File.basename(template, '.template') )
+        if !(File.exists?( config ))
+          if (init_all)
+            puts "Initilizing #{config}..."
+            FileUtils.cp(template, config)
+          else
+            puts "The config file [#{template}] was found not to be ready to use."
+            puts "Do you want to initialize it? [y]es | [N]o | initialize [a]ll"
+            response = STDIN.gets.chomp.downcase
+            response = 'Y' if ( response.empty? || !['y', 'n', 'a'].include?(response) )
+  
+            if response == 'n'
+              next
+            else
+              puts "Initilizing #{config}..."
+              FileUtils.cp(template, config)
+              if (response == 'a')
+                init_all = true
+              end
+            end
+          end
+        end
+      end
+    end
+
+    desc "migrate", "ensures the database schema is up-to-date"
+    def migrate
+      require 'config/environment'
+
+      print "** Checking database migrations...                                    "
+      ActiveRecord::Migrator.migrate("db/migrate/", nil)
+      puts "[  DONE  ]"
+    end
+    
+    desc "seed", "adds initial values to the database (i.e., categories and configurations)"
+    def seed
+      require 'config/environment'
+
+      print "** Seeding database...                                                "
+      require 'db/seeds'
+      puts "[  DONE  ]"
+    end
+    
+  end
+
+  class Logs < Thor
+    namespace     "dradis:logs"
+
+    desc "clean DAYS", "delete all logs older than DAYS days (default 7)"
+    def clean(days=7)
+      puts "Clearing old Logs..."
+      logs  = Log.where("created_at < (?)", days.to_i.days.ago)
+      count = logs.count
+      logs.destroy_all
+      puts "Deleted #{count} Log#{"s" if count != 1}"
+    end
+  end
+
+  class Settings < Thor
+    namespace     "dradis:settings"
+
+    desc "get SETTING", "get the value of a dradis setting"
+    def get(name)
+      require 'config/environment'
+
+      setting = Core::Configurator.configurables.collect(&:settings).flatten.detect { |c| c.name == name }
+
+      unless setting.nil?
+        puts "%s %s" % [setting.name, setting.value]
+      else
+        puts "Unknown setting %s." % [name]
+      end
+    end
+
+    desc "set SETTING VALUE", "change the value of a dradis setting"
+    def set(name, value)
+      require 'config/environment'
+
+      setting = Core::Configurator.configurables.collect(&:settings).flatten.detect { |c| c.name == name }
+
+      unless setting.nil?
+        old_value = setting.value
+
+        if setting.update_attribute(:value, value)
+          puts "Changed %s from \"%s\" to \"%s\"." % [setting.name, old_value, setting.value]
+        else
+          puts "Failed to change %s to \"%s\"." % [setting.name, value]
+        end
+      else
+        puts "Unknown setting %s." % [name]
+      end
+    end
+  end
+  
+  class Reset < Thor
+    namespace     "dradis:reset"
+
+    desc "attachments", "removes all attachments"
+    def attachments
+      print "** Deleting all attachments...                                        "
+      FileUtils.rm_rf(Dir.glob( Attachment::AttachmentPwd.join('*')) )
+      puts(Dir.glob( Attachment::AttachmentPwd.join('*')).empty? ? "[  DONE  ]" : "[ FAILED ]")
+    end
+
+    desc "database", "removes all data from a dradis repository, except configurations"
+    def database
+      require 'config/environment'
+      
+      print "** Cleaning database...                                               "
+
+      Note.destroy_all
+      Node.destroy_all
+      Category.destroy_all
+      
+      Log.destroy_all
+      
+      puts "[  DONE  ]"
+    end
+
+    desc "logs", "removes all log files"
+    def logs
+      print "** Deleting all log files...                                          "
+      FileUtils.rm_rf(Dir.glob('log/*.log'))
+      puts(Dir.glob('log/*.log').empty? ? "[  DONE  ]" : "[ FAILED ]")
+    end
+
+    desc "password", "Set a new shared password to access the web interface"
+    def password()
+      require 'config/environment'
+
+      say "Changing password for Dradis server."
+      password = ask "Enter new Dradis password:"
+      confirmation = ask "Retype new Dradis password:"
+
+      if !password.blank? && password == confirmation
+        Configuration.find_or_create_by(name: 'password').update_attribute(:value, ::Digest::SHA512.hexdigest(password))
+        say("Password Changed.", Thor::Shell::Color::GREEN)
+      else
+        say("Passwords do not match. Password Unchanged.", Thor::Shell::Color::RED)
+      end
+    end
+  end
+end
