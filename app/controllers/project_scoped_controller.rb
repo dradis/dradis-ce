8c6aa93f1ea9f07fb2fff4f4467bf88ffb1681aa
Daniel Martin <etd-github@nomejortu.com>
2016-02-01 14:13:16 +0100

    ProjectScoped#initialize_nodes_sidebar - sort Issues by title

--- app/controllers/project_scoped_controller.rb
+++ app/controllers/project_scoped_controller.rb
@@ -13,7 +13,7 @@ class ProjectScopedController < AuthenticatedController
   #
   # Used in node, evidence, note, and revision controllers.
   def initialize_nodes_sidebar
-    @issues          = Issue.where(node_id: Node.issue_library.id)
+    @issues          = Issue.where(node_id: Node.issue_library.id).sort
     # If you just tried to save an invalid Note, then that Note will be
     # included in @node.notes and will crash the sidebar (because the sidebar
     # won't be able to generate a route for a Note with no id) So filter out
