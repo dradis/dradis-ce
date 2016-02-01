3d451f013d616682354588f230f0c01a7570ff44
Daniel Martin <etd-github@nomejortu.com>
2016-02-01 15:34:17 +0100

    Add Configuration model

---
+++ app/models/configuration.rb
@@ -0,0 +1,56 @@
+# This class is used to store configuration options in the back-end database.
+# Each Configuration object has a :name and a :value. Some configuration
+# parameters can be accessed through the helper methods provided in this class.
+class Configuration < ActiveRecord::Base
+  validates_presence_of :name, :value
+  validates_uniqueness_of :name
+
+  def self.exists?(*attrs)
+    table_exists? && super(*attrs)
+  end
+
+  # --------------------------------------------------------------- Misc admin:
+
+  def self.emails_node
+    find_by(name: 'admin:emails_node').value
+  end
+
+  def self.signups_enabled?
+    find_or_create_by(name: 'admin:signups_enabled', value: 0).value.to_i == 1
+  end
+
+
+  # --------------------------------------------------------------- admin:paths
+  def self.paths_templates_plugins
+    find_or_create_by(
+      name:  'admin:paths:templates:plugins',
+      value: Rails.root.join('templates', 'plugins').to_s
+    ).value
+  end
+
+  def self.paths_templates_reports
+    find_or_create_by(
+      name:  'admin:paths:templates:reports',
+      value: Rails.root.join('templates', 'reports').to_s
+    ).value
+  end
+
+
+  # ------------------------------------------------------------- admin:plugins
+
+  # This setting is used by the plugins as the root of all the content the add.
+  def self.plugin_parent_node
+    find_or_create_by(
+      name:  'admin:plugins:parent_node',
+      value: 'plugin.output'
+    ).value
+  end
+
+  # Retrieve the name of the Node used to associate file uploads.
+  def self.plugin_uploads_node
+    find_or_create_by(
+      name:  'admin:plugins:uploads_node',
+      value: 'Uploaded files'
+    ).value
+  end
+end
---
+++ spec/models/configuration_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Configuration do
+
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :value }
+  it { create(:configuration); should validate_uniqueness_of :name }
+
+end
