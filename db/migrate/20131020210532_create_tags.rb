4cd8117b75f926b7eb684647fcac7f17729a7a72
Daniel Martin <etd-github@nomejortu.com>
2016-02-01 16:18:06 +0100

    DB migrations remove unused attributes

--- db/migrate/20131020210532_create_tags.rb
+++ db/migrate/20131020210532_create_tags.rb
@@ -1,7 +1,6 @@
 class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
-      t.integer :project_id, null: false
       t.string :name, index: true, null: false
       t.integer :taggings_count, default: 0, index: true, null: false
 
--- db/migrate/20151014171837_create_activities.rb
+++ db/migrate/20151014171837_create_activities.rb
@@ -1,7 +1,6 @@
 class CreateActivities < ActiveRecord::Migration
   def change
     create_table :activities do |t|
-      t.integer :project_id,     null: false
       t.integer :user_id,        null: false
       t.string  :trackable_type, null: false
       t.integer :trackable_id,   null: false
@@ -9,7 +8,6 @@ class CreateActivities < ActiveRecord::Migration
 
       t.timestamps
     end
-    add_index :activities, :project_id
     add_index :activities, :user_id
     add_index :activities, [:trackable_id, :trackable_type]
     add_index :activities, :created_at
