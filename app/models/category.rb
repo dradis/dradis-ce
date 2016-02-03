3a40dff1be26173db628e20f2807537ffd8649ff
Daniel Martin <etd-github@nomejortu.com>
2016-02-03 17:42:34 +0100

    Add Category model

---
+++ app/models/category.rb
@@ -0,0 +1,42 @@
+# This class represents a Note category. Each Category has a name.
+class Category < ActiveRecord::Base
+
+  # -- Relationships --------------------------------------------------------
+
+
+  # -- Callbacks ------------------------------------------------------------
+  before_destroy :valid_destroy
+
+
+  # -- Validations ----------------------------------------------------------
+
+
+  # -- Scopes ---------------------------------------------------------------
+
+
+  # -- Class Methods --------------------------------------------------------
+  def self.default
+    Category.find_or_create_by(name: 'Default category')
+  end
+
+  def self.issue
+    Category.find_or_create_by(name: 'Issue description')
+  end
+
+  def self.report
+    Category.find_or_create_by(name: 'Report category')
+  end
+
+  # -- Instance Methods -----------------------------------------------------
+
+  private
+  def valid_destroy
+    if (self.id == 1)
+      self.errors.add :base, 'Cannot delete Default category.'
+    end
+    if Note.count(conditions: { category_id: self.id }) > 0
+      self.errors.add :base, 'Cannot delete Category with notes.'
+    end
+    return errors.count.zero? ? true : false
+  end
+end
