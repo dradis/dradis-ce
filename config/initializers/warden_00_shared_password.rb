dcad340e9814117dcd7b63f6e78c3adae11ba7fb
Daniel Martin <etd-github@nomejortu.com>
2016-02-04 16:03:24 +0100

    Warden - enable :shared_password strategy by default

--- config/initializers/warden_00_shared_password.rb
+++ config/initializers/warden_00_shared_password.rb
@@ -4,7 +4,7 @@
 # http://railscasts.com/episodes/305-authentication-with-warden
 
 Rails.configuration.middleware.use Warden::Manager do |manager|
-  manager.default_strategies :db
+  manager.default_strategies :shared_password
   manager.failure_app = SessionsController.action(:new)
 end
 
@@ -17,7 +17,7 @@ Warden::Strategies.add(:shared_password) do
     username = params.fetch('username', nil)
     password = params.fetch('password', nil)
 
-    if not ( username.blank? || password.nil? || ::BCrypt::Password.new(::Configuration.password) != password )
+    if not ( username.blank? || password.nil? || ::BCrypt::Password.new(::Configuration.shared_password) != password )
       success!(username)
     else
       fail 'Invalid credentials.'
