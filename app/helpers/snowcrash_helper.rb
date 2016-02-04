299153606df38f847fe2ab8c6f5c749d53897614
Daniel Martin <etd-github@nomejortu.com>
2016-02-04 15:50:11 +0100

    SnowcrashHelper - #present helper used by Activity Stream

--- app/helpers/snowcrash_helper.rb
+++ app/helpers/snowcrash_helper.rb
@@ -34,4 +34,11 @@ module SnowcrashHelper
       end
     end.join("\n").html_safe
   end
+
+  def present(object, klass = nil)
+    klass ||= "#{object.class}Presenter".constantize
+    presenter = klass.new(object, self)
+    yield presenter if block_given?
+    presenter
+  end
 end
