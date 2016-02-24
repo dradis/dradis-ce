45691561de6d6d87fe49b91258c4cc3bc9b4e6e1
Daniel Martin <etd-github@nomejortu.com>
2016-02-24 10:14:49 +0100

    Projects#show - load all models to build the project summary

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,5 +3,29 @@ class ProjectsController < AuthenticatedController
   layout 'snowcrash'
 
   def show
+    @nodes   = Node.in_tree
+    @issues  = Issue.where(node_id: Node.issue_library.id).includes(:tags).sort
+    @authors = [current_user]
+
+    @methodologies = Node.methodology_library.notes.map{|n| Methodology.new(filename: n.id, content: n.text)}
+
+    @tags = Tag.all
+    @issues_by_tag = Hash.new{|h,k| h[k] = [] }
+    assigned = nil
+
+    # FIXME: Activity Feed
+    # @activities = Activity.latest
+
+    @issues.each do |issue|
+      assigned = false
+      @tags.each do |tag|
+        if issue.tags.include?(tag)
+          @issues_by_tag[tag.name] << issue
+          assigned = true
+        end
+      end
+      @issues_by_tag[:unassigned] << issue unless assigned
+    end
+
   end
 end
