cf6d15c216a6d71e825e1146599034695b41e98f
Daniel Martin <etd-github@nomejortu.com>
2016-03-03 16:25:02 +0100

    Log model for Export / upload logs
    Should these be replaced with a bunch of Resque entries?

---
+++ app/models/log.rb
@@ -0,0 +1,16 @@
+class Log < ActiveRecord::Base
+
+  def write(trace=nil, &block)
+    text = trace.nil? ? yield : trace
+    Log.create!(attributes.merge({:text => text}))
+  end
+
+  alias :info :write
+  alias :debug :write
+  alias :error :write
+  alias :fatal :write
+
+  def read
+    "[#{created_at.strftime('%H:%M:%S')}]  #{text}"
+  end
+end
