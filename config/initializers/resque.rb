a7710aac294fb7081817a4604d89fb9f47627c09
Daniel Martin <etd-github@nomejortu.com>
2016-03-03 16:24:20 +0100

    Add some default initializers

---
+++ config/initializers/resque.rb
@@ -0,0 +1,10 @@
+Resque::Plugins::Status::Hash.expire_in = (24 * 60 * 60) # 24hrs in seconds
+
+logfile = File.open(File.join(Rails.root, 'log', 'resque.log'), 'a')
+logfile.sync = true
+Resque.logger = Logger.new(logfile)
+Resque.logger.level = Logger::INFO
+Resque.logger.info "Resque Logger Initialized"
+# Resque::Server.use(Rack::Auth::Basic) do |user, password|
+#   user == 'admin' && password == 'dradispro'
+# end
