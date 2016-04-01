273f4a5df441dc25f2c2f4da70a0b6e4f418023d
dixpac <dino.onex@gmail.com>
2016-04-01 16:27:44 +0200

    Snipets in large texts

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -8,4 +8,19 @@ module SearchHelper
   	options = exist_opts.merge(options)
     search_path(options)
 	end
+
+  def format_match_row(text, term)
+    return text if text.length <= 300
+    max_length = 300
+    pos_start = text.index(term)
+    pos_end = text.index(term) + term.length
+    sanitize_snipet(pos_start, pos_end, max_length, text)
+  end
+
+  def sanitize_snipet(pos_start, pos_end, max_length, text)
+    return text[pos_start...max_length] if pos_start == 0
+    return text[(pos_end - max_length)..-1] if pos_end == text.length
+    return text[pos_end - 300...pos_end] if pos_start > max_length
+    return text[0..pos_end + (max_length - pos_end - 1)]
+  end
 end
