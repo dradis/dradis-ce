e8a30cf635ad4120a60610c5a9deb53f13cccd3a
Daniel Martin <etd-github@nomejortu.com>
2016-04-06 15:40:00 +0200

    API - fix exceptions_spec specs (still failing)

--- engines/dradis-api/spec/requests/dradis/ce/api/exceptions_spec.rb
+++ engines/dradis-api/spec/requests/dradis/ce/api/exceptions_spec.rb
@@ -5,46 +5,50 @@ describe "API" do
 
   describe "Exceptions" do
 
-    before do
+    before(:each) do
       allow(Rails.application.config.action_dispatch).to receive(:show_exceptions).and_return true
-      allow(Rails.application.config.action_dispatch).to receive(:consider_all_requests_local).and_return false
+      allow(Rails.application.config).to receive(:consider_all_requests_local).and_return false
       allow(Configuration).to receive(:shared_password).and_return(::BCrypt::Password.create('rspec_pass'))
     end
 
     describe "500 - Internal Server Error" do
 
       it "renders as JSON if it happens inside the API" do
-        params = { project: { bad_attribute: "Bad attribute" } }
-        expect(Dradis::Pro::API::ExceptionsController).to receive(:call)
-        post "/api/projects", params.to_json, api_env
+        expect(Node).to receive(:issue_library) do raise; end
+
+        get "/api/issues", {}, api_env
         expect(response.status).to eq(500)
-        # puts response.body
-        # puts "-----------------------------------------"
+        expect(response.headers['Content-Type']).to eq('application/json; charset=utf-8')
+
+        expect do
+          body = JSON.parse(response.body)
+        end.not_to raise_error
       end
 
       it "renders as html if it happens outside the API" do
-        post session_path, login: @user.email, password: @user.password
-        allow_any_instance_of(ProjectsController).to receive(:index).and_raise(ArgumentError)
-        get "/projects"
+        expect(SessionsController).to receive(:new) do raise; end
+        get "/login"
         expect(response.status).to eq(500)
-        # puts response.body
-        # puts "-----------------------------------------"
+        expect(response.headers['Content-Type']).not_to eq('application/json; charset=utf-8')
       end
     end
 
     describe "404 - Not Found" do
+
       it "renders as JSON if it happens inside the API" do
-        expect(Dradis::CE::API::ExceptionsController).to receive(:call)
         get "/api/issues/12345", {}, api_env
         expect(response.status).to eq(404)
-        # puts response.body
-        # puts "-----------------------------------------"
+        body = nil
+        expect do
+          body = JSON.parse(response.body)
+        end.not_to raise_error
+        expect(body[:message]).to eq(Rack::Utils::HTTP_STATUS_CODES[:not_found])
       end
 
       it "renders as html if it happens outside the API" do
         get "/bad_uri"
         expect(response.status).to eq(404)
-        # puts response.body
+        expect(response.headers['Content-Type']).not_to eq('application/json; charset=utf-8')
       end
     end
 
