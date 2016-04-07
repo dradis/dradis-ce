2f6472e28f8989f7026613c2eea603761c170887
Daniel Martin <etd-github@nomejortu.com>
2016-04-07 10:08:20 +0200

    Create CI Gemfile with GH repo clones

---
+++ bin/create_codeship_gemfile.rb
@@ -0,0 +1,14 @@
+# Auto-generate the Gemfile to be used on Codeship. Replace relative paths
+# using "path:" with instructions to pull the gems from Github.
+#
+# This script is run on Codeship before it runs `bundle install`. See See
+# https://codeship.com/projects/12691/configure_tests
+
+gemfile = File.read("./Gemfile")
+
+cs_gemfile = gemfile.gsub(/path:.*\.\.\/community\/.*['"]/) do |match|
+  gem_name = /community\/(.*)['"]/.match(match)[1]
+  "github: \"dradis/#{gem_name}\""
+end
+
+File.write("./Gemfile.codeship", cs_gemfile)
