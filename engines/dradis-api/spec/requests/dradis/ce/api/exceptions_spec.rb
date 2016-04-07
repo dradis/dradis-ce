f587004d0713a9ee4bf8310cdfd38b7e2c5ef6df
George Millo <georgejulianmillo@gmail.com>
2016-04-07 18:05:06 +0200

    fix non-deterministic failure in API exceptions_spec
    full explanation given in the code comment that this
    commit adds

--- engines/dradis-api/spec/requests/dradis/ce/api/exceptions_spec.rb
+++ engines/dradis-api/spec/requests/dradis/ce/api/exceptions_spec.rb
@@ -1,14 +1,45 @@
 require 'spec_helper'
 
 describe "API" do
-
-
   describe "Exceptions" do
 
-    before(:each) do
-      allow(Rails.application.config.action_dispatch).to receive(:show_exceptions).and_return true
-      allow(Rails.application.config).to receive(:consider_all_requests_local).and_return false
-      allow(Configuration).to receive(:shared_password).and_return(::BCrypt::Password.create('rspec_pass'))
+    before do
+      allow(Configuration).to receive(:shared_password).and_return(
+        ::BCrypt::Password.create('rspec_pass')
+      )
+
+      # To get the 'renders as HTML' test to work, we need to make show
+      # that the 'show_exceptions' config setting is `true`, despite it
+      # normally being set to `false` in the test ENV. The problem is that
+      # Rails.application memoizes this config value the first time the
+      # app receives a request (see
+      # https://github.com/rails/rails/blob/v4.2.6/railties/lib/rails/application.rb#L253
+      # ), and this memoization will persist between specs - which means that
+      # if *any* other spec makes a request to the app before the specs in
+      # this file get run, then stubbing 'show_exceptions' won't do anything
+      # (because it won't overwrite the memoized value) and the spec will
+      # fail.
+      #
+      # To get around this we're manually deleteing the memoized instance
+      # variable within Rails.application using remove_instance_variable. This
+      # is very hacky and may break without warning in future versions of Rails
+      # (at the time of writing we're using 4.2.6).
+
+      @show_exceptions_prev_val = Rails.application.config.action_dispatch.show_exceptions
+      # @app_env_config might not necessarily be defined, and
+      # remove_instance_variable will raise an error if it's not:
+      if Rails.application.instance_variable_get '@app_env_config'
+        Rails.application.remove_instance_variable '@app_env_config'
+      end
+      Rails.application.config.action_dispatch.show_exceptions = true
+    end
+
+    after do
+      # We need to reset the cached variable again, otherwise our stubbed value
+      # will be persisted to other specs and we'd have the same problem but in
+      # reverse.
+      Rails.application.remove_instance_variable '@app_env_config'
+      Rails.application.config.action_dispatch.show_exceptions = @show_exceptions_prev_val
     end
 
     describe "404 - Not Found" do
