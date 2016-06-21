d9c7e15eda20913fa49cc08dfcc141d45f7430c0
George Millo <georgejulianmillo@gmail.com>
2016-06-21 13:22:06 +0200

    refactor edit conflict logic and add comment

--- app/controllers/project_scoped_controller.rb
+++ app/controllers/project_scoped_controller.rb
@@ -28,7 +28,16 @@ class ProjectScopedController < AuthenticatedController
   def check_for_edit_conflicts(record, updated_at_before_save)
     name = record.model_name.name.downcase
     if params[name][:original_updated_at].to_i < updated_at_before_save
-      flash[:update_conflicts_since] = params[name][:original_updated_at].to_i
+      # Even if there have been edit conflicts, the save will still be
+      # successful, which means we're going to *redirect* to another action
+      # (#show), rather than just simply rendering a template - which means
+      # all the current variables and params will be forgotten. But we still
+      # need to pass information about the edit conflicts to the next action,
+      # so we use the flash.
+      #
+      # Only primitive types (String, Array, Hash) can be saved in the flash;
+      # we can't use it to pass a Time objec - so pass the time as a string.
+      flash[:update_conflicts_since] = Time.at(params[name][:original_updated_at].to_i + 1).utc.to_s(:db)
     end
   end
 
@@ -36,7 +45,7 @@ class ProjectScopedController < AuthenticatedController
     if flash[:update_conflicts_since]
       @conflicting_versions = record.versions\
         .order("created_at ASC")\
-        .where("created_at > '#{Time.at(flash[:update_conflicts_since].to_i + 1).utc}'")
+        .where("created_at > '#{flash[:update_conflicts_since]}'")
     end
   end
 end
