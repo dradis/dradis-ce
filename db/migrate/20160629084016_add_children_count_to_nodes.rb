30f5b9ab5c63f4a1063bc47133e179b354a71a5e
George Millo <georgejulianmillo@gmail.com>
2016-07-05 12:58:44 +0200

    Speed up migration further, and add notes about other tested approaches

--- db/migrate/20160629084016_add_children_count_to_nodes.rb
+++ db/migrate/20160629084016_add_children_count_to_nodes.rb
@@ -6,7 +6,7 @@ class AddChildrenCountToNodes < ActiveRecord::Migration
     # from working properly because the line
     # `node.update_attributes!(children_count: node.children.count) doesn't
     # actually update anything; it appears to work but after the migration is
-    # done every node still has 'children_count' set to 0. 
+    # done every node still has 'children_count' set to 0.
     #
     # Overriding `Node` with this dummy nested class where `counter_cache:
     # true` is not present solves the issue and makes the initialization of the
@@ -21,12 +21,37 @@ class AddChildrenCountToNodes < ActiveRecord::Migration
     reversible do |d|
       d.up do
         # I've tried several approaches to migrating the existing data and this
-        # is the fastest I can get it. When testing with about 11,000 Nodes in
-        # my database, this entire migration takes about 15 seconds to run on
-        # my machine.
-        Node.select(:id, :parent_id).where.not(parent_id: nil).includes(:parent).find_each do |node|
-          Node.update(node.parent_id, children_count: node.parent.children_count + 1)
+        # is the fastest I can get it:
+        updates = {}
+
+        Node.where.not(parent_id: nil).select(:parent_id, :id).find_each do |node|
+          updates[node.parent_id] ||= { children_count: 0 }
+          updates[node.parent_id][:children_count] += 1
         end
+
+        Node.update(updates.keys, updates.values)
+
+        # Some other approaches I've tried that were slower:
+
+        # 1:
+        # Node.find_each do |node|
+        #   node.update_attributes!(children_count: node.children.count)
+        # end
+
+        # 2:
+        # nodes = Node.includes(:parent)
+        # nodes.find_each do |node|
+        #   node.parent.children_count += 1 if node.parent
+        # end
+        #
+        # nodes.find_each do |node|
+        #   node.parent.save! if node.parent && node.parent.children_count > 0
+        # end
+
+        # 3:
+        # Node.select(:id, :parent_id).where.not(parent_id: nil).includes(:parent).find_each do |node|
+        #   Node.update(node.parent_id, children_count: node.parent.children_count + 1)
+        # end
       end
     end
   end
