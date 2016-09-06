d07e1165c8b12c94b54112d260ecc923f1d20aaf
George Millo <georgejulianmillo@gmail.com>
2016-09-06 16:59:19 +0200

    extract trash page methods to presenter

---
+++ app/presenters/trashed_revision_presenter.rb
@@ -0,0 +1,73 @@
+class TrashedRevisionPresenter < BasePresenter
+  presents :revision
+
+  def created_at_ago
+    h.local_time_ago(revision.created_at)
+  end
+
+  def info
+    [
+      icon,
+      title,
+      type.downcase,
+      location,
+    ].join(" ").html_safe
+  end
+
+  private
+
+  def icon
+    # Set icon css depending on object type.
+    icon_css = %w{fa}
+    icon_css << case type
+                when 'Evidence'
+                  'fa-flag'
+                when 'Issue'
+                  'fa-bug'
+                when 'Note'
+                  'fa-file-text-o'
+                else
+                  ''
+                end
+    h.content_tag(:i, '', class: icon_css)
+  end
+
+  def location
+    # Get node if object is a Note or an Evidence.
+    if ['Note','Evidence'].include?(type)
+      if trashed_object.node
+        "at " + h.link_to(trashed_object.node.label, trashed_object.node)
+      else
+        'at a Node which has since been deleted'
+      end
+    else
+      ''
+    end
+  end
+
+  def title
+    # Get title or content first characters.
+    if trashed_object.fields.empty?
+      title_text = trashed_object.respond_to?(:content) ? trashed_object.content : trashed_object.text
+    else
+      # Get title field, and if it's not set get first field value.
+      title_text = trashed_object.title? ? trashed_object.title : trashed_object.fields.values[0]
+    end
+    truncated_title = h.truncate(title_text, length: 25, separator: "...")
+    h.content_tag(:span, truncated_title, class: 'item-content')
+  end
+
+  def trashed_object
+    @trashed_object ||= revision.reify
+  end
+
+  def type
+    # If revision type is Note, check note's node id to determine object type.
+    if revision.item_type == 'Note' && trashed_object.node_id == Node.issue_library.id
+      'Issue'
+    else
+      revision.item_type
+    end
+  end
+
+end
