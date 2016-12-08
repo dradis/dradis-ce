3cba12e900960bd9e7bdb4d9e938896e31ad8f18
Aaron Manaloto <aaronpomanaloto@gmail.com>
2016-12-08 17:21:10 +0800

    Add issue trash recover spec

---
+++ spec/features/issue_trash_recover_spec.rb
@@ -0,0 +1,34 @@
+require "spec_helper"
+
+describe "issue trash" do
+
+  subject { page }
+
+  before do
+    login_to_project_as_user
+  end
+
+  example "reflects edit of a previously deleted issue" do
+    @issue = create(:issue)
+    edit_and_delete_issue "issue 1"
+
+    visit "/trash"
+    click_link 'Recover'
+
+    @issue = Note.find(@issue.id)
+    edit_and_delete_issue "issue 2"
+
+    visit "/trash"
+    expect(find(".item-content").text).to eq(@issue.title)
+  end
+
+
+  def edit_and_delete_issue title
+    @issue.update_attribute(
+      :text,
+      "#[Title]#\r\n#{title}\r\n\r\n#[Description]#\r\n\r\n"
+    )
+    @issue.destroy
+  end
+
+end
