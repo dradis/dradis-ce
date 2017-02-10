b86835774397a7087b711339f1cf3306b4e0385b
Daniel Martin <etd-github@nomejortu.com>
2017-02-10 06:29:29 +0100

    ApplicationHelper - cleanup #markup filter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,7 @@
 module ApplicationHelper # :nodoc:
   def markup(text)
-    # return unless text.present?
-    #
-    # output = text.dup
-    # Hash[ *text.scan(/#\[(.+?)\]#[\r|\n](.*?)(?=#\[|\z)/m).flatten.collect{ |str| str.strip } ].keys.each do |field|
-    #   output.gsub!(/#\[#{Regexp.escape(field)}\]#[\r|\n]/, "h4. #{field}\n\n")
-    # end
-    #
-    # auto_link(RedCloth.new(output, [:filter_html, :no_span_caps]).to_html, :sanitize => false ).html_safe
-    new_markup(text)
-  end
+    return unless text.present?
 
-  def new_markup(text)
     context = { }
 
     textile_pipeline = HTML::Pipeline.new [
