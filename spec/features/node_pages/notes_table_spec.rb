2b7d57861a77d041973539a57bc6cb2c3ee6362f
Xavi Vila <xavivm@gmail.com>
2017-11-13 12:50:07 +0000

    move spec to shared examples

--- spec/features/node_pages/notes_table_spec.rb
+++ spec/features/node_pages/notes_table_spec.rb
@@ -12,25 +12,8 @@ describe 'node pages' do
       visit node_path(node, tab: 'notes-tab')
     end
 
-    it 'displays column controls for Title, Created, Created by, Updated' do
-
-      # Prime the element's text, as it is hidden by default
-      find('.js-table-columns', visible: false).text(:all)
-
-      expect(find('.js-table-columns', visible: false)).to have_text('Title')
-      expect(find('.js-table-columns', visible: false)).to have_text('Created')
-      expect(find('.js-table-columns', visible: false)).to have_text('Created by')
-      expect(find('.js-table-columns', visible: false)).to have_text('Updated')
-    end
-
-    it 'displays custom columns based on Issue content' do
-      # Prime the element's text, as it is hidden by default
-      find('.js-table-columns', visible: false).text(:all)
-
-      expect(@note.fields.keys).to include('Description', 'Extra')
-      @note.fields.keys.each do |column|
-        expect(find('.js-table-columns', visible: false)).to have_text(column)
-      end
-    end
+    let(:columns) { ['Title', 'Created', 'Created by', 'Updated'] }
+    let(:custom_columns) { ['Description', 'Extra'] }
+    it_behaves_like 'an index table'
   end
 end
