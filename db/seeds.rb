df33e4d6014b956272eae19b2b8d8d4e76fa0f22
George Millo <george@millo.me>
2018-03-12 11:23:41 +0000

    lowercase names when creating tags
    they get normalised to lowercase anyway within a callback in Tag, so we
    might as well do it here to minimise confusion

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # Create a few default tags.
 [
-  '!9467bd_Critical',
-  '!d62728_High',
-  '!ff7f0e_Medium',
-  '!6baed6_Low',
-  '!2ca02c_Info',
+  '!9467bd_critical',
+  '!d62728_high',
+  '!ff7f0e_medium',
+  '!6baed6_low',
+  '!2ca02c_info',
 ].each do |name|
   Tag.create(name: name)
 end
