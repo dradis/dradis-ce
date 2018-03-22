399db80d0982d6f094a55bfab46e28f7f7022c60
Daniel Martin <etd-github@nomejortu.com>
2018-03-22 15:30:23 +0100

    PropertyPresenter#value - fix operator

--- app/presenters/property_presenter.rb
+++ app/presenters/property_presenter.rb
@@ -26,7 +26,7 @@ class PropertyPresenter < BasePresenter
       end
 
     else
-      if property_value ~= /\n/
+      if property_value =~ /\n/
         content_tag(:pre, property_value)
       else
         content_tag(:p, property_value)
