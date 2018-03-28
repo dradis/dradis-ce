81e97375eaa7b86df47d13f8a26ebe252ccb4502
George Millo <george@millo.me>
2018-03-28 18:47:44 +0100

    merge upload/status and console/status

--- app/controllers/console_controller.rb
+++ app/controllers/console_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This controller is used by the console_updater js to retrieve logs
 # for a specific job
 class ConsoleController < ProjectScopedController
--- config/routes.rb
+++ config/routes.rb
@@ -96,7 +96,6 @@ Rails.application.routes.draw do
   get  '/upload'        => 'upload#index',  as: :upload_manager
   post '/upload'        => 'upload#create'
   post '/upload/parse'  => 'upload#parse'
-  get  '/upload/status' => 'upload#status'
 
   root to: 'home#index'
 end
