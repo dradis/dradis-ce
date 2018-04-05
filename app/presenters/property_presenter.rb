1dd9b483528ab256a6da94e5d222cd561a46fd6b
George Millo <george@millo.me>
2018-04-05 17:10:54 +0100

    node propeties: render non-'service' tables
    Properties with the keys 'services' or 'services_extra' are special
    cases and should be handled accordingly - but that doesn't mean that
    other properties should never be rendered as a table.
    
    Note that a lot of the code being removed here was old redundant stuff
    that shouldn't been left in the presenter anyway

--- app/presenters/property_presenter.rb
+++ app/presenters/property_presenter.rb
@@ -26,7 +26,7 @@ class PropertyPresenter < BasePresenter
     elsif property_key == 'services_extras'
       render 'nodes/show/services_extras_table', extras: property_value
     elsif property_value.is_a?(Array)
-      if property_value[0].is_a?(Hash)
+      if property_value.all? { |x| x.is_a?(Hash) }
         render_table
       else
         content_tag(:p, property_value.join(', '))
@@ -47,13 +47,7 @@ class PropertyPresenter < BasePresenter
   end
 
   def render_table
-    values         = property_value
-    column_names   = values.map(&:keys).flatten.uniq.sort.map(&:to_sym).
-                     delete_if{ |cn| !services_table_columns.include?(cn) }
-    table          = table_info(
-      values: values,
-      sort_by_port: column_names.include?(:port)
-    )
+    column_names = property_value.map(&:keys).flatten.uniq.sort.map(&:to_s)
 
     thead = content_tag(:thead) do
       content_tag(:tr) do
@@ -64,7 +58,7 @@ class PropertyPresenter < BasePresenter
     end
 
     tbody = content_tag(:tbody) do
-      table.collect do |entry|
+      property_value.map do |entry|
         content_tag(:tr) do
           column_names.collect do |column_name|
             concat content_tag(:td, entry[column_name])
@@ -73,51 +67,10 @@ class PropertyPresenter < BasePresenter
       end.join.html_safe
     end
 
-    content_tag(:div,
+    content_tag(
+      :div,
       content_tag(:table, thead.concat(tbody)),
-      class: 'content-textile'
-    ).concat(output)
-  end
-
-  def merge_rows(rows)
-    rows.group_by{|e| [e[:port], e[:protocol]]}.map do |_, a|
-      a.reduce do |memo, row|
-        memo.merge(row) do |_key, oldvalue, newvalue|
-          if oldvalue == newvalue
-            oldvalue
-          elsif oldvalue.respond_to?(:concat)
-            backupvalue = oldvalue.clone
-            backupvalue.concat(content_tag(:br)).concat(newvalue).html_safe
-          elsif oldvalue.respond_to?(:merge)
-            oldvalue.merge(newvalue)
-          end
-        end
-      end
-    end
-  end
-
-  def services_table_columns
-    NodeProperties::SERVICE_KEYS
-  end
-
-  # Prepare info to be displayed in services table
-  # converts
-  # [ {"port" => 80, "name" => "foo"},
-  #   {"port" => 22, "name" => "bar"},
-  #   {"port" => 80, "name" => "baz"}, ... ]
-  # into
-  # [ {"port" => 22, "name" => "baz"},
-  #   {"port" => 80, "name" => "foo<br></br>bar"}, ... ]
-  # Id sort_by_port is true it merges duplicated rows by [port/protocol],
-  # adding duplicted values with <br/><br/>
-  # It always filters the keys to those available in `services_table_columns`
-  def table_info(values:, sort_by_port:)
-    if sort_by_port
-      merge_rows(values).sort_by{ |h| h[:port] }
-    else
-      values
-    end.map do |se|
-      se.select{ |k, _| services_table_columns.include?(k.to_sym) }
-    end
+      class: 'content-textile',
+    )
   end
 end
