2fe857fe7a3209dc010c9d6f13b7fe302baac897
Xavi Vila <xavivm@gmail.com>
2018-05-07 10:35:17 +0100

    skip unnecessary db queries

--- app/controllers/issues/nodes_controller.rb
+++ app/controllers/issues/nodes_controller.rb
@@ -1,13 +1,18 @@
 class Issues::NodesController < IssuesController
+  skip_before_action :find_issuelib
+  skip_before_action :find_issues
   skip_before_action :find_or_initialize_issue
+  skip_before_action :find_or_initialize_tags
 
   def show
-    issue = Issue.find(params[:issue_id])
-    @node = Node.joins(:evidence)
-                .select('nodes.id, label, type_id, count(evidence.id) as evidence_count, nodes.updated_at')
-                .where('evidence.issue_id = ? and nodes.id = ?', params[:issue_id], params[:id])
-                .first
-    @instances = Evidence.where(issue: issue, node: @node)
+    @node =
+      Node.joins(:evidence)
+          .select('nodes.id, label, type_id,
+                  count(evidence.id) as evidence_count, nodes.updated_at')
+          .where('evidence.issue_id = ? and nodes.id = ?',
+                 params[:issue_id], params[:id])
+          .first
+    @instances = Evidence.where(issue_id: params[:issue_id], node: @node)
 
     render layout: false
   end
