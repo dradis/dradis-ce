0112229583f5e5f6a121d8a04943669720fd0373
Xavi Vila <xavivm@gmail.com>
2018-06-07 13:48:18 +0100

    a few more relationships

--- app/models/concerns/notifiable.rb
+++ app/models/concerns/notifiable.rb
@@ -2,6 +2,6 @@ module Notifiable
   extend ActiveSupport::Concern
 
   included do
-    has_many :notifications, as: :notifiable
+    has_many :notifications, as: :notifiable, dependent: :destroy
   end
 end
--- app/models/concerns/subscribable.rb
+++ app/models/concerns/subscribable.rb
@@ -1,4 +1,4 @@
-module Suscribable
+module Subscribable
   extend ActiveSupport::Concern
 
   included do
