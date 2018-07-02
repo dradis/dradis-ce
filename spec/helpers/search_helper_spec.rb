6bb0013e60aed777009b103a7ddd171d9246b02b
Daniel Martin <etd-github@nomejortu.com>
2018-07-02 16:57:06 +0200

    SearchHelper spec - use :project_id URLs

--- spec/helpers/search_helper_spec.rb
+++ spec/helpers/search_helper_spec.rb
@@ -11,13 +11,17 @@ describe SearchHelper do
   describe ".search_path" do
     %w[all nodes notes issues evidences].each do |scope|
       it "formats correct #{scope} path" do
+        project = Project.new
+        assign(:project, project)
         expect(helper.search_filter_path(options(term: "test", scope: scope))).
-          to eq "/search?q=test&scope=#{scope}"
+          to eq "/projects/#{project.id}/search?q=test&scope=#{scope}"
       end
     end
 
     it "returns empty search path when no options provided" do
-      expect(helper.search_filter_path).to eq "/search"
+      project = Project.new
+      assign(:project, project)
+      expect(helper.search_filter_path).to eq "/projects/#{project.id}/search"
     end
   end
 
