8dcc2982746b78224e102a6528e7a19cc2ca9c52
George Millo <george@millo.me>
2018-07-03 12:56:47 +0100

    typo fixes

--- spec/helpers/search_helper_spec.rb
+++ spec/helpers/search_helper_spec.rb
@@ -62,7 +62,7 @@ RSpec.describe SearchHelper do
     end
 
     context "if text is in boundaries of 300 chars" do
-      it "return orginal text" do
+      it 'return original text' do
         old_text = "a" * 300
         term = "dradis"
 
