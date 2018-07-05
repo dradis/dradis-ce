a38087718934253e7bafb98853d4a960b81936ea
George Millo <george@millo.me>
2018-07-05 15:26:07 +0100

    update ProjectScoped for consistency with Pro

--- app/controllers/concerns/project_scoped.rb
+++ app/controllers/concerns/project_scoped.rb
@@ -2,27 +2,34 @@ module ProjectScoped
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_project
+    # Use prepend_before_action to keep things consistent with Pro:
+    prepend_before_action :set_project
+    before_action :set_nodes
 
     helper :snowcrash
     layout 'snowcrash'
   end
 
   protected
-  # Internal: Sets saves the current :project_id as PaperTrail::Version metadata
-  # this is going to allow us to speed up recovery of Versions scoped to the
-  # current project.
+
+  # In Pro this method sets the column `versions.project_id`.
   #
-  # See also:
+  # See https://github.com/airblade/paper_trail#metadata-from-controllers
   #
-  #   https://github.com/airblade/paper_trail#metadata-from-controllers
+  # Since versions.project_id doesn't exist in CE, the method is a no-op here.
   #
+  # We don't strictly need to include the empty method here, but leaving it in
+  # makes things clearer when dealing with CE/Pro merges in future.
   def info_for_paper_trail
-    { project_id: @project.id } if @project
   end
 
+  def set_nodes
+    @nodes = Node.in_tree
+  end
+
+  # In Pro this method will load a Project using params[:project_id]. In
+  # CE things are much simpler.
   def set_project
-    @nodes   = Node.in_tree
     @project = Project.new
   end
 end
