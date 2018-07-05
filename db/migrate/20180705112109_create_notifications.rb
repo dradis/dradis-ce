63d62b803f367335fce07820cb6d56c809f9dda0
Aaron Manaloto <aaronpomanaloto@gmail.com>
2018-07-05 19:34:35 +0800

    Fix notifications migration

--- db/migrate/20180705112109_create_notifications.rb
+++ db/migrate/20180705112109_create_notifications.rb
@@ -6,8 +6,8 @@ class CreateNotifications < ActiveRecord::Migration[5.1]
 
       t.references  :notifiable, polymorphic: true, index: true
 
-      t.references  :actor, index: true, foreign_key: { to_table: :user }
-      t.references  :recipient, index: true, foreign_key: { to_table: :user }
+      t.references  :actor, index: true, foreign_key: { to_table: :users }
+      t.references  :recipient, index: true, foreign_key: { to_table: :users }
 
       t.timestamps
     end
