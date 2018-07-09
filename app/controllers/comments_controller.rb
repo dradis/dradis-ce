3f1d0ae102c94afa28961801bd226d5cad274d27
George Millo <george@millo.me>
2018-07-09 12:54:35 +0100

    DRY comment websocket logic

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,41 +8,32 @@ class CommentsController < AuthenticatedController
     @comment = Comment.new(comment_params)
     @comment.user = current_user
     @comment.save!
-    ActionCable.server.broadcast(
-      'comment_channel',
-      action: 'create',
-      comment_feed_id: dom_id(@comment.commentable),
-      html: render_to_string(@comment), # TODO do we have to worry about XSS issues?
-    )
-    head :no_content
+    websocket_event(@comment, 'create')
   end
 
   def update
     @comment.update_attributes!(comment_params)
-    ActionCable.server.broadcast(
-      'comment_channel',
-      action: 'update',
-      comment_feed_id: dom_id(@comment.commentable),
-      comment_id: @comment.id,
-      html: render_to_string(@comment), # TODO do we have to worry about XSS issues?
-    )
-    head :no_content
+    websocket_event(@comment, 'update')
   end
 
   def destroy
     @comment.destroy
-    ActionCable.server.broadcast(
-      'comment_channel',
-      action: 'destroy',
-      comment_feed_id: dom_id(@comment.commentable),
-      comment_id: @comment.id,
-    )
-    head :no_content
+    websocket_event(@comment, 'destroy')
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:content, :commentable_type, :commentable_id)
+  end
+
+  def websocket_event(action, comment)
+    ActionCable.server.broadcast(
+      'comment_channel',
+      action: action,
+      comment_feed_id: dom_id(comment.commentable),
+      comment_id: comment.id,
+    )
+    head :no_content
   end
 end
