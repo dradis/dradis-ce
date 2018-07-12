30ff5cc4cc586d76dcac195e17d30eb0e2982a63
Aaron Manaloto <aaronpomanaloto@gmail.com>
2018-07-12 20:59:26 +0800

    Remove unused params

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,13 +2,7 @@ class NotificationsController < AuthenticatedController
   include ProjectScoped
 
   def index
-    @notifications =
-      if params[:count]
-        current_user.notifications.newest.limit(params[:count].to_i)
-      else
-        current_user.notifications.newest
-      end
-
+    @notifications = current_user.notifications.newest.limit(7)
     @unread_count = @notifications.unread.count
 
     respond_to do |format|
