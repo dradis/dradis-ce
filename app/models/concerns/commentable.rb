4ffbd159a4aa375017857a9af406e2301886bddf
Xavi Vila <xavivm@gmail.com>
2018-07-18 12:25:25 +0100

    commentable activities include comment activities

--- app/models/concerns/commentable.rb
+++ app/models/concerns/commentable.rb
@@ -4,4 +4,10 @@ module Commentable
   included do
     has_many :comments, as: :commentable, dependent: :destroy
   end
+
+  def commentable_activities
+    Activity.where(trackable_type: self.class.to_s, trackable_id: self.id).or(
+      Activity.where(trackable_type: 'Comment', trackable_id: [self.comments.map(&:id)])
+    )
+  end
 end
