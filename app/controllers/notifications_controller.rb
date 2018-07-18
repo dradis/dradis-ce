88e90368d7e17184145674be536f0f1d1355b62d
George Millo <george@millo.me>
2018-07-18 13:03:30 +0100

    add basic notifications#index page

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,11 +2,16 @@ class NotificationsController < AuthenticatedController
   include ProjectScoped
 
   def index
-    @notifications = current_user.notifications.newest.limit(20)
-    @unread_count = @notifications.unread.count
-
     respond_to do |format|
-      format.js
+      format.html do
+        @notifications = current_user.notifications.newest.page(params[:page])
+        @unread_count  = @notifications.unread.count
+      end
+      format.js do
+        @notifications = current_user.notifications.newest.limit(20)
+        # TODO how can we avoid this repetition of '@unread_count = ...'?
+        @unread_count  = @notifications.unread.count
+      end
     end
   end
 end
---
+++ spec/features/notifications/index_page_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'notifications index page' do
+  before do
+    login_to_project_as_user
+  end
+
+  let(:me) { @logged_in_as }
+  let(:comment) { create(:comment) }
+  let(:u1) { create(:user) }
+  let(:u2) { create(:user) }
+
+  example 'when I have no notifications' do
+    # other users have notifications but you don't:
+    create(:notification, recipient: u1, notifiable: comment)
+    create(:notification, recipient: u2, notifiable: comment)
+
+    visit project_notifications_path(@project)
+
+    expect(page).not_to have_selector '.notification'
+    expect(page).to have_content 'You have no notifications yet'
+  end
+
+  example 'when I have notifications' do
+    # my notifications: 1 read, 1 unread
+    create(:notification, recipient: me, notifiable: comment)
+    create(:notification, recipient: me, notifiable: comment, read_at: Time.now)
+
+    # other people's notifications:
+    create(:notification, recipient: u1, notifiable: comment)
+    create(:notification, recipient: u2, notifiable: comment)
+
+    visit project_notifications_path(@project)
+
+    expect(page).to have_selector '.notification', count: 2
+    expect(page).not_to have_content 'You have no notifications yet'
+  end
+
+  # TODO in Pro test that the page only shows notifs for *current* project
+end
