64039abf8e86d7259a808ba1956d0a99e9b69042
George Millo <george@millo.me>
2018-07-19 13:27:38 +0100

    bug fix: incorrect unread count in notif navbar

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -7,12 +7,12 @@ class NotificationsController < AuthenticatedController
     respond_to do |format|
       format.html do
         @notifications = notifs.page(params[:page])
-        @unread_count  = @notifications.unread.count
       end
       format.js do
         @notifications = notifs.limit(20)
-        # TODO how can we avoid this repetition of '@unread_count = ...'?
-        @unread_count  = @notifications.unread.count
+        # NB the unread count is not the same as @notifications.count because
+        # @notifications a) includes read notifs and b) is capped at 20
+        @unread_count  = current_user.notifications.unread.count
       end
     end
   end
