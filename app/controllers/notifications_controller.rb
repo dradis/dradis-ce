e236d17f9b29d6d42bc30703d4d47e9bef212b2b
George Millo <george@millo.me>
2018-07-19 13:10:02 +0100

    fix (not of all of the) N+1 issues on notifs#index

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,13 +2,15 @@ class NotificationsController < AuthenticatedController
   include ProjectScoped
 
   def index
+    # TODO this doesn't fix all the N+1 query problems, needs a closer look:
+    notifs = current_user.notifications.newest.includes(:actor, notifiable: [:user, :commentable])
     respond_to do |format|
       format.html do
-        @notifications = current_user.notifications.newest.page(params[:page])
+        @notifications = notifs.page(params[:page])
         @unread_count  = @notifications.unread.count
       end
       format.js do
-        @notifications = current_user.notifications.newest.limit(20)
+        @notifications = notifs.limit(20)
         # TODO how can we avoid this repetition of '@unread_count = ...'?
         @unread_count  = @notifications.unread.count
       end
