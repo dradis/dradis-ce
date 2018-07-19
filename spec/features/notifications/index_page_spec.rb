762f1152247edcfef9877b8814859a098b19eaa5
George Millo <george@millo.me>
2018-07-19 14:40:03 +0100

    notifications#index - DRY feature specs

--- spec/features/notifications/index_page_spec.rb
+++ spec/features/notifications/index_page_spec.rb
@@ -9,10 +9,15 @@ RSpec.describe 'notifications index page' do
   let(:u1) { create(:user) }
   let(:u2) { create(:user) }
 
+  def create_notification(opts = {})
+    opts[:notifiable] ||= create(:comment)
+    create(:notification, opts)
+  end
+
   example 'when I have no notifications' do
     # other users have notifications but you don't:
-    create(:notification, recipient: u1, notifiable: create(:comment))
-    create(:notification, recipient: u2, notifiable: create(:comment))
+    create_notification(recipient: u1)
+    create_notification(recipient: u2)
 
     visit project_notifications_path(@project)
 
@@ -22,18 +27,16 @@ RSpec.describe 'notifications index page' do
 
   example 'when I have notifications' do
     # my notifications: 1 read, 1 unread
-    create(:notification, recipient: me, notifiable: create(:comment))
-    create(:notification, recipient: me, notifiable: create(:comment), read_at: Time.now)
+    create_notification(recipient: me)
+    create_notification(recipient: me, read_at: Time.now)
 
     # other people's notifications:
-    create(:notification, recipient: u1, notifiable: create(:comment))
-    create(:notification, recipient: u2, notifiable: create(:comment))
+    create_notification(recipient: u1)
+    create_notification(recipient: u2)
 
     visit project_notifications_path(@project)
 
     expect(page).to have_selector '.notification', count: 2
     expect(page).not_to have_content 'You have no notifications yet'
   end
-
-  # TODO in Pro test that the page only shows notifs for *current* project
 end
