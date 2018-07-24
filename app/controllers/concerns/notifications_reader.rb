9e3f7795782e624ebe6c259642f47f148eb75115
Aaron Manaloto <aaronpomanaloto@gmail.com>
2018-07-24 21:55:25 +0800

    Use inner join and update_all to optimize queries

--- app/controllers/concerns/notifications_reader.rb
+++ app/controllers/concerns/notifications_reader.rb
@@ -3,11 +3,24 @@ module NotificationsReader
 
   # Mark each notifications associated with the item as read
   def read_item_notifications(item)
-    item.comments.each do |comment|
-      comment.notifications.where(recipient: current_user, read_at: nil).each(&:read!)
+    Notification.transaction do
+      notifications =
+        notifications_by_commentable(item).unread.where(recipient: current_user)
+      notifications.update_all(read_at: Time.now)
     end
 
     # Re-set the notifications alert dot
     @has_unread_notifications = current_user.notifications.unread.any?
+  end
+
+  def notifications_by_commentable(commentable)
+    Notification.
+      joins('INNER JOIN comments ON notifications.notifiable_id = comments.id').
+      where(notifiable_type: 'Comment').
+      where(
+        'comments.commentable_type = ? AND comments.commentable_id = ?',
+        commentable.class.to_s,
+        commentable.id
+      )
   end
 end
