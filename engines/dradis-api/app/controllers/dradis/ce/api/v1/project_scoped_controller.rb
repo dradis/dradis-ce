12e1f76f5e440dec04da41caf5d3307f8ba7b9a5
George Millo <george@millo.me>
2018-07-26 10:15:48 +0100

    add comment explaining API bug in development env
    --skip-ci

--- engines/dradis-api/app/controllers/dradis/ce/api/v1/project_scoped_controller.rb
+++ engines/dradis-api/app/controllers/dradis/ce/api/v1/project_scoped_controller.rb
@@ -1,5 +1,13 @@
 module Dradis::CE::API
   module V1
+    # In Pro the dradispro-api engine overrides 'current_project' by injecting
+    # a module into this controller on startup. Unfortunately it won't work
+    # properly in development because of code reloading; every time you make a
+    # new request, PSController will be reloaded without the module included.
+    #
+    # Workaround: set 'config.cache_classes = true' in config/development.rb
+    # (but don't commit it!) The downside is that you'll now have to restart
+    # the server every time you make a change.)
     class ProjectScopedController < Dradis::CE::API::APIController
       include ActivityTracking
 
