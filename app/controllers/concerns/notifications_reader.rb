b2e03aa0f73df3b184d88db285067b771282b509
Aaron Manaloto <aaronpomanaloto@gmail.com>
2018-07-27 19:40:43 +0800

    Delegate joins query to the job

--- app/controllers/concerns/notifications_reader.rb
+++ app/controllers/concerns/notifications_reader.rb
@@ -1,21 +1,10 @@
 module NotificationsReader
   protected
 
-  # Mark each notifications associated with the item as read
-  def read_item_notifications(item)
+  def read_item_notifications(commentable)
     NotificationsReaderJob.perform_later(
-      notification_ids: notifications_by_commentable(item).pluck(:id)
+      commentable_id: commentable.id,
+      commentable_type: commentable.class.to_s
     )
-  end
-
-  def notifications_by_commentable(commentable)
-    Notification.
-      joins('INNER JOIN comments ON notifications.notifiable_id = comments.id').
-      where(notifiable_type: 'Comment').
-      where(
-        'comments.commentable_type = ? AND comments.commentable_id = ?',
-        commentable.class.to_s,
-        commentable.id
-      )
   end
 end
--- app/jobs/notifications_reader_job.rb
+++ app/jobs/notifications_reader_job.rb
@@ -1,10 +1,24 @@
 class NotificationsReaderJob < ApplicationJob
   queue_as :dradis_project
 
-  def perform(notification_ids:)
+  # Mark each notifications associated with the item as read
+  def perform(commentable_id:, commentable_type:)
     Notification.transaction do
-      Notification.where(id: notification_ids).update_all(read_at: Time.now)
+      notifications_by_commentable(id: commentable_id, type: commentable_type).
+        update_all(read_at: Time.now)
     end
   end
 
+  private
+
+  def notifications_by_commentable(id:, type:)
+    Notification.
+      joins('INNER JOIN comments ON notifications.notifiable_id = comments.id').
+      where(notifiable_type: 'Comment').
+      where(
+        'comments.commentable_type = ? AND comments.commentable_id = ?',
+        type,
+        id
+      )
+  end
 end
