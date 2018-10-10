96ef7aa52c614f57dfbe142402d6a5cc7581013f
Aaron Manaloto <aaronpomanaloto@gmail.com>
2018-10-10 16:05:11 +0800

    Add routes for comments in api

--- engines/dradis-api/config/routes.rb
+++ engines/dradis-api/config/routes.rb
@@ -1,8 +1,17 @@
 Dradis::CE::API::Engine::routes.draw do
+  concern :with_comments do
+    resources :comments, only: [:index, :create]
+  end
+
   scope path: :api do
     defaults format: 'json' do
       scope module: :v1, constraints: Dradis::CE::API::RoutingConstraints.new(version: 1, default: true) do
-        resources :issues
+        resources :issues, concerns: :with_comments
+        resources :evidence, only: [], concerns: :with_comments
+        resources :notes, only: [], concerns: :with_comments
+
+        resources :comments, only: [:show, :update, :destroy]
+
         resources :nodes do
           resources :evidence
           resources :notes
