e2cadd99b6e4765cf0e63442094110ad4d1e7651
Xavi Vila <xavivm@gmail.com>
2018-10-16 12:27:30 +0100

    be safe if the user is not found in the helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,10 +2,14 @@ module CommentsHelper
   def format_comment(text)
     simple_format(text).gsub(/@[a-z0-9\-_\.]+/i) do |mention|
       user = User.find_by_email(mention[1..-1])
-      content_tag :span do
-        avatar_image(user, size: 30) +
-        content_tag(:strong, user.email)
+      if user
+        content_tag :span, class: 'format_comment' do
+          avatar_image(user, size: 30) +
+          content_tag(:strong, user.email)
+        end
+      else
+        mention
       end
-    end.html_safe
+    end.html
   end
 end
