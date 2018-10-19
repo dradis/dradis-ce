416b283904e30a296f27a26cf61f042281039019
Aaron Manaloto <aaronpomanaloto@gmail.com>
2018-10-19 19:14:41 +0800

    Use lengthy path instead

--- engines/dradis-api/config/routes.rb
+++ engines/dradis-api/config/routes.rb
@@ -7,14 +7,11 @@ Dradis::CE::API::Engine::routes.draw do
     defaults format: 'json' do
       scope module: :v1, constraints: Dradis::CE::API::RoutingConstraints.new(version: 1, default: true) do
         resources :issues, concerns: :with_comments
-        resources :evidence, only: [], concerns: :with_comments
-        resources :notes, only: [], concerns: :with_comments
-
         resources :comments, only: [:show, :update, :destroy]
 
         resources :nodes do
-          resources :evidence
-          resources :notes
+          resources :evidence, concerns: :with_comments
+          resources :notes, concerns: :with_comments
           constraints(:filename => /.*/) do
             resources :attachments, param: :filename
           end
