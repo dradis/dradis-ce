4efbbc00b514a0093ec2a839fee9946043fbe4c3
Xavi Vila <xavivm@gmail.com>
2019-03-19 10:28:47 +0000

    in db we save donclse tag names, so we use downcase when seraching in sqlite

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -17,7 +17,7 @@ module Taggable
 
   def tag_list=(names)
     self.tags = names.split(",").map do |n|
-      Tag.where(name: n.strip).first_or_create!
+      Tag.where(name: n.strip.downcase).first_or_create!
     end
   end
 end
