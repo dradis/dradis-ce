67af4bd70c73a19b20ab0a1cba89323f19d8c53e
Brian Pearce <brian.o.pearce@gmail.com>
2019-06-06 18:38:48 +0200

    Refactor Nodes::Merger
    Use smaller concise methods for each step of the move.

--- app/services/nodes/merger.rb
+++ app/services/nodes/merger.rb
@@ -8,31 +8,20 @@ class Nodes::Merger
     notes: :node_id
   }.freeze
 
-  attr_accessor :target_node_id, :source_node, :source_attachments
-
   def self.call(target_node_id, source_node, &block)
     new(target_node_id, source_node).call(&block)
   end
 
   def initialize(target_node_id, source_node)
-    @source_attachments = source_node.attachments
     @source_node = source_node
     @target_node_id = target_node_id
   end
 
   def call(&block)
     Node.transaction do
-      SOURCES.each do |relation, column|
-        source_node.send(relation).update_all(column => target_node_id)
-      end
-
-      Node.reset_counters target_node_id, :children_count
-      Node.reset_counters source_node.id, :children_count
-
-      source_node.attachments.each do |attachment|
-        attachment.node_id = target_node_id
-        attachment.save
-      end
+      move_descendents
+      reset_counter_caches
+      move_attachments
 
       yield if block
 
@@ -47,15 +36,39 @@ class Nodes::Merger
     return [e.message]
   end
 
-  def undo_attachments_move
-    source_attachments.each do |attachment|
-      next if File.exist? attachment.fullpath
+  private
 
-      saved_attachment = Attachment.find(attachment.filename,
-        conditions: { node_id: target_node_id })
+    attr_accessor :target_node_id, :source_node, :source_attachments
 
-      saved_attachment.node_id = source_node.id
-      saved_attachment.save
+    def move_descendents
+      SOURCES.each do |relation, column|
+        source_node.send(relation).update_all(column => target_node_id)
+      end
     end
-  end
+
+    def reset_counter_caches
+      Node.reset_counters target_node_id, :children_count
+      Node.reset_counters source_node.id, :children_count
+    end
+
+    def move_attachments
+      self.source_attachments = source_node.attachments
+
+      source_node.attachments.each do |attachment|
+        attachment.node_id = target_node_id
+        attachment.save
+      end
+    end
+
+    def undo_attachments_move
+      source_attachments.each do |attachment|
+        next if File.exist? attachment.fullpath
+
+        saved_attachment = Attachment.find(attachment.filename,
+          conditions: { node_id: target_node_id })
+
+        saved_attachment.node_id = source_node.id
+        saved_attachment.save
+      end
+    end
 end
