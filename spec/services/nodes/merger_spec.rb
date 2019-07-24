4090abe054ceca03ad9114c158d4d9eb8dcf380f
Brian Pearce <brian.o.pearce@gmail.com>
2019-07-24 09:23:44 +0200

    Remove :aggregate_failures flag
    It's usage in the codebase is an anti-pattern. Instead remove it and break tests
    that have multiple assertions into different tests.

--- spec/services/nodes/merger_spec.rb
+++ spec/services/nodes/merger_spec.rb
@@ -12,32 +12,48 @@ RSpec.describe Nodes::Merger do
 
     it { should match_array [] }
 
-    it 'moves notes to target node', :aggregate_failures do
+    it 'moves notes to target node' do
       note = create(:note, node: source_node)
-
-      expect { merge_nodes }.to change(target_node.notes, :count).by 1
+      merge_nodes
       expect(target_node.notes).to include note
     end
 
-    it 'moves evidence to target node', :aggregate_failures do
-      evidence = create(:evidence, node: source_node)
+    it 'increases the count of target node notes' do
+      create(:note, node: source_node)
+      expect { merge_nodes }.to change(target_node.notes, :count).by 1
+    end
 
-      expect { merge_nodes }.to change(target_node.evidence, :count).by 1
+    it 'moves evidence to target node' do
+      evidence = create(:evidence, node: source_node)
+      merge_nodes
       expect(target_node.evidence).to include evidence
     end
 
-    it 'moves activities to target node', :aggregate_failures do
-      activity = create(:activity, trackable: source_node)
+    it 'increases the count of target node evidence' do
+      create(:evidence, node: source_node)
+      expect { merge_nodes }.to change(target_node.evidence, :count).by 1
+    end
 
-      expect { merge_nodes }.to change(target_node.activities, :count).by 1
+    it 'moves activities to target node' do
+      activity = create(:activity, trackable: source_node)
+      merge_nodes
       expect(target_node.activities).to include activity
     end
 
-    it 'moves children to target node', :aggregate_failures do
-      child_node = create(:node, parent: source_node)
+    it 'increases the count of target node activities' do
+      create(:activity, trackable: source_node)
+      expect { merge_nodes }.to change(target_node.activities, :count).by 1
+    end
 
-      expect { merge_nodes }.to change(target_node.children, :count).by 1
+    it 'moves children to target node' do
+      child_node = create(:node, parent: source_node)
+      merge_nodes
       expect(target_node.children).to include child_node
+    end
+
+    it 'increases the count of target node children' do
+      create(:node, parent: source_node)
+      expect { merge_nodes }.to change(target_node.children, :count).by 1
     end
 
     it "updates the source node's children counter cache" do
@@ -52,11 +68,15 @@ RSpec.describe Nodes::Merger do
       expect { merge_nodes }.to change { target_node.reload.children_count }.by 1
     end
 
-    it "move's attachments to target node", :aggregate_failures do
+    it 'moves attachments to target node' do
       attachment = create(:attachment, node: source_node)
-
-      expect { merge_nodes }.to change { target_node.attachments.count }.by 1
+      merge_nodes
       expect(File.exist?(attachment.fullpath)).to be false
+    end
+
+    it 'increases the target node attachment count' do
+      create(:attachment, node: source_node)
+      expect { merge_nodes }.to change { target_node.attachments.count }.by 1
     end
 
     it 'executes a given block within the transaction' do
@@ -77,32 +97,48 @@ RSpec.describe Nodes::Merger do
 
       it { should match_array ['StandardError'] }
 
-      it 'source retains notes', :aggregate_failures do
+      it 'does not move note' do
         note = create(:note, node: source_node)
-
-        expect { merge_nodes }.not_to change(source_node.notes, :count)
+        merge_nodes
         expect(target_node.notes).not_to include note
       end
 
-      it 'source retains evidence', :aggregate_failures do
-        evidence = create(:evidence, node: source_node)
+      it 'does not change source node notes count' do
+        create(:note, node: source_node)
+        expect { merge_nodes }.not_to change(source_node.notes, :count)
+      end
 
-        expect { merge_nodes }.not_to change(source_node.evidence, :count)
+      it 'does not move evidence' do
+        evidence = create(:evidence, node: source_node)
+        merge_nodes
         expect(target_node.evidence).not_to include evidence
       end
 
-      it 'source retains activities', :aggregate_failures do
-        activity = create(:activity, trackable: source_node)
+      it 'does not change source node evidence count' do
+        create(:evidence, node: source_node)
+        expect { merge_nodes }.not_to change(source_node.evidence, :count)
+      end
 
-        expect { merge_nodes }.not_to change(source_node.activities, :count)
+      it 'does not move activity' do
+        activity = create(:activity, trackable: source_node)
+        merge_nodes
         expect(target_node.activities).not_to include activity
       end
 
-      it 'source retains children', :aggregate_failures do
-        child_node = create(:node, parent: source_node)
+      it 'does not change source node activities count' do
+        create(:activity, trackable: source_node)
+        expect { merge_nodes }.not_to change(source_node.activities, :count)
+      end
 
-        expect { merge_nodes }.not_to change(source_node.children, :count)
+      it 'does not move children' do
+        child_node = create(:node, parent: source_node)
+        merge_nodes
         expect(target_node.children).not_to include child_node
+      end
+
+      it 'does not change source node children count' do
+        create(:node, parent: source_node)
+        expect { merge_nodes }.not_to change(source_node.children, :count)
       end
 
       it 'no changes to counter caches' do
@@ -112,11 +148,15 @@ RSpec.describe Nodes::Merger do
         expect { merge_nodes }.not_to change { target_node.reload.children_count }
       end
 
-      it 'source retains attachments', :aggregate_failures do
+      it 'does not move attachments' do
         attachment = create(:attachment, node: source_node)
-
-        expect { merge_nodes }.not_to change { target_node.attachments.count }
+        merge_nodes
         expect(File.exist?(attachment.fullpath)).to be true
+      end
+
+      it 'does not change source node attachments count' do
+        create(:attachment, node: source_node)
+        expect { merge_nodes }.not_to change { target_node.attachments.count }
       end
     end
   end
