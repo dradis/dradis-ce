82239dcbffd5800124977b0f57a0e5fca65ef522
Xavi Vila <xavivm@gmail.com>
2019-08-30 12:34:16 +0100

    update kit_import_job_spec to keep only CE parts

--- spec/jobs/kit_import_job_spec.rb
+++ spec/jobs/kit_import_job_spec.rb
@@ -30,9 +30,6 @@ RSpec.describe KitImportJob do
       allow(Methodology).to receive(:pwd).and_return(
         Pathname.new(Configuration.paths_templates_methodologies)
       )
-      allow(ProjectTemplate).to receive(:pwd).and_return(
-        Pathname.new(Configuration.paths_templates_projects)
-      )
 
       described_class.new.perform(file: file, logger: Log.new.write('Testing...'))
     end
@@ -49,35 +46,19 @@ RSpec.describe KitImportJob do
       expect(NoteTemplate.find('evidence')).to_not be_nil
 
       # methodology template
-      expect(Methodology.find('OWASPv4_Testing_Methodology')).to_not be_nil
+      expect(Methodology.find('OWASPv4-methodology')).to_not be_nil
 
       # project template
-      expect(ProjectTemplate.find_template('dradis-template-welcome')).to_not be_nil
+      expect(Pathname.new(Configuration.paths_templates_projects) \
+        .join('OWASPv4-blank-project-template.xml').file?).to be true
 
       # report template files
-      word_template_properties = ReportTemplateProperties.find_by(template_file: 'dradis_welcome_template.v0.5.docm')
-      expect(word_template_properties).to_not be_nil
-      expect(ReportTemplateProperties.find_by(template_file: 'dradis_template-excel-simple.v1.3.xlsx')).to_not be_nil
-      expect(ReportTemplateProperties.find_by(template_file: 'html_welcome_report.html.erb')).to_not be_nil
-
-      # report template properties
-      expect(word_template_properties.content_blocks.keys).to match_array(['Conclusion', 'Scope'])
-      expect(word_template_properties.issue_fields.map(&:name)).to match_array(['Issue Field 1', 'Issue Field 2'])
-      expect(word_template_properties.evidence_fields.map(&:name)).to match_array(['Evidence Field 1', 'Evidence Field 2'])
-      expect(word_template_properties.document_properties).to match_array(['dradis.project', 'dradis.client'])
-
-      # rules engine
-      expect(Dradis::Pro::Rules::Rules::AndRule.find_by(name: 'Nessus N/A CVSS to 0.0')).to_not be_nil
-      expect(Dradis::Pro::Rules::Rules::AndRule.find_by(name: 'Nessus: Tag Critical')).to_not be_nil
-      expect(Dradis::Pro::Rules::Rules::AndRule.find_by(name: 'Nessus: Tag High')).to_not be_nil
-      expect(Dradis::Pro::Rules::Rules::AndRule.find_by(name: 'Nessus: Tag Medium')).to_not be_nil
-      expect(Dradis::Pro::Rules::Rules::AndRule.find_by(name: 'Nessus: Tag Low')).to_not be_nil
-      expect(Dradis::Pro::Rules::Rules::AndRule.find_by(name: 'Nessus: Tag Info')).to_not be_nil
+      expect(Pathname.new(Configuration.paths_templates_reports) \
+        .join('html_export').join('OWASPv4.v0.3.html.erb').file?).to be true
 
       # sample project
-      project = Project.find_by(name: 'dradis-export-welcome')
-      expect(project).to_not be_nil
-      expect(project.owners.first).to eq @user
+      project = Project.find(1)
+      expect(project.issues.count).to eq 8
     end
   end
 end
