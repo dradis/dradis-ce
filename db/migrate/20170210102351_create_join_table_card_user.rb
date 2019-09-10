8e5f6f06b67533287dc0d44dd9cd3c38d7658db1
Xavi Vila <xavivm@gmail.com>
2019-09-10 18:29:45 +0100

    Using 5.1 in another migration too

--- db/migrate/20170210102351_create_join_table_card_user.rb
+++ db/migrate/20170210102351_create_join_table_card_user.rb
@@ -1,4 +1,4 @@
-class CreateJoinTableCardUser < ActiveRecord::Migration[5.0]
+class CreateJoinTableCardUser < ActiveRecord::Migration[5.1]
   def change
     create_join_table :cards, :users do |t|
       t.index [:card_id, :user_id]
