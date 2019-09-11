8f61d443aade14c23e1b1c05e2f8d2e5470ec05a
Brian Pearce <brian.o.pearce@gmail.com>
2019-09-11 16:47:52 +0200

    Add new specs for boards

---
+++ spec/features/board_pages/board_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe 'Board pages:' do
+  subject { page }
+
+  it 'should require authenticated users' do
+    Configuration.create(name: 'admin:password', value: 'rspec_pass')
+    visit project_boards_path(create(:project))
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content('Access denied.')
+  end
+
+  context 'as authenticated user' do
+    before { login_to_project_as_user }
+
+    let(:board) do
+      create(:board, node: current_project.methodology_library)
+    end
+
+    describe 'when in index page' do
+
+      let(:boards_path) { project_boards_path(current_project) }
+      let(:board_path) { project_board_path(current_project, Board.last) }
+
+      include_examples 'managing boards'
+
+      it 'displays all boards' do
+        board
+        node = create(:node, project: current_project)
+        node_board = create(:board)
+
+        visit project_boards_path(current_project)
+
+        expect(page).to have_text board.name
+        expect(page).to have_text node_board.name
+      end
+    end
+  end
+end
