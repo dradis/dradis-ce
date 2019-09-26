a41d2cd234d47fe6a48e14e5d05266abf2181934
Brian Pearce <brian.o.pearce@gmail.com>
2019-09-26 15:21:07 +0200

    Refactor to utilize the same method

--- app/assets/javascripts/snowcrash/pages/nodes/new_form.js
+++ app/assets/javascripts/snowcrash/pages/nodes/new_form.js
@@ -1,43 +1,32 @@
 (function() {
-  document.addEventListener("turbolinks:load", function() {
-    if ($('[data-behavior~=multi-node]').length) {
-      $('[data-behavior~=multi-node]').click(function() {
-        var $multi = $('#nodes_list');
-        var labelVal = $('#node_label').val();
+  var copyOver = function($to, from, typeTo, typeFrom) {
+    if ($to.val() === '' && from !== '') {
+      $to.val(from.split('\n')[0] + '\n');
+      $(typeTo).val($(typeFrom).val());
 
-        if ($multi.val() === '' && labelVal !== '') {
-          $multi.val(labelVal + '\n');
-
-          $('#nodes_type_id').val($('#node_type_id').val());
-
-          // The click function we're in actually takes focus when clicked. If
-          // we called focus within the fucntion it gets cancelled out. We give
-          // it a brief timout to allow it to occur after this function ends,
-          // but seemingly at the same time.
-          setTimeout(function() {
-            $multi.focus();
-          }, 0);
-        }
-      });
+      // The click function we're in actually takes focus when clicked. If
+      // we called focus within the function it gets cancelled out. We give
+      // it a brief timout to allow it to occur after this function ends
+      // but seemingly at the same time.
+      setTimeout(function() {
+        $to.focus();
+      }, 0);
     }
+  }
 
-    if ($('[data-behavior~=one-node]').length) {
-      $('[data-behavior~=one-node]').click(function() {
-        var multiVal = $('#nodes_list').val();
+  document.addEventListener("turbolinks:load", function() {
+    if ($('[data-behavior~=copy-node-label]').length) {
+      $('[data-behavior~=copy-node-label]').click(function(eventData) {
+        var $multi = $('#nodes_list');
         var $label = $('#node_label');
 
-        if ($label.val() === '' && multiVal !== '') {
-          $label.val(multiVal.split('\n')[0]);
+        var multiVal = $multi.val();
+        var labelVal = $label.val();
 
-          $('#node_type_id').val($('#nodes_type_id').val());
-
-          // The click function we're in actually takes focus when clicked. If
-          // we called focus within the fucntion it gets cancelled out. We give
-          // it a brief timout to allow it to occur after this function ends,
-          // but seemingly at the same time.
-          setTimeout(function() {
-            $label.focus();
-          }, 0);
+        if ($(this).find('input').val() === 'one') {
+          copyOver($label, multiVal, '#node_type_id', '#nodes_type_id');
+        } else {
+          copyOver($multi, labelVal, '#nodes_type_id', '#node_type_id');
         }
       });
     }
