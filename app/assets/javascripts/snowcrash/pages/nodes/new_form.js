540fb488a2ff7db451f7b9b022b7e44c4a9cef7d
Brian Pearce <brian.o.pearce@gmail.com>
2019-09-26 15:58:38 +0200

    Pass vars not strings into the function

--- app/assets/javascripts/snowcrash/pages/nodes/new_form.js
+++ app/assets/javascripts/snowcrash/pages/nodes/new_form.js
@@ -1,8 +1,8 @@
 (function() {
-  var copyOver = function($to, from, typeTo, typeFrom) {
+  var copyOver = function($to, from, $typeTo, $typeFrom) {
     if ($to.val() === '' && from !== '') {
       $to.val(from.split('\n')[0] + '\n');
-      $(typeTo).val($(typeFrom).val());
+      $typeTo.val($typeFrom.val());
 
       // The click function we're in actually takes focus when clicked. If
       // we called focus within the function it gets cancelled out. We give
@@ -17,16 +17,19 @@
   document.addEventListener("turbolinks:load", function() {
     if ($('[data-behavior~=copy-node-label]').length) {
       $('[data-behavior~=copy-node-label]').click(function(eventData) {
-        var $multi = $('#nodes_list');
-        var $label = $('#node_label');
+        var $nodeType = $('#node_type_id'),
+            $nodesType = $('#nodes_type_id'),
 
-        var multiVal = $multi.val();
-        var labelVal = $label.val();
+            $multi = $('#nodes_list'),
+            $label = $('#node_label'),
+
+            multiVal = $multi.val(),
+            labelVal = $label.val();
 
         if ($(this).find('input').val() === 'one') {
-          copyOver($label, multiVal, '#node_type_id', '#nodes_type_id');
+          copyOver($label, multiVal, $nodeType, $nodesType);
         } else {
-          copyOver($multi, labelVal, '#nodes_type_id', '#node_type_id');
+          copyOver($multi, labelVal, $nodesType, $nodeType);
         }
       });
     }
