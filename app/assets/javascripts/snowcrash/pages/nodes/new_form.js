1652158e8dd10e93110485e021c83c209c8982ab
Brian Pearce <brian.o.pearce@gmail.com>
2019-09-27 15:10:44 +0200

    Move function inside load event for consistency

--- app/assets/javascripts/snowcrash/pages/nodes/new_form.js
+++ app/assets/javascripts/snowcrash/pages/nodes/new_form.js
@@ -1,20 +1,20 @@
 (function() {
-  var copyOver = function($to, fromVal, $typeTo, typeFromVal) {
-    if ($to.val() === '' && fromVal !== '') {
-      $to.val(fromVal.trim().split('\n')[0] + '\n');
-      $typeTo.val(typeFromVal);
-
-      // The click function we're in actually takes focus when clicked. If
-      // we called focus within the function it gets cancelled out. We give
-      // it a brief timout to allow it to occur after this function ends
-      // but seemingly at the same time.
-      setTimeout(function() {
-        $to.focus();
-      }, 0);
-    }
-  }
-
   document.addEventListener('turbolinks:load', function() {
+    var copyOver = function($to, fromVal, $typeTo, typeFromVal) {
+      if ($to.val() === '' && fromVal !== '') {
+        $to.val(fromVal.trim().split('\n')[0] + '\n');
+        $typeTo.val(typeFromVal);
+
+        // The click function we're in actually takes focus when clicked. If
+        // we called focus within the function it gets cancelled out. We give
+        // it a brief timout to allow it to occur after this function ends
+        // but seemingly at the same time.
+        setTimeout(function() {
+          $to.focus();
+        }, 0);
+      }
+    }
+
     if ($('[data-behavior~=copy-node-label]').length) {
       $('[data-behavior~=copy-node-label]').click(function() {
         var $modal = $(this).parents('.modal'),
