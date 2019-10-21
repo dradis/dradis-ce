cfccd31273334274f103cda5ed9513de4cd429f6
Brian Pearce <brian.pearce@protonmail.com>
2019-10-21 17:58:10 +0200

    Port UserPreferences to CE

---
+++ app/models/user_preferences.rb
@@ -0,0 +1,116 @@
+# This class provides a flexible way to store user preferences using
+# ActiveRecord's @serialize@ technique.
+#
+# This class is a bit of sugar on top of a standard `serialize :prefs, Hash`
+# call as we can inspect the attributes and add additional logic without
+# bloating the User model.
+#
+# The Class methods deal with ActiveRecord dump() and load(), and the Instance
+# methods deal with user preferences.
+#
+# See:
+#   http://viget.com/extend/how-i-used-activerecord-serialize-with-a-custom-data-type
+#   http://ruby-journal.com/how-to-write-custom-serializer-for-activerecord-number-serialize/
+
+class UserPreferences
+  class InvalidTourException < Exception; end
+
+  VALID_TOURS = [:first_sign_in, :projects_show]
+
+  # -- Class Methods ----------------------------------------------------------
+  # Used for `serialize` method in ActiveRecord
+  def self.dump(obj)
+    return if obj.nil?
+
+    unless obj.is_a?(self)
+      raise ::ActiveRecord::SerializationTypeMismatch,
+        "Attribute was supposed to be a #{self}, but was a #{obj.class}. -- #{obj.inspect}"
+    end
+
+    # YAML.dump(obj.attributes)
+    YAML.dump(obj)
+  end
+
+  def self.load(yaml)
+    return self.new if self != Object && yaml.nil?
+    return yaml unless yaml.is_a?(String) && yaml =~ /^---/
+
+    obj = YAML.load(yaml)
+
+    unless obj.is_a?(self) || obj.nil?
+      raise SerializationTypeMismatch,
+        "Attribute was supposed to be a #{self}, but was a #{obj.class}"
+    end
+
+    obj ||= self.new if self != Object
+
+    # self.new(obj)
+    obj
+  end
+
+
+  # -- Instance Methods -------------------------------------------------------
+
+  # Preferences:
+  #   :tours - This setting stores a dictionary of all the tours this user has
+  #            seen. Since we can have multiple versions of each tour we need
+  #            to be able to track what was the last version we presented to
+  #            them. See TourRegistry class.
+  attr_accessor :tours
+
+  def initialize(args={})
+    @tours = Hash.new { |hash, key| hash[key] = '0' }
+
+    args.each do |key, value|
+      if key.to_s =~ /\Atour_([\w_]*)\z/
+        @tours[$1.to_sym] = value
+      end
+    end
+  end
+
+  # ----------------------------------------------------------------- YAML.load
+  # This deals with YAML.load and legacy preferences
+  def init_with(coder)
+    coder.map.each do |key, value|
+      # Move the legacy :last_tour preference under the tour registry
+      # Also, the legacy preferences don't have a :tours attribute
+      if key == 'last_tour'
+        @tours = Hash.new { |hash, key| hash[key] = '0' }
+        @tours[:projects_show] = value
+      elsif key == 'tour' && value == {}
+        @tours = Hash.new { |hash, key| hash[key] = '0' }
+      else
+        instance_variable_set(:"@#{key}", value)
+      end
+    end
+  end
+
+
+  def method_missing(method_sym, *arguments, &block)
+    if method_sym.to_s =~ /\Alast_([\w_]*)=?\z/
+      method = $1
+      raise InvalidTourException.new('Tour not found!') unless VALID_TOURS.include?(method.to_sym)
+
+      if method_sym.to_s.ends_with?('=')
+        @tours[method.to_sym] = arguments.first
+      else
+        @tours[method.to_sym]
+      end
+    else
+      super
+    end
+  end
+
+  def respond_to?(method_sym, include_private = false)
+    if method_sym.to_s =~ /\Alast_([\w_]*)\z/
+      method = $1
+      if VALID_TOURS.include?(method.to_sym)
+        true
+      else
+        false
+      end
+    else
+      super
+    end
+  end
+end
---
+++ spec/models/user_preferences_spec.rb
@@ -0,0 +1,85 @@
+require 'rails_helper'
+
+describe UserPreferences do
+  it "provides an empty set of preferences with defaults if none have been initialized" do
+    subject.class::VALID_TOURS << :tour_rspec1
+
+    up1 = UserPreferences.new
+    expect(up1.last_tour_rspec1).to eq('0')
+
+    up1 = UserPreferences.new(tour_tour_rspec1: '1')
+    expect(up1.last_tour_rspec1).to eq('1')
+  end
+
+  # UPGRADE: this is the right place to test
+  context "loading from YAML" do
+    # context "valid tour name" do
+    #   it "returns 0 for a fresh set of preferences for a valid tour" do
+    #     subject.class::VALID_TOURS << :tour_rspec4
+    #
+    #     preferences = subject.class.load "--- !ruby/object:UserPreferences\ntours: {}\n"
+    #
+    #     expect do
+    #       preferences.last_tour_rspec4
+    #     end.not_to raise_error
+    #
+    #     expect(preferences.last_tour_rspec4).to eq('0')
+    #   end
+    #
+    #   it "returns the last tour version of XXX type that was visited for a valid tour" do
+    #     subject.class::VALID_TOURS << :tour_rspec5
+    #
+    #     preferences = subject.class.load "--- !ruby/object:UserPreferences\ntours:\n :tour_rspec5: 2.0.0\n"
+    #
+    #     expect do
+    #       preferences.last_tour_rspec5
+    #     end.not_to raise_error
+    #
+    #     expect(preferences.last_tour_rspec5).to eq('2.0.0')
+    #   end
+    # end
+  end
+
+
+  context "#last_tour_XXX" do
+    context "invalid tour name" do
+      it "raises an exception if the tour name isn't valid" do
+        expect do
+          subject.last_tour_rspec2
+        end.to raise_error(UserPreferences::InvalidTourException)
+      end
+    end
+
+    context "valid tour name" do
+      it "returns 0 for a fresh set of preferences for a valid tour" do
+        subject.class::VALID_TOURS << :tour_rspec3
+        expect do
+          subject.last_tour_rspec3
+        end.not_to raise_error
+        expect(subject.last_tour_rspec3).to eq('0')
+      end
+
+      it "returns the last tour version of XXX type that was visited for a valid tour" do
+        subject.class::VALID_TOURS << :tour_rspec3
+        subject.tours[:tour_rspec3] = '1'
+
+        expect do
+          subject.last_tour_rspec3
+        end.not_to raise_error
+        expect(subject.last_tour_rspec3).to eq('1')
+      end
+    end
+  end
+
+  context "#last_tour_XXX=" do
+    it "sets the new tour value" do
+      subject.class::VALID_TOURS << :tour_rspec4
+
+      expect do
+        subject.last_tour_rspec4 = '2'
+      end.not_to raise_error
+
+      expect(subject.last_tour_rspec4).to eq('2')
+    end
+  end
+end
