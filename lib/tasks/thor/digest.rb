e2b1cb9c02b31aabe850fcd7c5dd8cc05bd66c5c
Aaron Manaloto <aaronpomanaloto@gmail.com>
2019-10-21 18:23:38 +0800

    Call digestmailer in thor

--- lib/tasks/thor/digest.rb
+++ lib/tasks/thor/digest.rb
@@ -3,13 +3,15 @@ class DradisTasks < Thor
     namespace     'dradis:digests'
 
     desc 'send', 'Send a daily digest to all users'
-    def send
+    def send_digests
       print '** Sending digest to all users...'
+      DigestMailer.send_digests
     end
 
     desc 'send_instant', 'Send an instant digest to all users'
-    def send_instant
+    def send_instants
       print '** Sending an instant digest to all users...'
+      DigestMailer.send_instants
     end
   end
 end
