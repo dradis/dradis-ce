4b807344c49dee2f054b064a5bdce5712be03657
Aaron Manaloto <aaronpomanaloto@gmail.com>
2019-10-21 18:05:14 +0800

    Add DigestMailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,39 +1,18 @@
 class NotificationMailer < ActionMailer::Base
-  INSTANT_DIGEST_INTERVAL = 10.minutes
-
   default from: 'email@securityroots.com'
   default to: 'admin@securityroots.com'
 
-  def digest(user:)
+  def digest(user:, notifications:)
     @user = user
-    @notifications = digest_notifications
-
-    @user.last_digest_email = Time.now
-    @user.save
+    @notifications = notifications
 
     mail to: user.email, subject: 'Digest Email'
   end
 
-  def instant(user:)
+  def instant(user:, notifications:)
     @user = user
-    @notifications = instant_notifications
+    @notifications = notifications
 
     mail to: user.email, subject: 'Notifications in the last 10 minutes.'
-  end
-
-  private
-
-  def digest_notifications
-    @user.notifications.
-      where('created_at >= ?', @user.last_digest_email).
-      unread.
-      newest
-  end
-
-  def instant_notifications
-    @user.notifications.
-      where('created_at >= ?', Time.now - INSTANT_DIGEST_INTERVAL).
-      unread.
-      newest
   end
 end
---
+++ app/services/digest_mailer.rb
@@ -0,0 +1,47 @@
+class DigestMailer
+  DIGEST_INTERVAL   = 1.day
+  INSTANT_INTERVAL  = 10.minutes
+
+  def self.send_digests
+    digest_users = User.includes(:notifications).all
+    digest_users.each do |user|
+      DigestMailer.new(user: user, type: :digest).send
+    end
+  end
+
+  def self.send_instants
+    instant_users = User.includes(:notifications).all
+    instant_users.each do |user|
+      DigestMailer.new(user: user, type: :instant).send
+    end
+  end
+
+  def initialize(type:, user:)
+    @type = type
+    @user = user
+  end
+
+  attr_accessor :type, :user
+
+  def send
+    notifications = user.notifications.
+      where('created_at >= ?', Time.now - interval).
+      unread.
+      newest
+
+    NotificationMailer.with(user: user, notifications: notifications).instant
+  end
+
+
+  private
+
+  def interval
+    if type == :digest
+      DIGEST_INTERVAL
+    elsif type == :instant
+      INSTANT_INTERVAL
+    else
+      raise 'Invalid digest type'
+    end
+  end
+end
