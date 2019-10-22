1b603ba6f389973c5088fed232ccedbb30da1816
Aaron Manaloto <aaronpomanaloto@gmail.com>
2019-10-22 19:28:38 +0800

    Add defaults in the smtp config

--- config/initializers/z_03_setup_mail.rb
+++ config/initializers/z_03_setup_mail.rb
@@ -1,9 +1,12 @@
+# Defaults
+ActionMailer::Base.default_url_options[:host] = 'dradis-pro.dev'
+ActionMailer::Base.default_url_options[:script_name] = '' if Rails.env.production?
+
 config_path = Rails.root.join('config/smtp.yml')
 
 if File.exist?(config_path)
   config = YAML.load_file(config_path)[Rails.env]
 
   ActionMailer::Base.smtp_settings = config['smtp_settings']
-  ActionMailer::Base.default_url_options = config['default_url_options']
-  ActionMailer::Base.default_url_options[:script_name] = '' if Rails.env.production?
+  ActionMailer::Base.default_url_options = config['default_url_options'].symbolize_keys
 end
