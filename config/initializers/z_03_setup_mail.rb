753a603ce9ffcbd061cf1c2804842d9f7f0116bd
Aaron Manaloto <aaronpomanaloto@gmail.com>
2019-10-22 13:48:43 +0800

    Set smtp as optional

--- config/initializers/z_03_setup_mail.rb
+++ config/initializers/z_03_setup_mail.rb
@@ -1,5 +1,9 @@
-config = YAML.load_file(Rails.root.join('config/smtp.yml'))[Rails.env]
+config_path = Rails.root.join('config/smtp.yml')
 
-ActionMailer::Base.smtp_settings = config['smtp_settings']
-ActionMailer::Base.default_url_options = config['default_url_options']
-ActionMailer::Base.default_url_options[:script_name] = '' if Rails.env.production?
+if File.exist?(config_path)
+  config = YAML.load_file(config_path)[Rails.env]
+
+  ActionMailer::Base.smtp_settings = config['smtp_settings']
+  ActionMailer::Base.default_url_options = config['default_url_options']
+  ActionMailer::Base.default_url_options[:script_name] = '' if Rails.env.production?
+end
