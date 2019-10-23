d11988e75b3d7699c02281d7ce84b11aa418c600
Matt Budz <matt.budziosz@gmail.com>
2019-10-23 13:07:31 +0700

    remove old notification_mailer_preview.rb

--- test/mailers/preview/notification_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-class NotificationMailerPreview < ActionMailer::Preview
-  def instant
-    NotificationMailer.instant(User.first)
-  end
-
-  def digest
-    NotificationMailer.digest(User.first)
-  end
-end
