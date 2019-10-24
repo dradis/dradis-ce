88bf17082e4328c4ab679599d2c16c7fba9aa25f
Brian Pearce <brian.pearce@protonmail.com>
2019-10-24 10:07:15 +0200

    Wrap the code in a closure to prevent leakage

--- app/assets/javascripts/snowcrash/modules/mentions.js
+++ app/assets/javascripts/snowcrash/modules/mentions.js
@@ -1,18 +1,20 @@
-window.Mentions = {
-  init: function(elements) {
-    tribute = new Tribute({
-      allowSpaces: function() { return false },
-      menuItemTemplate: function(item) {
-        return '<img src="' + item.original.avatar_url + '" width="24px" height="24px" > ' + item.string
-      },
-      noMatchTemplate: function() { return '' },
-      values: JSON.parse($('meta[name=mentionable-users]').attr('content'))
-    });
+(function(window) {
+  window.Mentions = {
+    init: function(elements) {
+      tribute = new Tribute({
+        allowSpaces: function() { return false },
+        menuItemTemplate: function(item) {
+          return '<img src="' + item.original.avatar_url + '" width="24px" height="24px" > ' + item.string
+        },
+        noMatchTemplate: function() { return '' },
+        values: JSON.parse($('meta[name=mentionable-users]').attr('content'))
+      });
 
-    $('.main-content').scroll(function(){
-      tribute.hideMenu();
-    });
+      $('.main-content').scroll(function(){
+        tribute.hideMenu();
+      });
 
-    tribute.attach(elements);
+      tribute.attach(elements);
+    }
   }
-}
+})(window);
