87d33385516fb456764e7d6f7819829ce95bf500
Matt Budz <matt.budziosz@gmail.com>
2019-10-24 15:30:43 +0700

    consolidate instant/daily emails into one partial.

--- spec/mailers/previews/notification_mailer_preview.rb
+++ spec/mailers/previews/notification_mailer_preview.rb
@@ -1,8 +1,4 @@
 class NotificationMailerPreview < ActionMailer::Preview
-  def instant
-    NotificationMailer.with(user: User.first).instant
-  end
-
   def digest
     NotificationMailer.with(user: User.first).digest
   end
