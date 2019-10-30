0627ca67a67ef25a39fd5ae2ac6d3a9a2c96f32e
Matt Budz <matt.budziosz@gmail.com>
2019-10-30 17:29:01 +0700

    change strong tag to span tag in presenter

--- app/presenters/digest_presenter.rb
+++ app/presenters/digest_presenter.rb
@@ -59,7 +59,7 @@ class DigestPresenter < NotificationPresenter
   def linked_email
     if notifications.count > 1
       if notification.actor
-        h.content_tag :strong, notification.actor.email, class: 'user-name'
+        h.content_tag :span, notification.actor.email, class: 'user-name'
       else
         'a user who has since been deleted'
       end
