7ce2b41e843468f75b1fde9e91f2c29895222e70
Brian Pearce <brian.pearce@protonmail.com>
2019-11-15 17:45:28 +0100

    Consistently use default profile icon
    Set the default icon as a constant to use everywhere.
    Force gravatar to error if the user doesn't have a gravatar. That way we can use our own fallback.
    Return image paths early when no user is present.
    Return early for default avatars when no user is present.

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,31 +1,53 @@
+# frozen_string_literal: true
+
 module AvatarHelper
+  DEFAULT_PROFILE_IMAGE = ActionController::Base.helpers.image_path('profile')
+
+  # Gravatar will use a default image if one is not found. Having gravatar serve
+  # the default image is not desired. Instead force an error by using a bad
+  # default url and let our fallback image code take effect.
   def avatar_url(user, options = {})
-    if user.nil?
-      image_path('profile.png')
-    else
-      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      size = options.fetch(:size, 64).to_i
-      "https://secure.gravatar.com/avatar/#{gravatar_id}.png?r=PG&s=#{size}"
+    return DEFAULT_PROFILE_IMAGE if user.nil?
+
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    size = options.fetch(:size, 80).to_i
+    "https://secure.gravatar.com/avatar/#{gravatar_id}.png?r=PG&s=#{size}&d=forceErrorOnDefault"
+  end
+
+  def nil_user_avatar(klass, size)
+    removed_msg = 'This user has been deleted from the system'
+
+    content_tag :span, class: klass do
+      image_tag(
+        DEFAULT_PROFILE_IMAGE,
+        alt: removed_msg,
+        title: removed_msg,
+        width: size
+      )
     end
   end
 
   def avatar_image(user, options = {})
-    alt            = options.fetch(:alt, "#{user.email}'s avatar")
-    fallback_image = options.fetch(:fallback_image, image_path('logo_small.png'))
-    include_name   = options.fetch(:include_name, false)
-    size           = options.fetch(:size, 73)
-    title          = options.fetch(:title, user.email)
     klass          = options.fetch(:class, 'gravatar')
+    size           = options.fetch(:size, 73)
+
+    return nil_user_avatar(klass, size) if user.nil?
+
+    alt            = options.fetch(:alt, "#{user.email}'s avatar")
+    fallback_image = options.fetch(:fallback_image, DEFAULT_PROFILE_IMAGE)
+    gravatar_size  = options.fetch(:gravatar_size, 80)
+    include_name   = options.fetch(:include_name, false)
+    title          = options.fetch(:title, user.email)
 
     content_tag :span, class: klass do
       image_tag(
-        avatar_url(user, size: size),
+        avatar_url(user, size: gravatar_size),
         alt: alt,
         data: { fallback_image: fallback_image },
-        title: title,
         height: size,
-        width: size,
-        style: "width: #{size}px; height: #{size}px"
+        style: "width: #{size}px; height: #{size}px",
+        title: title,
+        width: size
       ) + (include_name ? ' ' + user.try(:name) : '')
     end
   end
