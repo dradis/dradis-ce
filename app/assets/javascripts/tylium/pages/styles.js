581bc1f8c5935565dec3e77c28f7bb52c5729430
Matt Budz <matt.budziosz@gmail.com>
2019-11-18 15:25:52 +0700

    style guide update

--- app/assets/javascripts/tylium/pages/styles.js
+++ app/assets/javascripts/tylium/pages/styles.js
@@ -1,7 +1,9 @@
 document.addEventListener('turbolinks:load', function(){
   if ($('body.styles_tylium')) {
-    $('[data-behavior~=reveal-code]').on('click', function() {
-      $(this).closest('[data-behavior~=content-container').find('[data-behavior~=style-code]').toggleClass('d-none');
+
+    $('[data-behavior~=quick-nav-btn]').on('click', function() {
+      let target = $(this).data('target');
+      $('[data-id~=' + target + ']')[0].scrollIntoView({behavior: "smooth"});
     })
   }
 });
