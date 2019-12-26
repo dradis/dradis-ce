acd1b6f706814d86da1302a9993d3fd5629b42f1
Xavi Vila <xavivm@gmail.com>
2019-12-26 20:58:24 +0100

    require 'resque/server'

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+require 'resque/server'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
