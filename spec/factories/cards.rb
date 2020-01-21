48c2ea1bae33d1c85f0b0f19379aa51abac7af57
Brian Pearce <brian.o.pearce@gmail.com>
2020-01-21 17:45:15 +0100

    Fix deprecation warnings

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :card do
     sequence(:name){ |n| "Card-#{n}" }
     sequence(:description){ |n| "Card-#{n} Description" }
-    due_date 1.week.from_now
+    due_date { 1.week.from_now }
     association :list
   end
 end
