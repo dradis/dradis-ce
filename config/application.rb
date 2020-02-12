14f67c0186f963be464f35b42d3c44c9998a82cb
Brian Pearce <brian.o.pearce@gmail.com>
2020-02-12 14:16:10 +0100

    Remove activestorage
    Our routes conflict with active storage, and to fix it properly we'll break engine routing. We don't need it right now so let's leave it out.

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require_relative 'boot'
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
