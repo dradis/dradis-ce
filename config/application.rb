3c09af535b3e4095896676226a0664affc35aa73
Liam Elliott <liamelliott1123@gmail.com>
2020-02-19 17:59:27 -0700

    Add Lib to AutoLoad Path

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Dradis
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
 
