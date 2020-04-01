87c792c366c9e4de22769955e62610e800a1ba31
Matt Budz <matt.budziosz@gmail.com>
2020-04-01 20:21:01 +0700

    inject markdown differently for firefox

--- app/assets/javascripts/shared/editor_toolbar.js
+++ app/assets/javascripts/shared/editor_toolbar.js
@@ -192,7 +192,7 @@ class EditorToolbar {
   injectMarkdown($element, prefix, suffix, placeholder) {
     var adjustedPrefixLength = prefix.length, adjustedSuffixLength = suffix.length;
     var startIndex = $element[0].selectionStart, endIndex = $element[0].selectionEnd;
-    var selectedText = $element.val().substring(startIndex, endIndex).split('\n');
+    var elementText = $element.val(), selectedText = $element.val().substring(startIndex, endIndex).split('\n');
     var markdownText = '';
 
     // create string to inject with markdown added
@@ -214,7 +214,12 @@ class EditorToolbar {
 
     // remove the original selection (if there was one) and add new markdown string in it's place
     $element.focus(); // bring focus back to $element from the toolbar
-    document.execCommand("insertText", false, markdownText);
+    if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) { // firefox
+      $element.val(elementText.slice(0, startIndex) + markdownText + elementText.slice(endIndex));
+    }
+    else { // all other browsers
+      document.execCommand("insertText", false, markdownText);
+    }
     
     // post-injection cursor location
     if (startIndex == endIndex) { // no text was selected, select injected placeholder text
