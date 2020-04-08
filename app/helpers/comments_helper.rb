759fabb156ba3424fc2ccedc3a9a52e3cb881347
Brian Pearce <brian.o.pearce@gmail.com>
2020-04-08 09:25:36 +0200

    Use our own textile formatter to be consistent

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -4,7 +4,7 @@ module CommentsHelper
   def comment_formatter(comment)
     @comment_pipeline ||= HTML::Pipeline.new [
       HTML::Pipeline::DradisFieldableFilter,
-      HTML::Pipeline::TextileFilter,
+      HTML::Pipeline::DradisTextileFilter,
       HTML::Pipeline::SanitizationFilter,
       HTML::Pipeline::AutolinkFilter,
       HTML::Pipeline::DradisMentionsFilter,
