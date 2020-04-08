89fbcd9cd2ba70535ea041667c398b396ec0cd70
Aaron Manaloto <aaronpomanaloto@gmail.com>
2020-04-08 15:32:04 +0800

    Use read_attribute instead

--- db/migrate/20200407061006_add_user_id_to_activities.rb
+++ db/migrate/20200407061006_add_user_id_to_activities.rb
@@ -4,7 +4,7 @@ class AddUserIdToActivities < ActiveRecord::Migration[5.2]
 
     Activity.transaction do
       Activity.all.each do |activity|
-        user = User.find_by_email(activity.user)
+        user = User.find_by_email(activity.read_attribute(:user))
         activity.update_attribute :user_id, user.id if user
       end
     end
