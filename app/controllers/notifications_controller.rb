b9844696197b353e81261fcde89cfd4166387094
Sean Yeoh <sean.yeoh@hotmail.com>
2020-04-10 22:31:32 +0800

    Remove eager loading since Card model does not have commentable and user associations.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,7 @@ class NotificationsController < AuthenticatedController
 
   def index
     notifications = current_user.notifications.newest.includes(
-      :actor, :notifiable
+      :actor, notifiable: [:user, :commentable]
     )
     respond_to do |format|
       format.html do
