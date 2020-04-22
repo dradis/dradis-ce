673ee4e346ca6f312187726c7d3d5b0e5c0108a7
Sean Yeoh <sean.yeoh@hotmail.com>
2020-04-22 15:16:13 +0800

    Finish local auto save

--- app/assets/javascripts/tylium/modules/local_auto_save.js
+++ app/assets/javascripts/tylium/modules/local_auto_save.js
@@ -11,52 +11,45 @@ document.addEventListener( "turbolinks:load", function(){
     }
 
     if (data !== null) {
-    //   if (item.value === "") {
-    //     item.value = data;
-    //     $(item).trigger('load-preview');
-    //   } else {
-    //     var response = confirm("There is data in your browser's cache for this form, but the field already contains some data. Would you like to overwrite the contents of the form with the saved data from the browser's cache?")
-    //     if (response) {
-    //       item.value = data;
-    //       $(item).trigger('load-preview');
-    //     }
-    //   }
+      for (let [key, value] of Object.entries(data)) {
+        form.querySelector(`[name='${key}']`).value = value
+      }
     } else {
       console.log("No data in localStorage for " + key);
     }
 
     // List of available inputs: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
+    // Exclude these inputs so that it does not store unnecessary data in local cache
     var excludedInputTypes = ["button", "file", "hidden", "image", "password", "reset", "submit"]
 
     // Find all inputs and textareas of form, then exclude base on excluded input types
-    Array.from(form.querySelectorAll("input, textarea")).filter(function(input) {
+    var formInputs = Array.from(form.querySelectorAll("input, textarea")).filter(function(input) {
       return !excludedInputTypes.includes(input.getAttribute("type"))
-    }).forEach(function(input) {
-      console.log(input)
-      $(input).on("textchange", function(event, previousText) {
-        console.log(event.currentTarget.value)
-      })
     })
 
     var timer;
-    // we're using a jQuery plugin for :textchange event, so need to use $()
 
-    // $(item).on("textchange", function(event, previousText) {
-    //   clearTimeout(timer)
+    formInputs.forEach(function(input) {
+      // we're using a jQuery plugin for :textchange event, so need to use $()
 
-    //   timer = setTimeout(function(){
-    //     if (typeof Storage !== "undefined" && Storage !== null) {
-    //       localStorage.setItem(key, JSON.stringify(event.currentTarget.value));
-    //     } else {
-    //       console.log("The browser doesn't support local storage of settings.");
-    //     }
-    //   }, 1000);
-    // });
+      $(input).on("textchange", function(event, previousText) {
+        timer = setTimeout(function(){
+          if (typeof Storage !== "undefined" && Storage !== null) {
+            localStorage.setItem(key, JSON.stringify(getData(formInputs)));
+          } else {
+            console.log("The browser doesn't support local storage of settings.");
+          }
+        }, 1000);
+      })
+    })
 
-    // item.form.addEventListener("submit", function(event){
-    //   if (typeof Storage !== "undefined" && Storage !== null) {
-    //     localStorage.removeItem(key);
-    //   }
-    // });
+    function getData(formInputs) {
+      var reducer = function(hash, input) {
+        hash[input.name] = input.value;
+        return hash
+      }
+
+      return formInputs.reduce(reducer, {})
+    }
   });
 });
