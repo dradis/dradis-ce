476147d6839529ea6cde158363f0b264d2f7c689
Matt Budz <matt.budziosz@gmail.com>
2020-04-23 14:07:52 +0700

    update js functionality for submit button

--- app/assets/javascripts/tylium/modules/state_menu.js
+++ app/assets/javascripts/tylium/modules/state_menu.js
@@ -1,41 +1,43 @@
 document.addEventListener( "turbolinks:load", function(){
-  if ($('[data-behavior~=state-menu]').length) {
-    var btnText = '',
-        $submitBtn = $('[data-behavior~=state-submit-button]');
 
-    if ($('body.new').length) {
-      btnText = 'Create Draft Issue';
-      $('[data-state~=draft]').addClass('selected');
-    }
-    else {
-      // This will need to be updated to use the current state of the issue on load.
-      btnText = 'Update Draft Issue';
-    }
+  function setBtnText(prefix, state) {
+    var btnText, $submitBtn = $('[data-behavior~=state-submit-button]');
+    var verboseState = ((state == 'review') ? 'ready for review' : state);
+
+    $('[data-state]').each(function() {
+      $(this).removeClass('selected');
+    });
+    
+    $('[data-state~=' + state + ']').addClass('selected');
+
+    btnText = prefix + ' ' + verboseState + ' Issue';
 
     $submitBtn.prop('value', btnText);
+  }
+
+  if ($('[data-behavior~=state-menu]').length) {
+    var textPrefix, currentState;
+
+    // once we have states implemented this if block can be streamlined
+    if ($('body.new').length) {
+      textPrefix = 'create';
+      currentState = 'draft';
+      setBtnText(textPrefix, currentState);
+    }
+    else {
+      textPrefix = 'update';
+      currentState = 'draft'; // This will need to be updated to use the current state of the issue on load.
+      setBtnText(textPrefix, currentState);
+    }
+
+    $('[data-state-header]').each(function() {
+      var headerText = $(this).text();
+      $(this).text(textPrefix + headerText);
+    });
 
     $('[data-state]').click(function () {
       var selectedState = $(this).data('state');
-
-      $('[data-state]').each(function() {
-        $(this).removeClass('selected');
-      });
-      
-      $(this).addClass('selected');
-          
-      switch (selectedState) {
-        case 'draft':
-          btnText = 'Create Draft Issue';
-          break;
-        case 'review':
-          btnText = 'Create Ready for Review Issue';
-          break;
-        case 'published':
-          btnText = 'Create Published Issue';
-          break;
-      }
-
-      $submitBtn.prop('value', btnText);
+      setBtnText(textPrefix, selectedState);
     });
   }
 });
