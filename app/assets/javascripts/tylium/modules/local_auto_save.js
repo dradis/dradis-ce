284cf44e7355b1d82738e982195fd7af7885a2fe
Sean Yeoh <sean.yeoh@hotmail.com>
2020-04-23 16:20:29 +0800

    Evidence local auto save.

--- app/assets/javascripts/tylium/modules/local_auto_save.js
+++ app/assets/javascripts/tylium/modules/local_auto_save.js
@@ -1,6 +1,6 @@
 document.addEventListener("turbolinks:load", function() {
 
-  document.querySelectorAll("[data-behavior~=local-auto-save").forEach(function(form) {
+  document.querySelectorAll("[data-behavior~=local-auto-save]").forEach(function(form) {
     var key  = form.dataset.autoSaveKey;
     var data = "";
 
@@ -12,7 +12,9 @@ document.addEventListener("turbolinks:load", function() {
 
     if (data !== null) {
       for (let [key, value] of Object.entries(data)) {
-        form.querySelector(`[name='${key}']`).value = value
+        var $input = $(form).find(`[name='${key}']`)
+        $input.val(value)
+        $input.trigger('load-preview')
       }
     } else {
       console.log("No data in localStorage for " + key);
@@ -23,7 +25,7 @@ document.addEventListener("turbolinks:load", function() {
     var excludedInputTypes = ["button", "file", "hidden", "image", "password", "reset", "submit"]
 
     // Find all inputs and textareas of form, then exclude base on excluded input types
-    var formInputs = Array.from(form.querySelectorAll("input, textarea")).filter(function(input) {
+    var formInputs = Array.from(form.querySelectorAll("input, textarea, select")).filter(function(input) {
       return !excludedInputTypes.includes(input.getAttribute("type"))
     })
 
@@ -56,6 +58,14 @@ document.addEventListener("turbolinks:load", function() {
       if (typeof Storage !== "undefined" && Storage !== null) {
         localStorage.removeItem(key);
       }
+    });
+
+    document.querySelectorAll("[data-behavior~=clear-local-auto-save]").forEach(function(element) {
+      element.addEventListener("click", function(event) {
+        if (typeof Storage !== "undefined" && Storage !== null) {
+          localStorage.removeItem(key);
+        }
+      })
     });
   });
 });
