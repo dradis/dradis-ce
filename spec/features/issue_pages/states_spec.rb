6fcc7ca97644ee1f20de7d597b7de20067b55b60
Aaron Manaloto <aaronpomanaloto@gmail.com>
2020-04-24 18:06:59 +0800

    Add initial multiple state update spec

---
+++ spec/features/issue_pages/states_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'issue pages' do
+  describe 'states', js: true do
+    before do
+      login_to_project_as_user
+
+      # create 2 issues
+      @issue1 = create(:issue, node: current_project.issue_library, state: 0)
+      @issue2 = create(:issue, node: current_project.issue_library, state: 0)
+
+      visit project_issues_path(current_project)
+
+      # click > 1 issue checkboxes
+      page.all('input.js-multicheck').each(&:click)
+
+      find('#state-selected').click
+    end
+
+    it 'updates the state of the issues' do
+      find('[data-state="2"]').click
+
+      # Replace with loading check once implemented
+      # wait for ajax
+      sleep(1)
+
+      expect(@issue1.reload.state).to eq(2)
+      expect(@issue2.reload.state).to eq(2)
+    end
+  end
+end
