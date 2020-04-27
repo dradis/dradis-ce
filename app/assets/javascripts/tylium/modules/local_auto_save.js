bba6d3d4c1e1c5f604b6a32283fd02a39932382f
Sean Yeoh <sean.yeoh@hotmail.com>
2020-04-27 20:11:27 +0800

    Change double quotes to single quotes in local_auto_save.js. Add debounce function instead of settimeout

--- app/assets/javascripts/tylium/modules/local_auto_save.js
+++ app/assets/javascripts/tylium/modules/local_auto_save.js
@@ -1,54 +1,55 @@
-document.addEventListener("turbolinks:load", function() {
-  document.querySelectorAll("[data-behavior~=local-auto-save]").forEach(function(form) {
+document.addEventListener('turbolinks:load', function() {
+  document.querySelectorAll('[data-behavior~=local-auto-save]').forEach(function(form) {
     var key  = form.dataset.autoSaveKey;
     var data = JSON.parse(localStorage.getItem(key));
 
     if (data !== null) {
       for (let [key, value] of Object.entries(data)) {
-        if (key.slice(-2) == "[]") {
+        if (key.slice(-2) == '[]') {
           value.forEach(function(checkboxValue) {
-            var $input = $(form).find(`[name="${key}"][value="${checkboxValue}"]`);
-            $input.prop("checked", true);
+            var $input = $(form).find(`[name='${key}'][value='${checkboxValue}']`);
+            $input.prop('checked', true);
           })
         } else {
-          var $input = $(form).find(`[name="${key}"]`)
+          var $input = $(form).find(`[name='${key}']`)
+          console.log(value)
           $input.val(value);
-          $input.trigger("load-preview");
+          $input.trigger('load-preview');
         }
       }
     } else {
-      console.log("No data in localStorage for " + key);
+      console.log('No data in localStorage for ' + key);
     }
 
     // List of available inputs: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
     // Exclude these inputs so that it does not store unnecessary data in local cache
-    var excludedInputTypes = ["button", "file", "image", "password", "reset", "submit"];
+    var excludedInputTypes = ['button', 'file', 'image', 'password', 'reset', 'submit'];
 
     // Don't store authenticity_token and utf8
-    var excludedHiddenInputNames = ["utf8", "authenticity_token"];
+    var excludedHiddenInputNames = ['utf8', 'authenticity_token'];
 
     // Find all inputs and textareas of form, then exclude base on excluded input types
-    var formInputs = Array.from(form.querySelectorAll("input, textarea, select")).filter(function(input) {
-      return !excludedInputTypes.includes(input.getAttribute("type")) && !excludedHiddenInputNames.includes(input.name);
+    var formInputs = Array.from(form.querySelectorAll('input, textarea, select')).filter(function(input) {
+      return !excludedInputTypes.includes(input.getAttribute('type')) && !excludedHiddenInputNames.includes(input.name);
     })
 
     var timer;
 
+    var setData = debounce(function() {
+      console.log(JSON.stringify(getData(formInputs)))
+      localStorage.setItem(key, JSON.stringify(getData(formInputs)));
+    }, 500);
+
     formInputs.forEach(function(input) {
       // we're using a jQuery plugin for :textchange event, so need to use $()
-
-      $(input).on("textchange change", function(event, previousText) {
-        timer = setTimeout(function() {
-          localStorage.setItem(key, JSON.stringify(getData(formInputs)));
-        }, 1000);
-      })
+      $(input).on('textchange change', storeData);
     })
 
     function getData(formInputs) {
       var hashBuilder = function(hash, input) {
         // Check if name is an array, i.e. checkboxes
-        if (input.name.slice(-2) == "[]") {
-          hash[input.name] = Array.from(form.querySelectorAll(`[name="${input.name}"]:checked`)).map(function(input) {
+        if (input.name.slice(-2) == '[]') {
+          hash[input.name] = Array.from(form.querySelectorAll(`[name='${input.name}']:checked`)).map(function(input) {
             return input.value;
           });
         } else {
@@ -61,12 +62,27 @@ document.addEventListener("turbolinks:load", function() {
       return formInputs.reduce(hashBuilder, {});
     }
 
-    form.addEventListener("submit", function(event) {
+    function debounce(func, wait, immediate) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    };
+
+    form.addEventListener('submit', function(event) {
       localStorage.removeItem(key);
     });
 
-    document.querySelectorAll("[data-behavior~=clear-local-auto-save]").forEach(function(element) {
-      element.addEventListener("click", function(event) {
+    document.querySelectorAll('[data-behavior~=clear-local-auto-save]').forEach(function(element) {
+      element.addEventListener('click', function(event) {
         localStorage.removeItem(key);
       })
     });
