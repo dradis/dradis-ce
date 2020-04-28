705616e162aa7859b028f282bb83b5cde8814b74
Sean Yeoh <sean.yeoh@hotmail.com>
2020-04-28 14:18:33 +0800

    Add semicolons

--- app/assets/javascripts/tylium/modules/issues/local_tag_auto_save.js
+++ app/assets/javascripts/tylium/modules/issues/local_tag_auto_save.js
@@ -1,19 +1,20 @@
 // This module restores the hidden tag input and ensure the selected tag
 // is shown correctly in in issues#form
 document.addEventListener('turbolinks:load', function(){
-  $form = $('#issues_editor .js-taglink').parents('form[data-behavior~=local-auto-save]')
+  $form = $('#issues_editor .js-taglink').parents('form[data-behavior~=local-auto-save]');
+
   if ($form.length) {
     var key = $form.data('autoSaveKey');
     var data = JSON.parse(localStorage.getItem(key));
 
     if (data != null && data['issue[tag_list]'].length) {
-      var $target = $(`#issues_editor .js-taglink[data-tag='${data['issue[tag_list]']}']`)
+      var $target = $(`#issues_editor .js-taglink[data-tag='${data['issue[tag_list]']}']`);
 
       if ($target.length) {
-        $('#issue_tag_list').val(data["issue[tag_list]"])
-        $span = $('#issues_editor .dropdown-toggle span.tag')
-        $span.html($target.html())
-        $span.css("color", $target.css("color"))
+        $('#issue_tag_list').val(data["issue[tag_list]"]);
+        $span = $('#issues_editor .dropdown-toggle span.tag');
+        $span.html($target.html());
+        $span.css("color", $target.css("color"));
       }
     }
   }
