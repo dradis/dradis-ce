808b2a74f00b1897ac9bf8e3251bf769435926c0
Aaron Manaloto <aaronpomanaloto@gmail.com>
2020-05-07 16:12:25 +0800

    Undo controller validation

--- engines/dradis-api/app/controllers/dradis/ce/api/v1/issues_controller.rb
+++ engines/dradis-api/app/controllers/dradis/ce/api/v1/issues_controller.rb
@@ -13,13 +13,12 @@ module Dradis::CE::API
       end
 
       def create
-        @issue = current_project.issues.new
+        @issue = current_project.issues.new(issue_params)
         @issue.author   = current_user.email
         @issue.category = Category.issue
         @issue.node     = current_project.issue_library
 
-        if is_valid_state? &&
-            (@issue.assign_attributes(issue_params) || @issue.valid?)
+        if @issue.valid?
           @issue.save
           track_created(@issue)
           @issue.tag_from_field_content!
@@ -32,7 +31,7 @@ module Dradis::CE::API
       def update
         @issue = current_project.issues.find(params[:id])
 
-        if is_valid_state? && @issue.update_attributes(issue_params)
+        if @issue.update_attributes(issue_params)
           track_updated(@issue)
           render node: @node
         else
@@ -48,17 +47,6 @@ module Dradis::CE::API
       end
 
       private
-
-      # We validate the state params here, otherwise it will throw an ArgumentError
-      # SEE: https://github.com/rails/rails/issues/13971
-      def is_valid_state?
-        if issue_params[:state] && !Issue.states.keys.include?(issue_params[:state])
-          @issue.errors.add(:state, 'is not valid.')
-          return false
-        end
-
-        true
-      end
 
       def issue_params
         params.require(:issue).permit(:text, :state)
