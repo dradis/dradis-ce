c7a3f292f2640d269382cb40e18544d91a687568
Matt Budz <matt.budziosz@gmail.com>
2020-05-08 16:09:53 +0700

    improve html structure

--- app/assets/javascripts/tylium/pages/activities.js
+++ app/assets/javascripts/tylium/pages/activities.js
@@ -1,18 +1,20 @@
 document.addEventListener( "turbolinks:load", function(){
 
-  // hide date header if the previous date header is the same
-  $('[data-behavior~=activity-date-header]').each(function() {
+  var $dateHeader = $('[data-behavior~=activity-date-header]')
+
+  // remove date header if the previous date header is the same
+  $dateHeader.each(function() {
     var currentDate = $(this).find('[data-behavior~=activity-day-value]').text();
     var prevDate = $(this).prevAll('[data-behavior~=activity-date-header]:first').find('[data-behavior~=activity-day-value]').text();
 
     if (prevDate.length && currentDate == prevDate) {
-      $(this).addClass('d-none').attr('data-visible', 'hidden');
+      $(this).remove();
     }
   });
 
   // select activities between visible date headers and group them together
-  $('[data-behavior~=activity-date-header]').not('[data-visible~=hidden]').each(function() {
-    $(this).nextUntil($('[data-behavior~=activity-date-header]').not('[data-visible~=hidden]')).wrapAll('<div class="activities-group" data-behavior="activities-group"></div>');
+  $dateHeader.each(function() {
+    $(this).nextUntil($dateHeader).wrapAll('<div class="activities-group" data-behavior="activities-group"></div>');
   });
 
   // move every other activity group to the right side
