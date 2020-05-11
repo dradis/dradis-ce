3627eaaee304ef18733c605261a3d584ae0da591
Matt Budz <matt.budziosz@gmail.com>
2020-05-11 17:16:14 +0700

    add drag/drop to editor fields & autoUpload option

--- app/assets/javascripts/tylium/modules/fileupload.js
+++ app/assets/javascripts/tylium/modules/fileupload.js
@@ -1,10 +1,11 @@
 // jQuery.fileUpload  - handles attachment uploads (gem: jquery-fileupload-rails)
 
-// Bind fileUpload on page load.
-document.addEventListener("turbolinks:load", function() {
-  if ($('[data-behavior~=jquery-upload]').length) {
-    $('[data-behavior~=jquery-upload]').fileupload({
-      dropZone: $('#drop-zone'),
+function fileUploadInit($element = $('[data-behavior~=jquery-upload]')) { 
+
+  $element.each(function() {
+
+    $(this).fileupload({
+      dropZone: $(this).find('[data-behavior~=drop-zone]'),
       destroy: function(e, data) {
         if (confirm('Are you sure?\n\nProceeding will delete this attachment from the associated node.')) {
           $.blueimp.fileupload.prototype.options.destroy.call(this, e, data);
@@ -20,14 +21,32 @@ document.addEventListener("turbolinks:load", function() {
           data.files[index] = newFile;
         });
       }
+    }).on('fileuploadadd', function (e, data) {
+
+      // Auto-upload if file is dropped on editor fields
+      if ($(this).parents('[data-behavior~=editor-field]').length) {
+
+        // Use path from attachments box in sidebar
+        var attachmentsPath = $(this).parents().find('#fileupload').attr('action');
+        
+        $(this).fileupload('option', { 
+          autoUpload: true,
+          url: attachmentsPath
+        });
+      }
     });
-  }
+  });
+}
+
+document.addEventListener("turbolinks:load", function() {
+  // Bind fileUpload on page load.
+  fileUploadInit();
 });
 
 // Un-bind fileUpload on page unload.
 document.addEventListener('turbolinks:before-cache', function() {
-  if ($('[data-behavior~=jquery-upload]').length) {
-    $('[data-behavior~=jquery-upload]').fileupload('destroy');
-  }
+  $('[data-behavior~=jquery-upload]').each(function() {
+    $(this).fileupload('destroy');
+  });
 });
   
