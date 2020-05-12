dfd27510c4326bb02fd271da93244a636f0e0289
Aaron Manaloto <aaronpomanaloto@gmail.com>
2020-05-12 16:42:51 +0800

    Remove example in spec. This doesn't work in CI because the other plugins are not installed there.

--- spec/features/upload_pages_spec.rb
+++ spec/features/upload_pages_spec.rb
@@ -15,13 +15,5 @@ describe 'upload pages' do
       find('#uploader').find(:option, 'Dradis Template').select_option
       expect(page).to have_css('#state:disabled')
     end
-
-    it 'is enabled when choosing non-dradis-projects plugins' do
-      find('#uploader').find(:option, 'Acunetix').select_option
-      expect(page).to have_css('#state:enabled')
-
-      find('#uploader').find(:option, 'Nexpose').select_option
-      expect(page).to have_css('#state:enabled')
-    end
   end
 end
