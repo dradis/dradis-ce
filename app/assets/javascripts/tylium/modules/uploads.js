dd84141e52b755df3fd03f6e56e9699c77f11110
Aaron Manaloto <aaronpomanaloto@gmail.com>
2020-05-12 14:23:42 +0800

    Implement uploading of issue states from plugins

---
+++ app/assets/javascripts/tylium/modules/uploads.js
@@ -0,0 +1,65 @@
+document.addEventListener("turbolinks:load", function() {
+  var $bar, $percent, $status;
+
+  if ($('body.upload').length) {
+    $bar = $('.progress-bar');
+    $percent = $('.percent');
+    $status = $('#status');
+
+    $('form#new_upload').ajaxForm({
+      resetForm: true,
+      dataType: 'script',
+      beforeSend: function() {
+        var percentVal;
+        $status.empty();
+        percentVal = '0%';
+        $bar.width(percentVal);
+        $bar.addClass('bg-primary');
+        return $percent.html(percentVal);
+      },
+      uploadProgress: function(event, position, total, percentComplete) {
+        var percentVal;
+        percentVal = percentComplete + '%';
+        $bar.width(percentVal);
+        $percent.html(percentVal);
+        return $percent.css('color', '#fff;');
+      },
+      success: function() {
+        var percentVal;
+        percentVal = '100%';
+        $bar.width(percentVal);
+        $bar.removeClass('bg-primary').addClass('bg-success');
+        return $percent.html(percentVal);
+      }
+    });
+
+    $('[data-behavior~=integration-select]').change(function() {
+      var selectedValue = $(this).find('option:selected').val().toLowerCase()
+      if (selectedValue.match(/(package|template)/)) {
+        $('[data-tool-type~=third-party]').addClass('d-none');
+        $('[data-tool-type~=dradis]').removeClass('d-none');
+        $('[data-behavior~=state-select]').addClass('disabled').prop('disabled', 'disabled');
+        $('[data-behavior~=state-select] option:eq(2)').prop('selected', true)
+      }
+      else {
+        $('[data-tool-type~=third-party]').removeClass('d-none');
+        $('[data-tool-type~=dradis]').addClass('d-none');
+        $('[data-behavior~=state-select]').removeClass('disabled').prop('disabled', false);
+      }
+    });
+
+    return $(':file').change(function() {
+      var fileName;
+      ConsoleUpdater.jobId = ConsoleUpdater.jobId + 1;
+      fileName = this.value.split('\\').pop();
+      $('#console').empty();
+      $('#filename').text(fileName);
+      $('#spinner').show();
+      $('#result').data('id', ConsoleUpdater.jobId);
+      $('#result').show();
+      $('#item_id').val(ConsoleUpdater.jobId);
+      $('[data-behavior~=file-label]').text(fileName);
+      return $(this).closest('form').submit();
+    });
+  }
+});
--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -15,9 +15,15 @@ class UploadController < AuthenticatedController
 
   before_action :find_uploaders
   before_action :validate_uploader, only: [:create, :parse]
+  before_action :validate_issue_state, only: [:create, :parse]
 
   def index
     @last_job = Log.new.uid
+
+    # Build the issue states collection for the form select
+    @issue_states = Issue.state_names.map { |state, name| [name, state] }
+
+    @uploaders_for_select = decorated_uploaders_for_select
   end
 
   # TODO: this would overwrite an existing file with the same name.
@@ -29,8 +35,8 @@ class UploadController < AuthenticatedController
     @attachment << params[:file].read
     @attachment.save
 
-    @success = true
     @item_id = params[:item_id].to_i
+    @success = true
     flash.now[:notice] = "Successfully uploaded #{ filename }"
   end
 
@@ -74,6 +80,7 @@ class UploadController < AuthenticatedController
       file: attachment.fullpath.to_s,
       plugin_name: @uploader.to_s,
       project_id: current_project.id,
+      state: @state,
       uid: params[:item_id].to_i
     )
   end
@@ -85,9 +92,10 @@ class UploadController < AuthenticatedController
     begin
       importer = @uploader::Importer.new(
         default_user_id: current_user.id,
-        logger:     job_logger,
-        plugin:     @uploader,
-        project_id: current_project.id
+        logger: job_logger,
+        plugin: @uploader,
+        project_id: current_project.id,
+        state:  @state
       )
 
       importer.import(file: attachment.fullpath)
@@ -110,8 +118,26 @@ class UploadController < AuthenticatedController
     # :upload plugins can define multiple uploaders
     @uploaders ||= Dradis::Plugins::with_feature(:upload).
                      collect(&:uploaders).
-                     flatten.
-                     sort_by(&:name)
+                     flatten
+  end
+
+  def decorated_uploaders_for_select
+    @uploaders.map do |uploader|
+      base_name = uploader.name.demodulize
+      if base_name == 'Package' || base_name == 'Template'
+        base_name = "Dradis #{base_name}"
+      end
+      [base_name, uploader.name]
+    end.sort { |a, b| a <=> b }
+  end
+
+  def validate_issue_state
+    @state =
+      if params[:state] && Issue.states.keys.include?(params[:state])
+        params[:state]
+      else
+        :published
+      end
   end
 
   # Ensure that the requested :uploader is valid and has been included in the
