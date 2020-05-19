2ca04aa2343b905c4b40738e8ca85f48caddd86b
Sean Yeoh <sean.yeoh@hotmail.com>
2020-05-19 19:25:02 +0800

    Add condition for template block

--- spec/support/local_auto_save_shared_examples.rb
+++ spec/support/local_auto_save_shared_examples.rb
@@ -118,60 +118,62 @@ shared_examples 'a form with local auto save' do |klass, action|
   end
 
   if action == :new
-    describe 'with template' do
-      before do
-        template_path = Rails.root.join('spec/fixtures/files/note_templates/')
-        allow(NoteTemplate).to receive(:pwd).and_return(template_path)
-        visit "#{model_path}?template=simple_note"
-        click_link 'Source'
-        @template_content = File.read(template_path.join('simple_note.txt'))
-
-        model_attributes_for_template.each do |model_attribute|
-          fill_in "#{form_object_name}_#{model_attribute[:name]}", with: model_attribute[:value]
-        end
-
-        sleep 1 # Needed for debounce function in local_auto_save.js
-      end
-
-      context 'when form is not saved' do
-        it 'prefill fields with cached data' do
-          page.driver.browser.navigate.refresh
+    if [Card, Evidence, Note, Issue].include? klass
+      describe 'with template' do
+        before do
+          template_path = Rails.root.join('spec/fixtures/files/note_templates/')
+          allow(NoteTemplate).to receive(:pwd).and_return(template_path)
+          visit "#{model_path}?template=simple_note"
           click_link 'Source'
+          @template_content = File.read(template_path.join('simple_note.txt'))
 
           model_attributes_for_template.each do |model_attribute|
-            if model_attribute[:value].is_a?(Date)
-              expect(page.find_field("#{form_object_name}[#{model_attribute[:name]}]").value).to eq model_attribute[:value].to_date.strftime('%Y-%m-%d')
-            else
-              expect(page.find_field("#{form_object_name}[#{model_attribute[:name]}]").value).to eq model_attribute[:value]
+            fill_in "#{form_object_name}_#{model_attribute[:name]}", with: model_attribute[:value]
+          end
+
+          sleep 1 # Needed for debounce function in local_auto_save.js
+        end
+
+        context 'when form is not saved' do
+          it 'prefill fields with cached data' do
+            page.driver.browser.navigate.refresh
+            click_link 'Source'
+
+            model_attributes_for_template.each do |model_attribute|
+              if model_attribute[:value].is_a?(Date)
+                expect(page.find_field("#{form_object_name}[#{model_attribute[:name]}]").value).to eq model_attribute[:value].to_date.strftime('%Y-%m-%d')
+              else
+                expect(page.find_field("#{form_object_name}[#{model_attribute[:name]}]").value).to eq model_attribute[:value]
+              end
             end
           end
         end
-      end
 
-      context 'when form is saved' do
-        it 'does not prefill fields with cached data' do
-          page.find('input[type="submit"]').click
-          visit "#{model_path}?template=simple_note"
-          click_link 'Source'
+        context 'when form is saved' do
+          it 'does not prefill fields with cached data' do
+            page.find('input[type="submit"]').click
+            visit "#{model_path}?template=simple_note"
+            click_link 'Source'
 
-          page_form_values = model_attributes_for_template.collect do |model_attribute|
-            page.find_field("#{form_object_name}[#{model_attribute[:name]}]").value
+            page_form_values = model_attributes_for_template.collect do |model_attribute|
+              page.find_field("#{form_object_name}[#{model_attribute[:name]}]").value
+            end
+
+            expect(page_form_values.include?(@template_content)).to eq true
           end
-
-          expect(page_form_values.include?(@template_content)).to eq true
         end
-      end
 
-      context 'when navigated to new form without template params' do
-        it 'does not prefill fields with cached template data' do
-          visit model_path
-          click_link 'Source'
+        context 'when navigated to new form without template params' do
+          it 'does not prefill fields with cached template data' do
+            visit model_path
+            click_link 'Source'
 
-          model_attributes_for_template.each do |model_attribute|
-            if model_attribute[:value].is_a?(Date)
-              expect(page.find_field("#{form_object_name}[#{model_attribute[:name]}]").value).not_to eq model_attribute[:value].to_date.strftime('%Y-%m-%d')
-            else
-              expect(page.find_field("#{form_object_name}[#{model_attribute[:name]}]").value).not_to eq model_attribute[:value]
+            model_attributes_for_template.each do |model_attribute|
+              if model_attribute[:value].is_a?(Date)
+                expect(page.find_field("#{form_object_name}[#{model_attribute[:name]}]").value).not_to eq model_attribute[:value].to_date.strftime('%Y-%m-%d')
+              else
+                expect(page.find_field("#{form_object_name}[#{model_attribute[:name]}]").value).not_to eq model_attribute[:value]
+              end
             end
           end
         end
