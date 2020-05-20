7d50ce1fe8549f786ecfd47f822e25764dccb55e
Aaron Manaloto <aaronpomanaloto@gmail.com>
2020-05-20 18:55:20 +0800

    Use compact syntax and remove extra spacing

--- app/controllers/markup_controller.rb
+++ app/controllers/markup_controller.rb
@@ -3,8 +3,7 @@ class MarkupController < AuthenticatedController
 
   # Returns the markup cheatsheet that is used by the jQuery.Textile plugin Help
   # button.
-  def help
-  end
+  def help; end
 
   # Returns the Textile version of a text passed as parameter
   def preview
