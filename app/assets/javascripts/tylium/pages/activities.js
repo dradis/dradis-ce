110aef023264281c156c78b5db410cc5ee5ef373
Sean Yeoh <sean.yeoh@hotmail.com>
2020-05-27 13:42:42 +0800

    Rename confusing js variable

--- app/assets/javascripts/tylium/pages/activities.js
+++ app/assets/javascripts/tylium/pages/activities.js
@@ -1,20 +1,20 @@
 document.addEventListener('turbolinks:load', function() {
-  var $activitiesGroupsContainer = $('[data-behavior="activities-groups-container"]');
+  var $infiniteScrollContainer = $('[data-behavior="infinite-scroll-container"]');
   var loading = false;
 
-  if ($activitiesGroupsContainer.length) {
+  if ($infiniteScrollContainer.length) {
     var $viewContent = $('[data-behavior="view-content"]');
 
     $viewContent.on('scroll', function() {
-      var canLoadMore = $activitiesGroupsContainer.data('canLoadMore');
+      var canLoadMore = $infiniteScrollContainer.data('canLoadMore');
       var scrollHeight = $viewContent[0].scrollHeight;
       var scrollTop = $viewContent.scrollTop();
       var viewHeight = $viewContent.height();
 
       // 64px is the sum of margin bottom and padding bottom of the content-container class
       if (canLoadMore && !loading && viewHeight + scrollTop >= (scrollHeight - 64)) {
-        var page = $activitiesGroupsContainer.data('page') + 1;
-        var url = $activitiesGroupsContainer.data('url');
+        var page = $infiniteScrollContainer.data('page') + 1;
+        var url = $infiniteScrollContainer.data('url');
         loading = true;
         $('[data-behavior="activities-spinner"]').show();
 
