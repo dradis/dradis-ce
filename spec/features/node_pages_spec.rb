6f9a7f1ef6a24ea27060f105e5f9f2e6b1e206a8
Matt Budz <matt.budziosz@gmail.com>
2020-06-24 18:08:20 +0700

    update specs

--- spec/features/node_pages_spec.rb
+++ spec/features/node_pages_spec.rb
@@ -20,8 +20,7 @@ describe "node pages" do
     describe "clicking the '+' button in the 'Nodes' sidebar", js: true do
       before do
         visit project_path(current_project)
-        find('.tree-header').click
-        find('.add-subnode > a').click
+        find('.add-node > a').click
       end
 
       let(:submit_form) { click_button "Add" }
