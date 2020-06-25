f58a687b119ce4bca214134f9db266558684c8a5
Matt Budz <matt.budziosz@gmail.com>
2020-06-25 16:04:24 +0700

    adjust navbar with calc'd value vs hardcoded value

--- app/assets/javascripts/tylium/modules/sidebar.js
+++ app/assets/javascripts/tylium/modules/sidebar.js
@@ -3,6 +3,7 @@
     this.$sidebar = $sidebar;
 
     this.$navbar = $sidebar.siblings('[data-behavior~=navbar]');
+    this.$navbarBrand = this.$navbar.find('[data-behavior~=navbar-brand]');
     this.$viewContent = $sidebar.siblings('[data-behavior~=view-content]');
     this.storageKey = $sidebar.data('storage-key');
 
@@ -26,7 +27,7 @@
       Turbolinks.clearCache();
     },
     close: function() {
-      this.$navbar.css('left', '0rem');
+      this.$navbarBrand.css('padding-left', 0);
       this.$sidebar
         .removeClass('sidebar-expanded')
         .addClass('sidebar-collapsed')
@@ -38,12 +39,14 @@
       return JSON.parse(localStorage.getItem(this.storageKey))
     },
     open: function() {
-      this.$navbar.css('left', '9.5rem');
       this.$sidebar
         .removeClass('sidebar-collapsed')
         .addClass('sidebar-expanded')
       this.$viewContent.css({'left': this.$sidebar.css('width'), 'width': 'calc(100vw - ' + this.$sidebar.css('width') + ')'});
 
+      var navbarBrandOffset = parseFloat(this.$navbarBrand.css('padding-left').slice(0,-2)) + parseFloat(this.$sidebar.css('width').slice(0,-2) / 1.65);
+      this.$navbarBrand.css('padding-left', navbarBrandOffset);
+      
       this.changeState(true);
     },
     toggle: function(openSidebar) {
