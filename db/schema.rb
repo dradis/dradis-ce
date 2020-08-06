46aa8eaf56c5ca7042297967fee8e5fece3aca08
Aaron Manaloto <aaronpomanaloto@gmail.com>
2020-08-06 20:11:45 +0800

    Bump schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_120900) do
+ActiveRecord::Schema.define(version: 2020_08_06_120434) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
