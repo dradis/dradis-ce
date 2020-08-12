6b34bdd145b0fc194413af0d6115bd80fb18ef10
Matt Budz <matt.budziosz@gmail.com>
2020-08-12 14:41:10 +0700

    add functionality to browse images in the modal

--- app/assets/javascripts/tylium/modules/image_modals.js
+++ app/assets/javascripts/tylium/modules/image_modals.js
@@ -1,14 +1,80 @@
 document.addEventListener("turbolinks:load", function() {
-  var $modal = $('[data-behavior~=image-modal]');
+  const $modal = $('[data-behavior~=image-modal]');
+  var images = [],
+      index = 0,
+      titles = [];
+
+  function buildArrays($element) {
+    titles.push($element.attr('alt'));
+    images.push($element.attr('src'));
+  }
+
+  function changeImage(direction) {
+    if (direction == 'next' && index < images.length - 1) {
+      index += 1;
+    } else if (direction == 'prev' && index > 0) {
+      index -= 1;
+    }
+
+    loadImage(index);
+  }
+
+  function loadImage(index) {
+    $modal.find('[data-behavior~=modal-title]').text(titles[index]);
+    $modal.find('[data-behavior~=image-modal-image]').attr('src', images[index]);
+
+    if (index == images.length - 1) {
+      $('[data-direction~=next]').addClass('d-none');
+    } else {
+      $('[data-direction~=next]').removeClass('d-none');
+    }
+
+    if (index == 0) {
+      $('[data-direction~=prev]').addClass('d-none');
+    } else {
+      $('[data-direction~=prev]').removeClass('d-none');
+    }
+  }
 
   $('[data-behavior~=content-textile] img').each(function() {
     $(this).attr('data-toggle', 'modal').attr('data-target', '[data-behavior~=image-modal]');
   });
 
   $('[data-behavior~=content-textile] img').click(function() {
-    var title = $(this).attr('alt');
+    images = [];
+    index = 0;
+    titles = [];
 
-    $modal.find('[data-behavior~=modal-title]').text(title);
-    $modal.find('[data-behavior~=image-modal-image]').attr('src', $(this).attr('src'));
+    if ($(this).parents('[data-behavior~=comment-feed]').length) {
+      $(this).parents('[data-behavior~=comment-feed]').find('[data-behavior~=content-textile]').each(function() {
+        $(this).find('img').each(function() {
+          buildArrays($(this));
+        });
+      });
+    } else {
+      $(this).parents('[data-behavior~=content-textile]').find('img').each(function() {
+        buildArrays($(this));
+      });
+    }
+
+    index = images.indexOf($(this).attr('src'));
+
+    loadImage(index);
+  });
+
+  $('[data-direction]').click(function() {
+    if ($(this).is('[data-direction~=next]')) {
+      changeImage('next');
+    } else if ($(this).is('[data-direction~=prev]')) {
+      changeImage('prev');
+    }
+  });
+
+  $($modal).keydown(function(e) {
+    if (e.which == 39) {
+      changeImage('next');
+    } else if (e.which == 37) {
+      changeImage('prev');
+    }
   });
 });
