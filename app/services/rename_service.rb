adacbe87f6eb804f229ca83ca5cdefe079014e27
Sean Yeoh <sean.yeoh@hotmail.com>
2020-12-04 20:05:39 +0800

    Refactor RenameService

--- app/services/rename_service.rb
+++ app/services/rename_service.rb
@@ -1,15 +1,18 @@
 class RenameService
-  # Obtain a suitable filename for a new file. If the directory
-  # does not contain a file with the same filename, use the original filename,
-  # otherwise provide count-based alternative.
-  #   original_filename is a String object
-  #   pathname is a Pathname object
+  # If filename exists within the directory,
+  # return a count based alternative, E.g. file_name-copy-01.png, file_name-copy-02.png
+  # If not, return the original filename.
+  #   original_filename: the filename in String
+  #   pathname: Pathname object
   def self.rename_file(original_filename:, pathname:)
-    return original_filename unless File.exists?(pathname.join(original_filename))
-
-    extension = File.extname(original_filename)
-    basename  = File.basename(original_filename, extension)
-    sequence  = Dir.glob(pathname.join("#{basename}_copy-*#{extension}")).map { |a| a.match(/_copy-([0-9]+)#{extension}\z/)[1].to_i }.max || 0
-    "%s_copy-%02i%s" % [basename, sequence + 1, extension]
+    if File.exists?(pathname.join(original_filename))
+      extension = File.extname(original_filename)
+      basename = File.basename(original_filename, extension)
+      files = Dir.glob(pathname.join("#{basename}_copy-*#{extension}"))
+      sequence = files.map { |file| file.match(/_copy-([0-9]+)#{extension}\z/)[1].to_i }.max || 0
+      "%s_copy-%02i%s" % [basename, sequence + 1, extension]
+    else
+      original_filename
+    end
   end
 end
