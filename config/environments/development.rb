6efc011407edf0628da93a73b887628b88f91b9e
Brian Pearce <brian.o.pearce@gmail.com>
2021-01-21 14:28:00 +0100

    Remove duplicated line

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,6 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.action_controller.enable_fragment_cache_logging = true
 
   # Do not eager load code on boot.
   config.eager_load = false
