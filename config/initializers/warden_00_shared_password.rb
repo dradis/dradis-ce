7d4b4921c58dc1574ffd57fa798300256280b9b1
Sean Yeoh <sean.yeoh@hotmail.com>
2021-01-22 15:49:55 +0800

    Fix deprecation warnings for autoloaded constants

--- config/initializers/warden_00_shared_password.rb
+++ config/initializers/warden_00_shared_password.rb
@@ -17,21 +17,23 @@ Rails.configuration.middleware.use Warden::Manager do |manager|
   manager.failure_app = ->(env) { SessionsController.action(:failure).call(env) }
 end
 
-# A simple db-backed strategy that uses the User.authenticate() method.
-Warden::Strategies.add(:shared_password) do
-  def valid?
-    params['login'] || params['password']
-  end
+Rails.application.reloader.to_prepare do
+  # A simple db-backed strategy that uses the User.authenticate() method.
+  Warden::Strategies.add(:shared_password) do
+    def valid?
+      params['login'] || params['password']
+    end
 
-  def authenticate!
-    username = params.fetch('login', nil)
-    password = params.fetch('password', nil)
+    def authenticate!
+      username = params.fetch('login', nil)
+      password = params.fetch('password', nil)
 
-    if not ( username.blank? || password.nil? || ::BCrypt::Password.new(::Configuration.shared_password) != password )
-      user = User.find_or_create_by(email: username)
-      success!(user)
-    else
-      fail 'Invalid credentials.'
+      if not ( username.blank? || password.nil? || ::BCrypt::Password.new(::Configuration.shared_password) != password )
+        user = User.find_or_create_by(email: username)
+        success!(user)
+      else
+        fail 'Invalid credentials.'
+      end
     end
   end
 end
