682e929af6c58abde8ad93bc7db5d573468ced08
Matt Budz <matt.budziosz@gmail.com>
2021-02-02 18:32:40 +0700

    Update projects_controller.rb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,7 +12,7 @@ class ProjectsController < AuthenticatedController
   def show
     @activities    = Activity.latest
     @authors       = [current_user]
-    @boards        = current_project.boards.where(node_id: current_project.methodology_library.id)
+    @boards        = current_project.methodology_library.boards
     @issues        = current_project.issues.includes(:tags).sort
     @methodologies = current_project.methodology_library.notes.map{|n| Methodology.new(filename: n.id, content: n.text)}
     @nodes         = current_project.nodes.in_tree
