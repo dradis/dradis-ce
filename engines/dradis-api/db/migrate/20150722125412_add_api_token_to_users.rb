aad3b24ea1ce5b380ce6a5f47531de2e203aded4
Aaron Manaloto <aaronpomanaloto@gmail.com>
2021-04-30 18:33:26 +0800

    Simplify api token migration

--- engines/dradis-api/db/migrate/20150722125412_add_api_token_to_users.rb
+++ engines/dradis-api/db/migrate/20150722125412_add_api_token_to_users.rb
@@ -1,17 +1,14 @@
 class AddAPITokenToUsers < ActiveRecord::Migration[5.1]
-  # Future-proof this migration by ignoring everything in the actual model.
-  class User < ActiveRecord::Base; end
-
   def up
-    add_column :users, :api_token, :string
+    add_column :users, :api_token, :string, null: false
     add_index :users, :api_token, unique: true
 
-    # Create an API token for all existing users
-    User.reset_column_information
-    User.find_each do |user|
-      user.update_column(:api_token, SecureRandom.base64(18).tr("+/", "-_"))
+    User.transaction do
+      User.find_each do |user|
+        # Create an API token for all existing users
+        user.update_column(:api_token, SecureRandom.base64(18).tr("+/", "-_"))
+      end
     end
-    change_column :users, :api_token, :string, null: false
   end
 
   def down
