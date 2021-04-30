fff619d38d13933bbb8c1d1e6db7f015a0ebe388
Matt Budz <matt.budziosz@gmail.com>
2021-04-30 16:43:59 +0200

    init DataTables with additional options

--- app/assets/javascripts/shared/datatable.js
+++ app/assets/javascripts/shared/datatable.js
@@ -1,5 +1,17 @@
 document.addEventListener('turbolinks:load', function() {
   $('[data-behavior~=datatable]').each(function() {
-    $(this).DataTable();
+    $(this).DataTable({
+      autoWidth: false,
+      dom: "<'row'<'col-lg-6'><'col-lg-6'f>>" +
+      "<'row'<'col-lg-12'tr>>" +
+      "<'dataTables_footer_content'lip>",
+      fixedHeader: {
+        header: true,
+        headerOffset: $('[data-behavior~=navbar]').height() //FIXME: header is not fixed/sticky.
+      },
+      scrollX: true
+    })
+
+    //$(this).columns.adjust();
   });
 });
