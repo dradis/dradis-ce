2d614c9727d4fcb297abd0ad3e208408258bb8a3
Sean <sean.yeoh@hotmail.com>
2021-05-03 20:33:48 +0800

    Unbind dataTable on page unload

--- app/assets/javascripts/shared/datatable.js
+++ app/assets/javascripts/shared/datatable.js
@@ -1,5 +1,15 @@
 document.addEventListener('turbolinks:load', function() {
   $('[data-behavior~=datatable]').each(function() {
-    $(this).DataTable();
+    $(this).DataTable({
+      pageLength: 25,
+      lengthChange: false
+    });
+  });
+});
+
+// Un-bind DataTable on page unload.
+document.addEventListener('turbolinks:before-cache', function() {
+  $('[data-behavior~=datatable]').each(function() {
+    $(this).DataTable().destroy();
   });
 });
