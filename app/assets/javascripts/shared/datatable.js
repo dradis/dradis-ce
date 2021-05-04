8d43d59a30772baab57922ef16c54097302fa326
Sean <sean.yeoh@hotmail.com>
2021-05-04 16:02:33 +0800

    Add column visibility extension to DataTable

--- app/assets/javascripts/shared/datatable.js
+++ app/assets/javascripts/shared/datatable.js
@@ -1,8 +1,27 @@
 document.addEventListener('turbolinks:load', function() {
   $('[data-behavior~=datatable]').each(function() {
     $(this).DataTable({
+      // https://datatables.net/reference/option/dom,
+      // The 'dom' attribute defines the order of elements in a DataTable.
+      dom: 'Bfrtip',
       pageLength: 25,
-      lengthChange: false
+      lengthChange: false,
+      buttons: {
+        dom: {
+          button: {
+            tag: 'button',
+            className: 'btn'
+          }
+        },
+        buttons: [
+          {
+            extend: 'colvis',
+            text: '<i class="fa fa-columns"></i><i class="fa fa-caret-down"></i>',
+            titleAttr: 'Choose columns to show',
+            className: 'btn',
+          }
+        ]
+      }
     });
   });
 });
