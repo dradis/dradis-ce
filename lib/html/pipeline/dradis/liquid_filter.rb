c1f4eb3960aaa1b9b610ecf81c28a9d97455ed5a
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2021-05-27 08:53:52 +0200

    LiquidFilter - pre- and post- process to hide code highlight syntax

--- lib/html/pipeline/dradis/liquid_filter.rb
+++ lib/html/pipeline/dradis/liquid_filter.rb
@@ -2,7 +2,11 @@ module HTML
   class Pipeline
     module Dradis
       class LiquidFilter < TextFilter
+        REGEX = /\$\$\[\[(.+?)\]\]\$\$/
+
         def call
+          hide_code_highlight_syntax
+
           assigns = context.fetch(:liquid_assigns, {})
 
           options = {
@@ -12,6 +16,21 @@ module HTML
           }
 
           Liquid::Template.parse(@text).render(assigns, options)
+
+          restore_code_highlight_syntax
+        end
+
+        private
+        def hide_code_highlight_syntax
+          @text.gsub(CodeHighlightFilter::REGEX) do |match|
+            %|$$[[#{$1}]]$$|
+          end
+        end
+        def restore_code_highlight_syntax
+          @text.gsub(REGEX) do |match|
+            %|$${{#{$1}}}|
+          end
+          @text
         end
       end
     end
