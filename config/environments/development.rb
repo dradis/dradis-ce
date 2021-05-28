d005e53dd74ef579678b518518370c0fd696cc66
Brian Pearce <brian.o.pearce@gmail.com>
2021-05-28 15:22:01 +0200

    Initialize ActiveStorage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Store uploaded files on the local file system in a temporary directory.
+  config.active_storage.service = :local
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
