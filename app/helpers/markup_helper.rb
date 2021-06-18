c7b1750801d9ef3cf715a332fc031381f58ddd4b
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2021-06-18 11:19:05 +0200

    MarkupHelper - bring sync even closer

--- app/helpers/markup_helper.rb
+++ app/helpers/markup_helper.rb
@@ -4,10 +4,10 @@ module MarkupHelper
     preview_params[:project_id] = params[:project_id] if params[:project_id]
 
     {
-      form_url: form_fields_path(format: :js),
-      help_url: help_markup_path,
-      preview_url: preview_markup_path(preview_params),
-      source_url: source_fields_path
+      form_url: main_app.form_fields_path(format: :js),
+      help_url: main_app.help_markup_path,
+      preview_url: main_app.preview_markup_path(preview_params),
+      source_url: main_app.source_fields_path
     }
   end
 end
