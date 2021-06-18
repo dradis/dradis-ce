f668ac70d91fafe789a900f45176a90998eb9d9d
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2021-06-18 11:12:30 +0200

    MarkupHelper#editor_paths - better keep in sync

--- app/helpers/markup_helper.rb
+++ app/helpers/markup_helper.rb
@@ -1,9 +1,12 @@
 module MarkupHelper
   def editor_paths
+    preview_params = {}
+    preview_params[:project_id] = params[:project_id] if params[:project_id]
+
     {
       form_url: form_fields_path(format: :js),
       help_url: help_markup_path,
-      preview_url: preview_markup_path,
+      preview_url: preview_markup_path(preview_params),
       source_url: source_fields_path
     }
   end
