ee18f9262357197932025ec83722657062727036
Brian Pearce <brian.o.pearce@gmail.com>
2021-07-23 08:10:36 +0200

    Update ability line for parity

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    can :manage, Project
+    can [:read, :use, :update], Project, authors: { id: [user.id] }
     can [:create, :read], Comment
     can [:update, :destroy], Comment, user_id: user.id
   end
