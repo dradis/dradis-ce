d9284e13d26a1db712f0511debf17baa234fb237
Matt Budz <matt.budziosz@gmail.com>
2021-08-06 16:54:17 +0200

    fix turbolinks caching bug

--- app/assets/javascripts/tylium/modules/resource_sidebar.js
+++ app/assets/javascripts/tylium/modules/resource_sidebar.js
@@ -1,20 +1,24 @@
 document.addEventListener('turbolinks:load', function() {
-  const $resourceSidebar = $('[data-behavior~=resource-sidebar]'),
-        $resourceSidebarToggle = $('[data-behavior~=resource-sidebar-toggle]'),
-        localStorageKey = 'project.ce.resource-sidebar-collapsed',
-        savedState = localStorage.getItem(localStorageKey);
-  
-  function toggleResourceSidebar() {
-    $resourceSidebar.toggleClass('collapsed');
-    $resourceSidebarToggle.find('i').toggleClass('fa-chevron-right fa-chevron-left');
-    localStorage.setItem(localStorageKey, $resourceSidebar.hasClass('collapsed'));
-  }
-  
-  if (savedState == 'true') {
-    toggleResourceSidebar();
-  }
 
-  $resourceSidebarToggle.on('click', function() {      
-    toggleResourceSidebar();
-  });
+  if ($('[data-behavior~=resource-sidebar]').length) {
+    const $resourceSidebar = $('[data-behavior~=resource-sidebar]'),
+          $resourceSidebarToggle = $('[data-behavior~=resource-sidebar-toggle]'),
+          localStorageKey = 'project.ce.resource-sidebar-collapsed',
+          savedState = localStorage.getItem(localStorageKey);
+
+    function toggleResourceSidebar() {
+      $resourceSidebar.toggleClass('collapsed');
+      $resourceSidebarToggle.find('i').toggleClass('fa-chevron-right fa-chevron-left');
+      localStorage.setItem(localStorageKey, $resourceSidebar.hasClass('collapsed'));
+      Turbolinks.clearCache();
+    }
+
+    if (savedState == 'true') {
+      toggleResourceSidebar();
+    }
+
+    $resourceSidebarToggle.on('click', function() {
+      toggleResourceSidebar();
+    });
+  };
 });
