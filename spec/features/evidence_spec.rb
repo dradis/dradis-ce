8e95541022a872e26c767fdb1e3baa7420fe707e
Matt Budz <matt.budziosz@gmail.com>
2021-11-05 17:36:41 +0100

    update specs

--- spec/features/evidence_spec.rb
+++ spec/features/evidence_spec.rb
@@ -45,9 +45,6 @@ describe 'evidence' do
       should have_selector 'p',  text: 'Issue info'
     end
 
-    let(:trackable) { @evidence }
-    it_behaves_like 'a page with an activity feed'
-
     let(:commentable) { @evidence }
     it_behaves_like 'a page with a comments feed'
 
