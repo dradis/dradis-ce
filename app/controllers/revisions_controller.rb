7bac692ea72a8f544924568547834494d2328f8e
Matt Budz <matt.budziosz@gmail.com>
2021-11-05 16:48:32 +0100

    render revisions#show even without revisions

--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -7,15 +7,17 @@ class RevisionsController < AuthenticatedController
   before_action :load_record, except: [ :trash, :recover ]
 
   def index
-    redirect_to action: :show, id: @record.versions.where(event: 'update').last.try(:id) || 0
+    redirect_to action: :show, id: @record.versions.last.try(:id) || 0
   end
 
   def show
     # Use `reorder`, not `order`, to override Paper Trail's default scope
     @revisions = @record.versions.includes(:item).reorder("created_at DESC")
     @revision  = @revisions.find(params[:id])
-
-    @diffed_revision = DiffedRevision.new(@revision, @record)
+    
+    if @revision.event == 'update'
+      @diffed_revision = DiffedRevision.new(@revision, @record)
+    end 
   end
 
   def trash
