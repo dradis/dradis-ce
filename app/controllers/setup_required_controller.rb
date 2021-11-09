571fe9b5f66cf208ba5ae2fcc9f6897bb219b22d
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2021-11-09 09:53:39 +0100

    SetupRequiredController - prepare for sync

--- app/controllers/setup_required_controller.rb
+++ app/controllers/setup_required_controller.rb
@@ -3,8 +3,17 @@ class SetupRequiredController < ApplicationController
 
   private
   def setup_required
+    defined?(Dradis::Pro) ? setup_required_pro : setup_required_ce
+  end
+
+  # -- CE methods -------------------------------------------------------------
+  def setup_required_ce
     if (::Configuration.shared_password == 'improvable_dradis')
       redirect_to new_setup_password_path
     end
+  end
+
+  # -- Pro methods ------------------------------------------------------------
+  def setup_required_pro
   end
 end
