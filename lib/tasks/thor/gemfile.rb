77fd83f62c4d994e8ef62937e92c14be33087682
Brian Pearce <brian.o.pearce@gmail.com>
2021-11-17 11:18:37 +0100

    Use a constant for the regex

--- lib/tasks/thor/gemfile.rb
+++ lib/tasks/thor/gemfile.rb
@@ -3,6 +3,8 @@ class DradisTasks < Thor
     include Thor::Actions
     namespace 'dradis:gemfile'
 
+    ADDON_REGEX = /['"](dradis(pro)?-\w*(?!.*engines\/))['"], .*/
+
     # The regex matches the following types:
     # gem 'dradis-zap', '~> 4.5.2'
     # gem 'dradis-zap', github: 'dradis/dradis-zap', branch: 'release-4.5.2'
@@ -14,9 +16,9 @@ class DradisTasks < Thor
     method_option :branch, aliases: '-b', desc: 'The branch to assign to the addons ex. \'release-4.0.1\''
     def update(file)
       if options[:branch]
-        gsub_file file, /['"](dradis(pro)?-\w*(?!.*engines\/))['"], .*/, "'\\1', github: 'dradis/\\1', branch: '#{options[:branch]}'"
+        gsub_file file, ADDON_REGEX, "'\\1', github: 'dradis/\\1', branch: '#{options[:branch]}'"
       elsif options[:version]
-        gsub_file file, /['"](dradis(pro)?-\w*(?!.*engines\/))['"], .*/, "'\\1', '~> #{options[:version]}'"
+        gsub_file file, ADDON_REGEX, "'\\1', '~> #{options[:version]}'"
       else
         puts 'ERROR: You need to specify :version or :branch'
         DradisTasks::Gemfile.command_help(Thor::Base.shell.new, 'update')
