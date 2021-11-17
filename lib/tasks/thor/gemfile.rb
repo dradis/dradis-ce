da76c9ef07cfa2eb7e5ff2b2814af6f8b861908e
Brian Pearce <brian.o.pearce@gmail.com>
2021-11-17 10:33:53 +0100

    Move the gemfile update helper to a thor task

---
+++ lib/tasks/thor/gemfile.rb
@@ -0,0 +1,35 @@
+class DradisTasks < Thor
+  class Gemfile < Thor
+    namespace 'dradis:gemfile'
+
+    desc 'update FILE', 'Updates the version or source of Dradis addons'
+    method_option :version, aliases: '-v', desc: 'The version to assign to the addons ex. \'4.0.1\''
+    method_option :branch, aliases: '-b', desc: 'The branch to assign to the addons ex. \'release-4.0.1\''
+    def update(file)
+      if options[:version] || options[:branch]
+        update_gemfile(file, options)
+      else
+        puts 'ERROR: You need to specify :version or :branch'
+        DradisTasks::Gemfile.command_help(Thor::Base.shell.new, 'update')
+      end
+    end
+
+    private
+
+    def update_gemfile(file, options)
+      gemfile = File.read(file)
+
+      updated_gemfile = gemfile.gsub(/['"](dradis(pro)?-\w*)['"], .*/) do |match|
+        gem_name = /(dradis(pro)?-\w*)['"]/.match(match)[1]
+
+        if options[:branch]
+          "'#{gem_name}', github: 'dradis/#{gem_name}', branch: '#{options[:branch]}'"
+        elsif options[:version]
+          "'#{gem_name}', '~>#{options[:version]}'"
+        end
+      end
+
+      File.write(file, updated_gemfile)
+    end
+  end
+end
