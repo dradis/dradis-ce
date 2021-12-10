8b4519aea2a880d708f005d4a7f759c95543939f
Matt Budz <matt.budziosz@gmail.com>
2021-12-10 15:42:10 +0100

    set auto save key in it's own method

--- app/controllers/issues/evidence_controller.rb
+++ app/controllers/issues/evidence_controller.rb
@@ -1,15 +1,20 @@
 class Issues::EvidenceController < IssuesController
   def new
-    @auto_save_key =  if params[:template]
-                        "project-#{current_project.id}-issue-#{params[:issue_id]}-evidence-#{params[:template]}"
-                      else
-                        "project-#{current_project.id}-issue-#{params[:issue_id]}-evidence"
-                      end
     @issues = Issue.where(node_id: current_project.issue_library.id)
     @issue = @issues.find(params[:issue_id])
 
     @nodes_for_add_evidence = current_project.nodes.user_nodes.order(:label)
 
     @template_content = template_content if params[:template]
+  end
+
+  private
+
+  def set_auto_save_key
+    @auto_save_key =  if params[:template]
+      "project-#{current_project.id}-issue-#{params[:issue_id]}-evidence-#{params[:template]}"
+    else
+      "project-#{current_project.id}-issue-#{params[:issue_id]}-evidence"
+    end
   end
 end
