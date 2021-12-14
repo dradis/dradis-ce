57d827ddea5a9114f2efb819f3a29ccc50d88b11
Aaron Manaloto <aaronpomanaloto@gmail.com>
2021-12-14 19:29:46 +0800

    Improve spec parity

--- spec/requests/exports_spec.rb
+++ spec/requests/exports_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 describe 'export' do
-  before do
+  let(:ce_login) do
     @project = create(:project)
     @user = create(:user, :admin)
     Configuration.create(name: 'admin:password', value: ::BCrypt::Password.create('rspec_pass'))
     post session_path, params: { login: @user.email, password: 'rspec_pass' }
   end
+
+  let(:pro_login) do
+    login_to_project_as_user
+  end
+
+  before { defined?(Dradis::Pro) ? pro_login : ce_login }
 
   it 'prevents invalid templates' do
     export = post project_export_path(
