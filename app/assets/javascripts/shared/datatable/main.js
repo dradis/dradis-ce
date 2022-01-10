5cfb6e4a632a8d3c3a34f1c581eab7a306151567
Matt Budz <matt.budziosz@gmail.com>
2022-01-10 12:34:03 +0100

    remove unused method

--- app/assets/javascripts/shared/datatable/main.js
+++ app/assets/javascripts/shared/datatable/main.js
@@ -229,7 +229,6 @@ class DradisDatatable {
 
   behaviors() {
     this.setupCheckboxListeners();
-    this.setupEvidenceCountListener();
     this.setupMergeButtonToggle();
     this.setupTagButtonToggle();
     this.setupBulkDeleteButtonToggle();
