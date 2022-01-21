0c836d8dbdb14b3d03b8c0674238318dbe2b0939
Brian Pearce <brian.o.pearce@gmail.com>
2022-01-21 10:39:52 +0100

    Use a less project based identifier

--- app/controllers/concerns/cached_columns.rb
+++ app/controllers/concerns/cached_columns.rb
@@ -1,18 +1,15 @@
 module CachedColumns
-  CACHE_KEY = "projects/%{project_id}/%{record_type}/column-keys/%{tail}".freeze
+  CACHE_KEY = "%{identifier}/%{record_type}/column-keys/%{tail}".freeze
 
   # Takes an ActiveRecord::Relation so we can make one more query off it
-  def cached_collection_column_keys(collection, extra_columns)
+  def cached_collection_column_keys(collection, extra_columns, identifier: @current_project.id)
     last_updated_record = collection.order(updated_at: :desc).first
     # Exit early if the collection is empty.
     return extra_columns unless last_updated_record
 
     last_updated = last_updated_record.updated_at.to_i
 
-    # DradisPro IssueLibrary support
-    current_project ||= OpenStruct.new(id: 'issuelib')
-
-    key_opts = { project_id: current_project.id, record_type: last_updated_record.class.name }
+    key_opts = { identifier: identifier, record_type: last_updated_record.class.name }
 
     Rails.cache.fetch(CACHE_KEY % key_opts.merge(tail: last_updated)) do
       # Delete any old key before we write the new one
