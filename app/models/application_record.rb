b406fecd5765611e88e13d13313b3c535f998472
Brian Pearce <brian.o.pearce@gmail.com>
2022-01-28 10:03:57 +0100

    Move the location of the db limit constants

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  DB_MAX_STRING_LENGTH = 255
+  DB_MAX_TEXT_LENGTH = 4294967295
 end
--- config/initializers/db_constants.rb
+++
@@ -1,7 +0,0 @@
-DB_MAX_STRING_LENGTH = 255
-
-# The max length we set most fields as
-DB_MAX_TEXT_LENGTH = 65535
-
-# The max length supported by the DB
-DB_REAL_MAX_LENGTH = 4294967295
