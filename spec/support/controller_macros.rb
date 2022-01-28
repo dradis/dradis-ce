8cb498e9936934dade8f82ec7b83879ced70909e
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2022-01-28 10:00:30 +0100

    Cleanup Configuration fixtures

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,8 +1,6 @@
 module ControllerMacros
   extend ActiveSupport::Concern
 
-  included { fixtures :configurations }
-
   # Macro to emulate user login
   def login_as_user(user=create(:user))
     allow_any_instance_of(ApplicationController).to \
