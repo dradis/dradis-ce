e410070ee3903e21405988b205c1bf9085a79ecb
Matt Budz <matt.budziosz@gmail.com>
2022-02-03 16:44:26 +0100

    more explict specs

--- spec/features/revisions_spec.rb
+++ spec/features/revisions_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Revisions#show:' do
   subject { page }
 
-  describe 'when the record has activities' do
+  describe 'when the record has 2 revisions' do
 
     let(:record) do
       with_versioning do
@@ -17,11 +17,11 @@ describe 'Revisions#show:' do
       record.save
     end
     
-    it 'lists them in the activity feed' do
+    it 'the 2 revisions are listed in the history table' do
       visit project_issue_revisions_path(current_project, record)
 
       within '.revisions-table tbody' do
-        should have_selector('tr', count: record.versions.count)
+        should have_selector('tr', count: 2)
         record.versions.each do |version| 
           should have_content("#{version.event}d".capitalize)
           should have_selector("time[datetime='#{version.created_at.strftime('%FT%TZ')}']")
