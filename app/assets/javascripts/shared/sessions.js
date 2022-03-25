982cf3ac4870484a8d1b79dff5ee15dd79b63efa
Matt Budz <matt.budziosz@gmail.com>
2022-03-25 11:31:47 +0100

    add js

---
+++ app/assets/javascripts/shared/sessions.js
@@ -0,0 +1,28 @@
+document.addEventListener('turbolinks:load', function() {
+  if ($('body.unauthenticated').length) {
+    var charCount = 0,
+        strings = [
+          'Tame the output from multiple scan tools, email threads, and notes',
+          'Deliver quality security assessments at light speed',
+          'Focus less on tedious reporting tasks and more on testing',
+          'Ditch the overhead that drags down project delivery'
+        ],
+        randomStringIndex = Math.floor(Math.random() * strings.length),
+        randomString = strings[randomStringIndex];
+
+    setTimeout(function() {
+      window.setInterval(function() {
+        if (charCount <= randomString.length) {
+          $('[data-behavior~=mission-text]').text(randomString.substring(0, charCount));
+          charCount += 1;
+        }
+      }, 60);
+    }, 500);
+
+    $('[data-behavior~=logo-icons]').addClass('expand');
+
+    $('[data-behavior~=login-btn]').on('click', function() {
+      $('[data-behavior~=logo-icons]').removeClass('expand').addClass('shrink');
+    });
+  }
+});
