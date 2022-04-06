1b2205c80479de3e896bc37372646a32f82771e1
Aaron Manaloto <mail@aapomm.io>
2022-04-06 16:14:03 +0800

    Handle comments with missing user

--- app/drops/comment_drop.rb
+++ app/drops/comment_drop.rb
@@ -2,6 +2,6 @@ class CommentDrop < BaseDrop
   delegate :content, :created_at, :updated_at, to: :@record
 
   def user
-    UserDrop.new(@record.user)
+    @record.user ? UserDrop.new(@record.user) : nil
   end
 end
