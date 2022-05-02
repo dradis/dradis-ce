92d3677ea71956ff678d3f4e09efb75f127ce2ad
Matt Budz <matt.budziosz@gmail.com>
2022-05-02 11:49:32 +0200

    close nodes menu on click in mobile format

--- app/assets/javascripts/tylium/modules/sidebar.js
+++ app/assets/javascripts/tylium/modules/sidebar.js
@@ -16,7 +16,7 @@
       var that = this;
 
       $('[data-behavior~=sidebar-toggle]').on('click', function(e) {
-        if ($(this).is('[data-behavior~=nodes-tree')) {
+        if ($(this).is('[data-behavior~=nodes-tree]')) {
           if (window.innerWidth < that.minBreakpoint) {
             $(this).attr('data-behavior', 'nodes-tree sidebar-toggle');
           } else {
@@ -28,6 +28,12 @@
           that.toggle(!that.isSidebarOpen());
         }
       });
+
+      if (window.innerWidth < that.minBreakpoint) {
+        $('[data-behavior~=sidebar-node-link]').on('click', function() {
+          that.close();
+        });
+      }
     },
     changeState: function(state) {
       localStorage.setItem(this.storageKey, state);
@@ -37,25 +43,25 @@
       this.$navbarBrand.css('padding-left', 0);
       this.$sidebar
         .removeClass('sidebar-expanded')
-        .addClass('sidebar-collapsed')
+        .addClass('sidebar-collapsed');
       this.$viewContent.css({'left': this.$sidebar.css('width'), 'width': 'calc(100vw - ' + this.$sidebar.css('width') + ')'});
 
       this.changeState(false);
     },
     isSidebarOpen: function() {
-      return JSON.parse(localStorage.getItem(this.storageKey))
+      return JSON.parse(localStorage.getItem(this.storageKey));
     },
     open: function() {
       this.$sidebar
         .removeClass('sidebar-collapsed')
-        .addClass('sidebar-expanded')
+        .addClass('sidebar-expanded');
       this.$viewContent.css({'left': this.$sidebar.css('width'), 'width': 'calc(100vw - ' + this.$sidebar.css('width') + ')'});
 
       if (window.innerWidth > this.minBreakpoint) {
         var navbarBrandOffset = parseFloat(this.$navbarBrand.css('padding-left').slice(0,-2)) + parseFloat(this.$sidebar.css('width').slice(0,-2) / 1.65);
         this.$navbarBrand.css('padding-left', navbarBrandOffset);
       }
-      
+
       this.changeState(true);
     },
     toggle: function(openSidebar) {
@@ -65,7 +71,7 @@
         this.close();
       }
     }
-  }
+  };
 
   window.Sidebar = Sidebar;
 })(jQuery, window);
