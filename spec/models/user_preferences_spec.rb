732344e6cc9cc0226541421264ff2027e7bda907
Aaron Manaloto <mail@aapomm.io>
2022-06-30 16:49:45 +0800

    Remove YAML methods from user preferences and spec

--- spec/models/user_preferences_spec.rb
+++ spec/models/user_preferences_spec.rb
@@ -13,38 +13,6 @@ describe UserPreferences do
     expect(preferences.digest_frequency).to eq 'daily'
   end
 
-  context "loading from YAML" do
-    context "valid tour name" do
-      it "returns 0 for a fresh set of preferences for a valid tour" do
-        preferences = subject.class.load "--- !ruby/object:UserPreferences\ntours: {}\n"
-
-        expect do
-          preferences.last_first_sign_in
-        end.not_to raise_error
-
-        expect(preferences.last_first_sign_in).to eq('0')
-      end
-
-      it "returns the last tour version of XXX type that was visited for a valid tour" do
-        preferences = subject.class.load "--- !ruby/object:UserPreferences\ntours:\n :first_sign_in: '2.0.0'\n"
-
-        expect do
-          preferences.last_first_sign_in
-        end.not_to raise_error
-
-        expect(preferences.last_first_sign_in).to eq('2.0.0')
-      end
-    end
-
-    it "only serializes designated attributes" do
-      preferences = UserPreferences.new
-      yaml = UserPreferences.dump(preferences)
-
-      expect(yaml).to eq "--- !ruby/object:UserPreferences\ndigest_frequency: instant\ntours: {}\n"
-      expect(yaml).not_to include 'errors'
-    end
-  end
-
   context "#last_tour_XXX" do
     context "invalid tour name" do
       it "raises an exception if the tour name isn't valid" do
