3fd5542bc325f1aeec8d5a99cd237d8cda6b99f6
sean-yeoh <sean.yeoh@hotmail.com>
2022-07-13 15:54:42 +0800

    Permit class for yaml column serializer

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -1,1 +1,2 @@
 Rails.application.config.action_view.form_with_generates_remote_forms = true
+Rails.application.config.active_record.yaml_column_permitted_classes = [ActiveModel::Errors, UserPreferences]
