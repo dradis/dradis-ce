84a50eca082656f8f300805629bf93e07c6f27ad
sean-yeoh <sean.yeoh@hotmail.com>
2022-07-13 17:40:40 +0800

    Move yaml_column_permitted_classes into application.reloader.to_prepare block

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -1,2 +1,6 @@
 Rails.application.config.action_view.form_with_generates_remote_forms = true
-Rails.application.config.active_record.yaml_column_permitted_classes = [ActiveModel::Errors, Symbol, UserPreferences]
+
+Rails.application.reloader.to_prepare do
+  Rails.application.config.active_record.yaml_column_permitted_classes = [ActiveModel::Errors, Symbol, UserPreferences]
+end
+
