da3d91f7b1dd1263aa9174babf86e0c7f713a77a
Alex Rupom Hasdak <alexrupom@hotmail.com>
2022-09-15 17:18:01 +0600

    :wrench:

--- db/schema.rb
+++ db/schema.rb
@@ -197,7 +197,6 @@ ActiveRecord::Schema.define(version: 2021_03_16_202870) do
     t.integer "taggings_count", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "color", default: "#555555", null: false
     t.index ["name"], name: "index_tags_on_name"
     t.index ["taggings_count"], name: "index_tags_on_taggings_count"
   end
