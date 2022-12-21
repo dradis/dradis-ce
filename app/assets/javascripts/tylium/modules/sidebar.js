582bcc9364c552afbe1e9d383e78413bdd4df7e7
Matt Budz <matt.budziosz@gmail.com>
2022-12-21 16:50:02 +0100

    :alpha constructor declarations + linting

--- app/assets/javascripts/tylium/modules/sidebar.js
+++ app/assets/javascripts/tylium/modules/sidebar.js
@@ -1,76 +1,91 @@
-(function($, window) {
+(function ($, window) {
   function Sidebar($sidebar) {
     this.$sidebar = $sidebar;
-    this.$navbarBrand = $sidebar.siblings('[data-behavior~=navbar]').find('[data-behavior~=navbar-brand]');
-    this.$viewContent = $sidebar.siblings('[data-behavior~=view-content]');
-    this.storageKey = $sidebar.data('storage-key');
     this.minBreakpoint = 992;
+    this.$navbarBrand = $sidebar
+      .siblings('[data-behavior~=navbar]')
+      .find('[data-behavior~=navbar-brand]');
+    this.storageKey = $sidebar.data('storage-key');
+    this.$viewContent = $sidebar.siblings('[data-behavior~=view-content]');
 
     this.init();
   }
 
   Sidebar.prototype = {
-    init: function() {
+    init: function () {
       this.toggle(this.isSidebarOpen());
 
       var that = this;
 
-      $('[data-behavior~=sidebar-toggle]').on('click', function(e) {
+      $('[data-behavior~=sidebar-toggle]').on('click', function (e) {
         if ($(this).is('[data-behavior~=nodes-tree]')) {
           if (window.innerWidth < that.minBreakpoint) {
             $(this).attr('data-behavior', 'nodes-tree sidebar-toggle');
           } else {
-            $(this).attr('data-behavior', 'nodes-tree sidebar-toggle open-only');
+            $(this).attr(
+              'data-behavior',
+              'nodes-tree sidebar-toggle open-only'
+            );
           }
         }
 
-        if (!(that.isSidebarOpen() && $(this).is('[data-behavior~=open-only]'))) {
+        if (
+          !(that.isSidebarOpen() && $(this).is('[data-behavior~=open-only]'))
+        ) {
           that.toggle(!that.isSidebarOpen());
         }
       });
 
       if (window.innerWidth < that.minBreakpoint) {
-        $('[data-behavior~=sidebar-node-link]').on('click', function() {
+        $('[data-behavior~=sidebar-node-link]').on('click', function () {
           that.close();
         });
       }
     },
-    changeState: function(state) {
+    changeState: function (state) {
       localStorage.setItem(this.storageKey, state);
       Turbolinks.clearCache();
     },
-    close: function() {
+    close: function () {
       this.$navbarBrand.css('padding-left', 0);
       this.$sidebar
         .removeClass('sidebar-expanded')
         .addClass('sidebar-collapsed');
-      this.$viewContent.css({'left': this.$sidebar.css('width'), 'width': 'calc(100vw - ' + this.$sidebar.css('width') + ')'});
+      this.$viewContent.css({
+        left: this.$sidebar.css('width'),
+        width: 'calc(100vw - ' + this.$sidebar.css('width') + ')',
+      });
 
       this.changeState(false);
     },
-    isSidebarOpen: function() {
+    isSidebarOpen: function () {
       return JSON.parse(localStorage.getItem(this.storageKey));
     },
-    open: function() {
+    open: function () {
       this.$sidebar
         .removeClass('sidebar-collapsed')
         .addClass('sidebar-expanded');
-      this.$viewContent.css({'left': this.$sidebar.css('width'), 'width': 'calc(100vw - ' + this.$sidebar.css('width') + ')'});
+      this.$viewContent.css({
+        left: this.$sidebar.css('width'),
+        width: 'calc(100vw - ' + this.$sidebar.css('width') + ')',
+      });
 
       if (window.innerWidth > this.minBreakpoint) {
-        var navbarBrandOffset = parseFloat(this.$navbarBrand.css('padding-left').slice(0,-2)) + parseFloat(this.$sidebar.css('width').slice(0,-2) / 1.65);
+        var navbarBrandOffset =
+          parseFloat(this.$navbarBrand.css('padding-left').slice(0, -2)) +
+          parseFloat(this.$sidebar.css('width').slice(0, -2) / 1.65);
         this.$navbarBrand.css('padding-left', navbarBrandOffset);
       }
 
       this.changeState(true);
     },
-    toggle: function(openSidebar) {
+    toggle: function (openSidebar) {
       if (openSidebar) {
         this.open();
       } else {
         this.close();
       }
-    }
+    },
   };
 
   window.Sidebar = Sidebar;
