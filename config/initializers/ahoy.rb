c8563843945b5f0273d770613e94837231fb48b5
Caitlin Henry <caitlinhenry.ss@gmail.com>
2023-01-18 11:28:59 -0500

    add ahoy gem and migration

---
+++ config/initializers/ahoy.rb
@@ -0,0 +1,12 @@
+class Ahoy::Store < Ahoy::DatabaseStore
+end
+
+# set to true for JavaScript tracking
+Ahoy.api = false
+
+# set to true for geocoding (and add the geocoder gem to your Gemfile)
+# we recommend configuring local geocoding as well
+# see https://github.com/ankane/ahoy#geocoding
+Ahoy.geocode = false
+Ahoy.mask_ips = true
+Ahoy.cookies = false
