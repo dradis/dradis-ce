5c3a21426a2da55550fc011de455505ffb2448f3
Caitlin Henry <caitlinhenry.ss@gmail.com>
2023-01-19 12:00:07 -0500

    only create visits as needed and bump visit duration to 24 hours

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -3,10 +3,12 @@ end
 
 # set to true for JavaScript tracking
 Ahoy.api = false
-
 # set to true for geocoding (and add the geocoder gem to your Gemfile)
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
 Ahoy.geocode = false
+
 Ahoy.mask_ips = true
 Ahoy.cookies = false
+Ahoy.server_side_visits = :when_needed
+Ahoy.visit_duration = 24.hours
