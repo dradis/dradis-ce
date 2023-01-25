bc957b1d8c2e863c96388041ae6c5c6ff3b4173d
Caitlin <caitlinhenry.ss@gmail.com>
2023-01-25 13:33:25 -0500

    re-lint

--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,20 @@
 require_relative 'boot'
 
 # Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'active_storage/engine'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-# require 'action_mailbox/engine'
-# require 'action_text/engine'
-require 'action_view/railtie'
-require 'action_cable/engine'
-require 'sprockets/railtie'
-# require 'rails/test_unit/railtie'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
-require 'resque/server'
+require "resque/server"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
