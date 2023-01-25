3cb7913920add3553bbed229b0259b3a45c9b80d
Caitlin <caitlinhenry.ss@gmail.com>
2023-01-25 13:39:49 -0500

    rubocop

--- spec/factories/visits.rb
+++ spec/factories/visits.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-    factory :visit, :class => Ahoy::Visit do
-      visit_token { SecureRandom.uuid }
-      started_at { Date.today }
-    end
+  factory :visit, class: Ahoy::Visit do
+    visit_token { SecureRandom.uuid }
+    started_at { Date.today }
+  end
 end
--- spec/jobs/event_tracking_job_spec.rb
+++ spec/jobs/event_tracking_job_spec.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe EventTrackingJob do
   before do
     @visit = create(:visit)
   end
-  
+
   describe '#perform' do
     it 'creates an ahoy_event entry' do
       described_class.new.perform(visit: @visit, event_name: 'Test Event', properties: {})
