d00ee9972724ac206b956c4a856e81ac8bfddb22
Caitlin <caitlinhenry.ss@gmail.com>
2023-01-26 12:40:02 -0500

    lint

--- spec/factories/configurations.rb
+++ spec/factories/configurations.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :configuration do
     sequence(:name) { |n| "configuration#{n}" }
-    value { "Factory-generated value" }
+    value { 'Factory-generated value' }
 
     trait :analytics_config do
       name { 'admin:analytics' }
