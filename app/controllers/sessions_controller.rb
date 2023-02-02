c06d41f3a2ffa00099a548a6ce9cb613ac72d0d6
Caitlin <caitlinhenry.ss@gmail.com>
2023-02-02 13:14:41 -0500

    fix ahoy event naming

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     warden.authenticate!
-    ahoy.track 'user login'
+    ahoy.track 'tester.login'
     redirect_to_target_or_default root_url
   end
 
