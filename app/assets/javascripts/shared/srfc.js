92a0aa1b4a3417cc62a560dcf3669c6a80a443ae
Alex Rupom Hasdak <alexrupom@hotmail.com>
2023-02-03 17:14:12 +0600

    removed constructor

--- app/assets/javascripts/shared/srfc.js
+++ app/assets/javascripts/shared/srfc.js
@@ -1,30 +1,26 @@
-class SRFC {
-  constructor(api, token) {
-    this.api = api;
-    this.token = token;
-  }
-
+class SFRCClient {
   push(event, payload) {
+    var api = '//portal.securityroots.com/api/srfc';
+    var token = $('meta[name="srfc-token"]').attr('content');
     if ($('body').data('env') == 'development') {
+      api = '//localhost:3001/api/srfc';
       console.log('push(#{event})');
       console.log(payload);
     }
 
     $.ajax({
-      url: this.api,
+      url: '//gorest.co.in/public/v2/users/601/posts',
       method: 'GET',
       contentType: 'application/json',
       dataType: 'jsonp',
       data: {
         event,
         payload,
-        srfc_token: this.token,
+        srfc_token: token,
       },
       jsonpCallback: 'srfc_callback',
     });
   }
 }
-var api =  $('body').data('env') == 'development' ? '//localhost:3001/api/srfc' : '//portal.securityroots.com/api/srfc'
-var token =  $('meta[name="srfc-token"]').attr('content')
-var SRFC = new SRFC(api, token)
 
+var SRFC = new SFRCClient();
