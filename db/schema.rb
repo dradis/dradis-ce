d1b0c84fa419ec92eaecd18b4c0c9a8a75966e14
Aaron Manaloto <mail@aapomm.io>
2023-02-13 15:32:44 +0800

    Run app:update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_202870) do
+ActiveRecord::Schema[6.1].define(version: 2021_03_16_202870) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
