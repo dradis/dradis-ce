6ab09ea4d3b4b451429232fd4410914475c7f36f
Aaron Manaloto <mail@aapomm.io>
2023-03-03 17:40:27 +0800

    Appease rubocop

--- spec/factories/issues.rb
+++ spec/factories/issues.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :issue do
-    sequence(:text){ |n| "#[Title]#\nRspec multiple Apache bugs #{n}\n\n#[Description]#\nFoo" }
-    author { "factory_bot" }
+    sequence(:text) { |n| "#[Title]#\nRspec multiple Apache bugs #{n}\n\n#[Description]#\nFoo" }
+    author { 'factory_bot' }
     category { Category.issue }
     node { Project.new.issue_library }
     state { :published }
