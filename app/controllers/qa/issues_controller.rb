fe810b558c6276374a9a8d92e7fe32f807619932
Matt Budz <matt.budziosz@gmail.com>
2023-03-08 14:50:16 +0100

    update controller to show issues in sidebar

--- app/controllers/qa/issues_controller.rb
+++ app/controllers/qa/issues_controller.rb
@@ -2,12 +2,19 @@ class QA::IssuesController < AuthenticatedController
   include LiquidEnabledResource
   include ProjectScoped
 
+  before_action :set_issues
+
   def index
-    @issues = current_project.issues.ready_for_review
     @all_columns = ['Title']
   end
 
   def show
     @issue = current_project.issues.find(params[:id])
+  end
+
+  private
+
+  def set_issues
+    @issues = current_project.issues.ready_for_review
   end
 end
