0c6faa278b4ae1182c07fbcafaed4790b40777cd
Matt Budz <matt.budziosz@gmail.com>
2023-03-09 14:57:04 +0100

    add specs

--- spec/support/qa_shared_examples.rb
+++ spec/support/qa_shared_examples.rb
@@ -7,9 +7,59 @@ shared_examples 'qa pages' do |item_type|
 
     it 'lists all the records that are ready for review' do
       records.each do |record|
-        expect(page).to have_text(record.title)
+        record_qa_path = polymorphic_path([current_project, :qa, record])
+        expect(page).to have_link(record.title, href: record_qa_path)
       end
+    end
+
+    it 'redirects the user back after updating the record' do
+      click_link 'Edit', match: :first
+
+      expect(current_path).to eq polymorphic_path([:edit, current_project, records.first])
+
+      click_button "Update #{item_type.to_s.humanize}"
+
+      expect(current_path).to eq polymorphic_path([current_project, :qa, item_type.to_s.pluralize.to_sym])
+      expect(page).to have_selector('.alert-success', text: 'Issue updated.')
     end
   end
 
+  describe 'show page' do
+    before do
+      visit polymorphic_path([current_project, :qa, records.first])
+    end
+
+    it 'shows the record\'s content' do
+      expect(page).to have_content(records.first.title)
+    end
+
+    it 'redirects the user back after updating the record' do
+      within '.note-text-inner' do
+        click_link 'Edit'
+      end
+
+      expect(current_path).to eq polymorphic_path([:edit, current_project, records.first])
+
+      click_button "Update #{item_type.to_s.humanize}"
+
+      expect(current_path).to eq polymorphic_path([current_project, :qa, records.first])
+      expect(page).to have_selector('.alert-success', text: 'Issue updated.')
+    end
+
+    it 'updates the state to draft' do
+      click_button 'Draft'
+
+      expect(current_path).to eq polymorphic_path([current_project, :qa, item_type.to_s.pluralize.to_sym])
+      expect(page).to have_selector('.alert-success', text: 'State updated successfully.')
+      expect(records.first.reload.draft?).to eq true
+    end
+
+    it 'updates the state to published' do
+      click_button 'Published'
+
+      expect(current_path).to eq polymorphic_path([current_project, :qa, item_type.to_s.pluralize.to_sym])
+      expect(page).to have_selector('.alert-success', text: 'State updated successfully.')
+      expect(records.first.reload.published?).to eq true
+    end
+  end
 end
