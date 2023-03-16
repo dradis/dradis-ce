9f4d75384258c883f9f4af45f3515e9ca2762fba
Matt Budz <matt.budziosz@gmail.com>
2023-03-16 16:45:14 +0100

    update data-state attribute on state selection

--- app/assets/javascripts/tylium/modules/state_button.js
+++ app/assets/javascripts/tylium/modules/state_button.js
@@ -1,19 +1,20 @@
 document.addEventListener('turbolinks:load', function () {
   if ($('[data-behavior~=state-radio]').length) {
-    function updateBtnText($selectedRadio) {
+    function updateBtn($selectedRadio) {
       var selectedState = $selectedRadio
         .parent()
         .find('[data-behavior~=state-label]');
 
-      $selectedRadio
+      var $stateBtn = $selectedRadio
         .closest('[data-behavior~=btn-states]')
-        .find('[data-behavior~=state-button]')
-        .text(selectedState.text());
+        .find('[data-behavior~=state-button]');
+
+      $stateBtn.text(selectedState.text());
+      $stateBtn.parent().attr('data-state', $selectedRadio.val());
     }
 
     $('[data-behavior~=state-radio]').on('change', function () {
-      var $selectedRadio = $(this);
-      updateBtnText($selectedRadio);
+      updateBtn($(this));
     });
   }
 });
