9410d4fd70c47cffbfc1a5ecc55b0624002ce387
Aaron Manaloto <mail@aapomm.io>
2023-03-21 19:48:06 +0800

    Use published scope in TagDrop

--- app/drops/tag_drop.rb
+++ app/drops/tag_drop.rb
@@ -2,6 +2,8 @@ class TagDrop < BaseDrop
   delegate :color, :display_name, :name, to: :@record
 
   def tag_issues
-    Issue.includes(:taggings).where(taggings: {tag: @record}).map { |issue| IssueDrop.new(issue) }
+    Issue.published.includes(:taggings).where(taggings: {tag: @record}).map do |issue|
+      IssueDrop.new(issue)
+    end
   end
 end
