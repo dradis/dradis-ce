b5c324b244fe4d38426018d04fe12075090bc83a
Aaron Manaloto <mail@aapomm.io>
2023-03-21 21:38:19 +0800

    Appease rubocop

--- app/drops/tag_drop.rb
+++ app/drops/tag_drop.rb
@@ -2,7 +2,7 @@ class TagDrop < BaseDrop
   delegate :color, :display_name, :name, to: :@record
 
   def tag_issues
-    Issue.published.includes(:taggings).where(taggings: {tag: @record}).map do |issue|
+    Issue.published.includes(:taggings).where(taggings: { tag: @record }).map do |issue|
       IssueDrop.new(issue)
     end
   end
