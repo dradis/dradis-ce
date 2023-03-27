d01c02b531bc797b52992025e28874ac6a585aa4
Matt Budz <matt.budziosz@gmail.com>
2023-03-27 12:13:35 +0200

    make the js more readable

--- app/assets/javascripts/tylium/pages/qa.js
+++ app/assets/javascripts/tylium/pages/qa.js
@@ -1,12 +1,7 @@
 document.addEventListener('turbolinks:load', function () {
   if ($('[data-behavior~=qa-viewer]').length) {
     $('[data-qa-visible]').each(function () {
-      if ($(this).data('qa-visible') === true) {
-        $(this).removeClass('d-none');
-        $(this).prev().addClass('d-none');
-      } else {
-        $(this).addClass('d-none');
-      }
+      $(this).toggleClass('d-none', $(this).data('qa-visible') === false);
     });
   }
 });
