476414a87588b25f46137d1aacb6e69e0387ed1b
Caitlin <caitlinhenry.ss@gmail.com>
2023-05-05 16:10:25 -0400

    remove liquid_resource_assigns from notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -82,13 +82,6 @@ class NotesController < NestedNodeResourceController
     end
   end
 
-  def liquid_resource_assigns
-    {
-      'note' => NoteDrop.new(@note),
-      'node' => NodeDrop.new(@note.node)
-    }
-  end
-
   def note_params
     params.require(:note).permit(:category_id, :text, :node_id)
   end
