2e870336ceb003691341da5c56a4dd08f84327ce
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2023-05-09 10:24:52 +0200

    Fix Boards Factory - associate with the right project
    Before this PR, a Board created via the factory could end up associated
    with a Node in a different project. We used create(:project) inside the
    node association which was independent from the board's own :project
    association.
    
    By using an after-build callback we can ensure that the node belongs to
    the same project already setup via the :project association.
    
    We guard this assignment to allow the caller to specify a different node
    (e.g. a host in the project).

--- spec/factories/boards.rb
+++ spec/factories/boards.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :board do
     sequence(:name){ |n| "Board-#{n}" }
-    node { create(:project).methodology_library }
-
     association :project
+
+    after(:build) do |board|
+      board.node = board.project.methodology_library unless board.node.present?
+    end
   end
 end
