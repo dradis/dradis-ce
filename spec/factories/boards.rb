e1e8ef8d910442fa66b2615649d474bf93f26d40
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2023-05-09 10:30:51 +0200

    Rubocop'ing

--- spec/factories/boards.rb
+++ spec/factories/boards.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :board do
-    sequence(:name){ |n| "Board-#{n}" }
+    sequence(:name) { |n| "Board-#{n}" }
     association :project
 
     after(:build) do |board|
