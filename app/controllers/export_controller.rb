d6e610e62f04f68728b444a8ce7b02dd5dc38a72
Caitlin <caitlinhenry.ss@gmail.com>
2023-07-05 10:36:30 -0400

    removed unused validation code in export controller and associated routes

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -20,35 +20,6 @@ class ExportController < AuthenticatedController
     )
   end
 
-  # Runs a pre-export validation of the contents of the project
-  def validate
-    @validators = Dradis::Pro::Plugins::Export::Validators::BaseValidator.descendants
-
-    logger = Log.new
-    @log_uid = logger.uid
-
-    @job_id = ProjectValidator.create(
-      plugin: AdvancedWordExport.name,
-      template: params[:template],
-      uid: @log_uid
-    )
-
-    logger.write("Enqueueing pre-export validation job to start in the background. Job id is #{ @log_uid }")
-  end
-
-  def validation_status
-    @log_uid = params[:log_uid].to_i
-    @job_id  = params[:job_id]
-    @logs    = Log.where('uid = ? and id > ?', @log_uid, params[:after].to_i)
-
-    status = Resque::Plugins::Status::Hash.get(@job_id)
-    render json: status.reverse_merge({
-      logs: @logs,
-      validating: nil,
-      validators: []
-    }).to_json(root: false)
-  end
-
   private
 
   # The list of available Export plugins. See the dradis_plugins gem.
