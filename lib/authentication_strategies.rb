d6b4da41472c9d6d7d1a19601cafcdd434d0b1ba
Aaron Manaloto <mail@aapomm.io>
2023-09-26 18:47:01 +0800

    Use authentication strategies

--- lib/authentication_strategies.rb
+++ lib/authentication_strategies.rb
@@ -1,9 +1,9 @@
-class StrategyService
+class AuthenticationStrategies
   @@strategies = [:shared_password]
 
   class << self
     attr_accessor :strategies
   end
 
-  ActiveSupport.run_load_hooks(:strategy_service)
+  ActiveSupport.run_load_hooks(:authentication_strategies)
 end
