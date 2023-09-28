de4dce079d40bbc259adaa0a788de8ce168fe802
Aaron Manaloto <mail@aapomm.io>
2023-09-28 17:29:41 +0800

    Migrate to manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,13 @@
-//= link_directory ../stylesheets .css
+//= link application.css
+//= link setup/setup.css
+//= link tylium.css
 
 //= link legacy.application.js
+//= link tylium.js
 
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 
 //= link_tree ../images
+
+//= link_tree ../../../vendor/assets
