cf31cc863cb83caa1fda906f4ff4840de03b694a
Caitlin <caitlinhenry.ss@gmail.com>
2023-10-10 14:42:12 -0400

    lint

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 # Pin npm packages by running ./bin/importmap
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin 'jquery', to: 'https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js', preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 
 pin 'application', preload: true
 
