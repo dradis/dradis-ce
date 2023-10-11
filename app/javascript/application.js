64f34c1214750fbcc0ecc7013f4cb025aaacfc10
Caitlin <caitlinhenry.ss@gmail.com>
2023-10-11 13:28:58 -0400

    fix specs by removing extra jquery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-import 'jquery';
-window.jQuery = $;
-window.$ = $;
 import "@hotwired/turbo-rails"
 
 import 'datatables'
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,4 @@
 # Pin npm packages by running ./bin/importmap
-pin 'jquery', to: 'https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 
 pin 'application', preload: true
