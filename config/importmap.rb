2f29a261314557b93f02f01f985b6c358ffc91e2
Caitlin <caitlinhenry.ss@gmail.com>
2023-10-23 16:31:56 -0400

    include turbo-rails in vendor/javascript

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,7 @@
 # Pin npm packages by running ./bin/importmap
-pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin '@hotwired/turbo-rails', to: '@hotwired--turbo-rails.js', preload: true
+pin '@hotwired/turbo', to: '@hotwired--turbo.js', preload: true # @7.3.0
+pin '@rails/actioncable/src', to: '@rails--actioncable--src.js', preload: true # @7.1.1
 
 pin 'application', preload: true
 
