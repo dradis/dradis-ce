de7876fe6d2d063174d72369e003fdd2dd4b6798
Caitlin <caitlinhenry.ss@gmail.com>
2023-11-22 16:19:20 -0500

    remove extra liquid drops

--- app/controllers/concerns/liquid_enabled_resource.rb
+++ app/controllers/concerns/liquid_enabled_resource.rb
@@ -27,9 +27,7 @@ module LiquidEnabledResource
   def project_assigns
     # This is required because we may be in Markup#preview that's passing
     # :project_id for Tylium rendered editors
-    project = Project.includes(
-      :content_blocks, :evidence, :nodes, :notes, :tags, :team
-    ).find(params[:project_id])
+    project = Project.includes(:team).find(params[:project_id])
     authorize! :use, project
 
     LiquidAssignsService.new(project).assigns
--- app/services/liquid_assigns_service.rb
+++ app/services/liquid_assigns_service.rb
@@ -8,13 +8,8 @@ class LiquidAssignsService
   def assigns
     result = {}
     result['project'] = ProjectDrop.new(project)
-    result['issues'] = project.issues.map { |issue| IssueDrop.new(issue) }
-    result['nodes'] = project.nodes.user_nodes.map { |node| NodeDrop.new(node) }
-    result['evidences'] = project.evidence.map { |evidence| EvidenceDrop.new(evidence) }
-    result['tags'] = project.tags.map { |tag| TagDrop.new(tag) }
 
     if defined?(Dradis::Pro)
-      result['content_blocks'] = project.content_blocks.map { |cb| ContentBlockDrop.new(cb) }
       result['document_properties'] = DocumentPropertiesDrop.new(properties: project.content_library.properties)
       result['team'] = TeamDrop.new(project.team)
     end
