8b583203c193ebb0aa435c63e2def2c16a0bb8e5
Aaron Manaloto <mail@aapomm.io>
2023-11-22 17:37:08 +0800

    Add example for pro records

--- spec/services/liquid_assigns_service_spec.rb
+++ spec/services/liquid_assigns_service_spec.rb
@@ -17,4 +17,23 @@ RSpec.describe LiquidAssignsService do
     expect(liquid_assigns['evidences'].map(&:title)).to match_array(evidence.map(&:title))
     expect(liquid_assigns['tags'].map(&:name)).to match_array(tags.pluck(:name))
   end
+
+  context 'with pro records', skip: !defined?(Dradis::Pro)  do
+    let!(:project) { create(:project, :with_team) }
+    let!(:content_blocks) { create_list(:content_block, 5, project: project) }
+
+    before do
+      report_content = project.content_library
+      report_content.properties = { 'dradis.project' => project.name }
+      report_content.save
+    end
+
+    it 'builds a hash with Dradis::Pro assigns' do
+      content_assigns = liquid_assigns['content_blocks'].map { |c| c.fields['Title'] }
+
+      expect(content_assigns).to match_array(content_blocks.map { |c| c.fields['Title'] })
+      expect(liquid_assigns['document_properties'].available_properties).to eq({ 'dradis.project' => project.name })
+      expect(liquid_assigns['team'].name).to eq(project.team.name)
+    end
+  end
 end
