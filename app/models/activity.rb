e40190ef3bb61263b113146decccb501e4183e66
Kostas Sklias <>
2023-11-28 16:47:28 +0200

    Activity filtering basic functionality

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -26,6 +26,11 @@ class Activity < ApplicationRecord
   scope :latest, -> do
     includes(:trackable).order('activities.created_at DESC').limit(20)
   end
+  scope :filter_by_user_id, -> (id) { where user_id: id }
+  scope :filter_by_type, -> (type) { where trackable_type: type }
+  scope :filter_by_date, -> (period_start, period_end) {
+    where("created_at >= ? AND created_at <= ?", period_start.beginning_of_day, period_end.end_of_day )
+  }
 
   # -- Callbacks ------------------------------------------------------------
 
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -25,4 +25,72 @@ describe Activity do
     end
   end
 
+  describe "#filter_by_user_id" do
+    before do
+      issue = create(:issue)
+      @user = create(:user)
+      activity = create(:activity, trackable: issue, user: @user)
+      activity2 = create(:activity, trackable: issue, user: @user)
+      activity3 = create(:activity, trackable: issue)
+    end
+
+    context "when passed a valid user id" do
+      it "returns activities of this user" do
+        expect(Activity.filter_by_user_id(@user.id).count).to eq 2
+        expect(Activity.all.count).to eq 3
+      end
+    end
+
+    context "when passed a non valid user id" do
+      it "returns an empty collection" do
+        user_id = User.last.id + 1
+        expect(Activity.filter_by_user_id(user_id).count).to eq 0
+        expect(Activity.all.count).to eq 3
+      end
+    end
+  end
+
+  describe "#filter_by_type" do
+    before do
+      issue = create(:issue)
+      card = create(:card)
+      activity = create(:activity, trackable: issue)
+      activity2 = create(:activity, trackable: card)
+      activity3 = create(:activity, trackable: card)
+    end
+
+    context "when passed a valid type" do
+      it "returns activities of this user" do
+        expect(Activity.filter_by_type("Card").count).to eq 2
+        expect(Activity.all.count).to eq 3
+      end
+    end
+
+    context "when passed a non valid type" do
+      it "returns an empty collection" do
+        expect(Activity.filter_by_type("galaxy").count).to eq 0
+        expect(Activity.all.count).to eq 3
+      end
+    end
+  end
+
+  describe "#filter_by_date" do
+    before do
+      issue = create(:issue)
+      activity = create(:activity, trackable: issue)
+      activity2 = create(:activity, trackable: issue, created_at: DateTime.now.beginning_of_year - 1.year)
+      activity3 = create(:activity, trackable: issue, created_at: DateTime.now.beginning_of_year - 1.year)
+    end
+
+    context "when passed a valid date" do
+      it "returns activities within the date" do
+        year_start = DateTime.now.beginning_of_year
+        last_year_start = year_start - 1.year
+        period_end = DateTime.now.end_of_day
+
+        expect(Activity.filter_by_date(year_start, period_end).count).to eq 1
+        expect(Activity.filter_by_date(last_year_start, period_end).count).to eq 3
+      end
+    end
+  end
 end
