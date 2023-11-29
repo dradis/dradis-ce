c5628bf4f68b89dabebca5c1a64c7e234d68da3e
Caitlin <caitlinhenry.ss@gmail.com>
2023-11-29 13:07:58 -0500

    Prevent creating notifications for subscribed users that no longer have access to the resource

--- app/models/concerns/notifiable.rb
+++ app/models/concerns/notifiable.rb
@@ -8,12 +8,14 @@ module Notifiable
   def create_notifications(action:, actor:, recipients:)
     ActiveRecord::Base.transaction do
       recipients.each do |recipient|
-        Notification.create(
-          action: action,
-          actor: actor,
-          notifiable: self,
-          recipient: recipient
-        )
+        if Ability.new(recipient).can?(:read, self)
+          Notification.create(
+            action: action,
+            actor: actor,
+            notifiable: self,
+            recipient: recipient
+          )
+        end
       end
     end
   end
