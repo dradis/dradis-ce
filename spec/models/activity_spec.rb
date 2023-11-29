a57fa649d8267baa6c2b693a4c6ebeb1a77c1e7f
Kostas Sklias <>
2023-11-29 13:55:18 +0200

    Rubocop fixes

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -11,21 +11,21 @@ describe Activity do
 
   it { should validate_inclusion_of(:action).in_array %i[create update destroy] }
 
-  describe "#trackable=" do
-    context "when passed an Issue" do
-      it "sets trackable_type as Issue, not Note" do
+  describe '#trackable=' do
+    context 'when passed an Issue' do
+      it 'sets trackable_type as Issue, not Note' do
         # Default Rails behaviour is to set trackable_type to 'Note' when you
         # pass an Issue, meaning that it gets loaded as a Note, not an Issue,
         # when you call #trackable later.
         issue    = create(:issue)
         activity = create(:activity, trackable: issue)
-        expect(activity.trackable_type).to eq "Issue"
+        expect(activity.trackable_type).to eq 'Issue'
         expect(activity.reload.trackable).to eq issue
       end
     end
   end
 
-  describe "#filter_by_user_id" do
+  describe '#filter_by_user_id' do
     before do
       issue = create(:issue)
       @user = create(:user)
@@ -34,15 +34,15 @@ describe Activity do
       activity3 = create(:activity, trackable: issue)
     end
 
-    context "when passed a valid user id" do
-      it "returns activities of this user" do
+    context 'when passed a valid user id' do
+      it 'returns activities of this user' do
         expect(Activity.filter_by_user_id(@user.id).count).to eq 2
         expect(Activity.all.count).to eq 3
       end
     end
 
-    context "when passed a non valid user id" do
-      it "returns an empty collection" do
+    context 'when passed a non valid user id' do
+      it 'returns an empty collection' do
         user_id = User.last.id + 1
         expect(Activity.filter_by_user_id(user_id).count).to eq 0
         expect(Activity.all.count).to eq 3
@@ -50,7 +50,7 @@ describe Activity do
     end
   end
 
-  describe "#filter_by_type" do
+  describe '#filter_by_type' do
     before do
       issue = create(:issue)
       card = create(:card)
@@ -59,22 +59,22 @@ describe Activity do
       activity3 = create(:activity, trackable: card)
     end
 
-    context "when passed a valid type" do
-      it "returns activities of this user" do
-        expect(Activity.filter_by_type("Card").count).to eq 2
+    context 'when passed a valid type' do
+      it 'returns activities of this user' do
+        expect(Activity.filter_by_type('Card').count).to eq 2
         expect(Activity.all.count).to eq 3
       end
     end
 
-    context "when passed a non valid type" do
-      it "returns an empty collection" do
-        expect(Activity.filter_by_type("galaxy").count).to eq 0
+    context 'when passed a non valid type' do
+      it 'returns an empty collection' do
+        expect(Activity.filter_by_type('galaxy').count).to eq 0
         expect(Activity.all.count).to eq 3
       end
     end
   end
 
-  describe "#filter_by_date" do
+  describe '#filter_by_date' do
     before do
       issue = create(:issue)
       activity = create(:activity, trackable: issue)
@@ -82,8 +82,8 @@ describe Activity do
       activity3 = create(:activity, trackable: issue, created_at: DateTime.now.beginning_of_year - 1.year)
     end
 
-    context "when passed a valid date" do
-      it "returns activities within the date" do
+    context 'when passed a valid date' do
+      it 'returns activities within the date' do
         year_start = DateTime.now.beginning_of_year
         last_year_start = year_start - 1.year
         period_end = DateTime.now.end_of_day
