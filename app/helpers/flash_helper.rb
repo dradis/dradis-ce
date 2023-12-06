d0a8d952bb8b50155e24cbfff665109b02cb8326
Caitlin <caitlinhenry.ss@gmail.com>
2023-12-06 10:53:35 -0500

    add flash helper to store alert types and flash attributes

---
+++ app/helpers/flash_helper.rb
@@ -0,0 +1,18 @@
+module FlashHelper
+  # In general controllers use :error, but :alert is used with redirect_to
+  # http://guides.rubyonrails.org/action_controller_overview.html#the-flash
+  ALERT_TYPES = {
+    'alert'   => 'alert-danger',
+    'error'   => 'alert-danger',
+    'info'    => 'alert-info',
+    'notice'  => 'alert-success',
+    'warning' => 'alert-warning'
+  }
+
+  def flash_attrs(name)
+    attrs = {}
+    attrs[:flash_css] = "alert #{ALERT_TYPES.fetch(name)} alert-dismissible"
+    attrs[:data_attrs] = { bs_dismiss: 'alert' }
+    attrs
+  end
+end
