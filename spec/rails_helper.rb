6cd67825ba48444fdad272adde2efae1d79848c8
Aaron Manaloto <mail@aapomm.io>
2024-02-20 20:56:16 +0800

    Remove window size option

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,7 @@ end
 ActiveRecord::Migration.maintain_test_schema!
 
 Capybara.register_driver :firefox do |app|
-  options = %w[--headless --disable-gpu --width=1920 --height=1080]
+  options = %w[--headless --disable-gpu]
   Capybara::Selenium::Driver.new(
     app,
     browser: :firefox,
