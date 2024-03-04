7d20aff95b5cd5dc05e9387eeb3b0ced85df0aa4
Caitlin <caitlinhenry.ss@gmail.com>
2024-03-04 18:01:18 -0500

    use hard-coded source mapping for burp and qualys to update template naming

--- app/services/mapping_migration_service.rb
+++ app/services/mapping_migration_service.rb
@@ -1,21 +1,22 @@
 class MappingMigrationService
   LEGACY_TEMPLATE_REGEX = /%(\S*?)%/
-  attr_reader :integration_name, :rtp_id, :template_file
+  attr_reader :integration_name, :rtp_id
 
   def call
     upload_integrations = Dradis::Plugins::with_feature(:upload)
+    upload_integrations = upload_integrations - [
+      Dradis::Plugins::Projects::Engine, Dradis::Plugins::CSV::Engine
+    ]
 
     upload_integrations.each do |integration|
       @integration_name = integration.plugin_name.to_s
 
-      template_files.each do |template_file|
-        @template_file = template_file
-
-        rtp_ids = defined?(Dradis::Pro) ? ReportTemplateProperties.ids : [nil]
-        rtp_ids.each do |rtp_id|
-          @rtp_id = rtp_id
-          migrate
-          File.rename template_file, "#{template_file}.legacy"
+      if integration.uploaders.count > 1
+        migrate_multiple_uploaders(integration_name)
+      else
+        template_files.each do |template_file|
+          mapping_source = File.basename(template_file, '.template')
+          migrate(template_file, mapping_source)
         end
       end
     end
@@ -23,8 +24,7 @@ class MappingMigrationService
 
   private
 
-  def create_mapping
-    mapping_source = File.basename(template_file, '.template')
+  def create_mapping(template_file, mapping_source)
     destination = rtp_id ? "rtp_#{rtp_id}" : nil
 
     Mapping.find_or_create_by!(
@@ -42,36 +42,75 @@ class MappingMigrationService
     )
   end
 
-  def migrate
-    # for each file, create a mapping for the uploader&plugin_name combination
-    ActiveRecord::Base.transaction do
-      mapping = create_mapping
+  def migrate_multiple_uploaders(integration_name)
+    source_mapping = self.send("#{integration_name}_source_mapping")
+    integration_templates_dir = File.join(@templates_dir, integration_name)
 
-      template_fields.each do |field_title, field_content|
-        # set source_field by taking the first match to the existing %% syntax
-        source_field = field_content.match(LEGACY_TEMPLATE_REGEX)
-        @source_field =
-          if source_field && !source_field[1].empty?
-            source_field[1]
-          else
-            'custom text'
-          end
-        @updated_content = update_syntax(field_content)
-
-        # create a mapping field for each field in the .template file
-        create_mapping_field(mapping, field_title)
+    source_mapping.each do |source_field, legacy_name|
+      template_file = Dir["#{integration_templates_dir}/#{legacy_name}.template*"]
+      if template_file.any? { |file| File.exist?(file) }
+        migrate(template_file[0], source_field)
       end
     end
   end
 
-  def template_fields
+  def burp_source_mapping
+    # create a mapping for each legacy template type:
+    {
+      'html_evidence' => 'html_evidence',
+      'html_issue' => 'issue',
+      'xml_evidence' => 'evidence',
+      'xml_issue' => 'issue'
+    }
+  end
+
+  def qualys_source_mapping
+    # create a mapping for each legacy template type:
+    {
+      'asset_evidence' => 'asset-evidence',
+      'asset_issue' => 'asset-issue',
+      'vuln_evidence' => 'evidence',
+      'vuln_element' => 'element',
+      'was_evidence' => 'was-evidence',
+      'was_issue' => 'was-issue'
+    }
+  end
+
+  def migrate(template_file, mapping_source)
+    rtp_ids = defined?(Dradis::Pro) ? ReportTemplateProperties.ids : [nil]
+    rtp_ids.each do |rtp_id|
+      @rtp_id = rtp_id
+      # for each file, create a mapping for the uploader&plugin_name combination
+      ActiveRecord::Base.transaction do
+        mapping = create_mapping(template_file, mapping_source)
+        template_fields = template_fields(template_file)
+        template_fields.each do |field_title, field_content|
+          # set source_field by taking the first match to the existing %% syntax
+          source_field = field_content.match(LEGACY_TEMPLATE_REGEX)
+          @source_field =
+            if source_field && !source_field[1].empty?
+              source_field[1]
+            else
+              'custom text'
+            end
+          @updated_content = update_syntax(field_content)
+
+          # create a mapping field for each field in the .template file
+          create_mapping_field(mapping, field_title)
+        end
+      end
+    end
+    File.rename template_file, "#{template_file}.legacy"
+  end
+
+  def template_fields(template_file)
     template_content = File.read(template_file)
     FieldParser.source_to_fields(template_content)
   end
 
   def template_files
-    templates_dir = Configuration.paths_templates_plugins
-    plugin_templates_dir = File.join(templates_dir, integration_name)
+    @templates_dir = Configuration.paths_templates_plugins
+    plugin_templates_dir = File.join(@templates_dir, integration_name)
     Dir["#{plugin_templates_dir}/*.template"]
   end
 
