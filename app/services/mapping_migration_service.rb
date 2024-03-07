6251206d8988ee48185a699e4c60f98c7351c797
Caitlin <caitlinhenry.ss@gmail.com>
2024-03-07 10:03:46 -0500

    move shared code to separate method

--- app/services/mapping_migration_service.rb
+++ app/services/mapping_migration_service.rb
@@ -3,17 +3,14 @@ class MappingMigrationService
   attr_reader :integration_name, :rtp_id
 
   def call
-    upload_integrations = Dradis::Plugins::with_feature(:upload)
-    upload_integrations = upload_integrations - [
-      Dradis::Plugins::Projects::Engine, Dradis::Plugins::CSV::Engine
-    ]
-
     upload_integrations.each do |integration|
       @integration_name = integration.plugin_name.to_s
+      set_integration_template_dirs
 
       if integration.uploaders.count > 1
         migrate_multiple_upload_integration(integration)
       else
+        integration_template_files = Dir["#{@integration_templates_dir}/*.template"]
         integration_template_files.each do |template_file|
           mapping_source = File.basename(template_file, '.template')
           # create a mapping & mapping_fields for each field in the file
@@ -24,19 +21,13 @@ class MappingMigrationService
   end
 
   def reverse_migration
-    upload_integrations = Dradis::Plugins::with_feature(:upload)
-    upload_integrations = upload_integrations - [
-      Dradis::Plugins::Projects::Engine, Dradis::Plugins::CSV::Engine
-    ]
-    templates_dir = Configuration.paths_templates_plugins
-
     upload_integrations.each do |integration|
-      integration_name = integration.plugin_name.to_s
+      @integration_name = integration.plugin_name.to_s
+      set_integration_template_dirs
 
       Mapping.where(component: integration_name).destroy_all
 
-      plugin_templates_dir = File.join(templates_dir, integration_name)
-      legacy_files = Dir["#{plugin_templates_dir}/*.template.legacy"]
+      legacy_files = Dir["#{@integration_templates_dir}/*.template.legacy"]
 
       legacy_files.each do |file|
         File.rename file, file.split('.legacy').first
@@ -89,16 +80,16 @@ class MappingMigrationService
   end
 
   # previously our integrations with multiple uploaders (Burp, Qualys) had inconsistent
-  # template names (some included the uploader, some didn't)
+  # template names (some included the uploader, some didn't ex. burp issue vs html_evidence)
   # they have been renamed to follow a consistent 'uploader_entity' structure, but
   # in order to migrate the old templates to the db with the new names as the source
   # we need to reference an object in the integration that maps the new name to the old one
   def migrate_multiple_upload_integration(integration)
+    # pull legacy mapping reference from Burp & Qualys
     legacy_mapping_reference = integration.module_parent::Mapping.legacy_mapping_reference
-    integration_templates_dir = File.join(@templates_dir, integration_name)
 
     legacy_mapping_reference.each do |source_field, legacy_template_name|
-      template_file = Dir["#{integration_templates_dir}/#{legacy_template_name}.template*"]
+      template_file = Dir["#{@integration_templates_dir}/#{legacy_template_name}.template*"]
       if template_file.any? { |file| File.exist?(file) }
         migrate(template_file[0], source_field)
       end
@@ -110,10 +101,9 @@ class MappingMigrationService
     FieldParser.source_to_fields(template_content)
   end
 
-  def integration_template_files
+  def set_integration_template_dirs
     @templates_dir = Configuration.paths_templates_plugins
-    plugin_templates_dir = File.join(@templates_dir, integration_name)
-    Dir["#{plugin_templates_dir}/*.template"]
+    @integration_templates_dir = File.join(@templates_dir, integration_name)
   end
 
   def update_syntax(field_content)
@@ -122,5 +112,12 @@ class MappingMigrationService
     field_content.gsub(LEGACY_FIELDS_REGEX) do |content|
       "{{ #{integration_name}[#{content[1..-2]}] }}"
     end
+  end
+
+  def upload_integrations
+    upload_integrations = Dradis::Plugins::with_feature(:upload)
+    upload_integrations - [
+      Dradis::Plugins::Projects::Engine, Dradis::Plugins::CSV::Engine
+    ]
   end
 end
