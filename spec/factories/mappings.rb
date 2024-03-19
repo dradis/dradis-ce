9bd2376236d0944cd64740b64564b61f4a87b478
Caitlin <caitlinhenry.ss@gmail.com>
2024-03-19 09:37:10 -0400

    remove pro only mapping in spec

--- spec/factories/mappings.rb
+++ spec/factories/mappings.rb
@@ -1,13 +1,7 @@
 FactoryBot.define do
   factory :mapping do
     component { 'qualys' }
-    sequence(:source) { |n| "source_#{n}" }
+    sequence(:source) { 'asset_evidence' }
     sequence(:destination) { |n| "rtp_#{n}" }
-
-    trait :export_integration do
-      component { 'jira' }
-      sequence(:source) { |n| "rtp_#{n}" }
-      sequence(:destination) { |n| "project_1_issuetype_#{n}" }
-    end
   end
 end
