8a1e95ac5cd5a26471e87004a0019e04a49484fc
Matt Budz <matt.budz@protonmail.com>
2024-03-25 12:38:39 +0100

    fix merge

--- app/assets/javascripts/tylium/pages/issues.js
+++ app/assets/javascripts/tylium/pages/issues.js
@@ -1,5 +1,10 @@
 document.addEventListener('turbolinks:load', function () {
   if ($('body.issues.index').length) {
+    $('[data-behavior~=issues-dropdown-toggle]').on('click', function (e) {
+      e.stopPropagation();
+      new bootstrap.Dropdown($('[data-name~=issues]')).show();
+    });
+
     $('[data-behavior~=import-dropdown-toggle]').on('click', function (e) {
       e.stopPropagation();
       $(
