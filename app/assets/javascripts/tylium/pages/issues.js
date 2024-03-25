74763e377c81c73d2205b7a99635784873fe40c9
Matt Budz <matt.budz@protonmail.com>
2024-03-25 12:32:36 +0100

    fix issues empty state link for importing from lib

--- app/assets/javascripts/tylium/pages/issues.js
+++ app/assets/javascripts/tylium/pages/issues.js
@@ -1,8 +1,11 @@
 document.addEventListener('turbolinks:load', function () {
   if ($('body.issues.index').length) {
-    $('[data-behavior~=issues-dropdown-toggle]').on('click', function (e) {
+    $('[data-behavior~=import-dropdown-toggle]').on('click', function (e) {
       e.stopPropagation();
-      new bootstrap.Dropdown($('[data-name~=issues]')).show();
+      $(
+        '[data-behavior~=import-box-header] [data-behavior~=toggle-chevron]'
+      ).toggleClass('fa-chevron-down fa-chevron-up');
+      $('[data-behavior~=import-box]').toggle();
     });
   }
 });
