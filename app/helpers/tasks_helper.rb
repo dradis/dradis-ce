a337a33864774e0e69833262a1c6cb81bbc9dabd
nicolachr <nicola.christou@gmail.com>
2024-08-13 11:16:59 +0300

    Add styling to tasks datable fields

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -4,11 +4,11 @@ module TasksHelper
 
     case task.due_date
     when Date.today
-      content_tag(:span, 'Today', class: 'due-today')
+      content_tag(:span, 'Today', class: 'badge-due-date badge bg-warning-dark')
     when Date.tomorrow
-      content_tag(:span, 'Tomorrow', class: 'due-tomorrow')
+      content_tag(:span, 'Tomorrow', class: 'badge-due-date badge bg-warning')
     else
-      due_date_class = task.due_date < Date.today ? 'due-overdue' : 'due-later'
+      due_date_class = task.due_date < Date.today ? 'badge-due-date badge bg-danger' : 'badge-due-date badge bg-success'
       content_tag(:span, task.due_date.strftime('%b %e'), class: due_date_class)
     end
   end
