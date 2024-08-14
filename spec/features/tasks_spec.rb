0e45cda03838d07d5ce2b32e47c9985041b6e57c
Matt Budz <matt.budz@protonmail.com>
2024-08-14 12:00:21 +0200

    add specs for my tasks view

---
+++ spec/features/tasks_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe 'My Tasks' do
+  subject { page }
+
+  it 'should require authenticated users' do
+    project = create(:project)
+    visit project_tasks_path(project)
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content('Access denied.')
+  end
+
+  context 'as authenticated user' do
+    before do
+      login_to_project_as_user
+
+      board = create(:board, project: current_project, node: current_project.methodology_library)
+      list = create(:list, board: board)
+      @card = create(:card, list: list)
+    end
+
+    context 'with assigned tasks' do
+      it 'renders the user\'s assigned tasks in a dataTable', js: true do
+        @card.assignees = [User.first]
+        @card.save
+
+        visit project_tasks_path(current_project)
+        expect(page).to have_selector('table.dataTable')
+        expect(page).to have_selector('td a', text: @card.name)
+      end
+    end
+
+    context 'without assigned tasks' do
+      it 'renders an empty state' do
+        @card.assignees = []
+        @card.save
+
+        visit project_tasks_path(current_project)
+        expect(page).not_to have_selector('table.dataTable')
+        expect(page).not_to have_selector('td a', text: @card.name)
+
+        expect(page).to have_content("You don't have any assigned tasks yet")
+      end
+    end
+  end
+
+end
