38cc0eb9e75002553b6021ad6eab22747368db3b
Matt Budz <matt.budz@protonmail.com>
2024-08-14 17:24:24 +0200

    refactor `due_date_badge` for consistency

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,14 +2,19 @@ module TasksHelper
   def due_date_badge(task)
     return unless task.due_date
 
-    case task.due_date
-    when Date.today
-      content_tag(:span, 'Today', class: 'badge-due-date badge bg-warning-dark')
-    when Date.tomorrow
-      content_tag(:span, 'Tomorrow', class: 'badge-due-date badge bg-warning')
-    else
-      due_date_class = task.due_date < Date.today ? 'badge-due-date badge bg-danger' : 'badge-due-date badge bg-success'
-      content_tag(:span, task.due_date.strftime('%b %e'), class: due_date_class)
+    badge_class =
+      case task.due_date
+      when Date.today
+        'bg-warning'
+      when ->(date) { date < Date.today }
+        'bg-danger'
+      else
+        'bg-success'
+      end
+
+    content_tag(:span, class: "badge-due-date badge #{badge_class}") do
+      content_tag(:i, nil, class: 'fa-regular fa-clock me-1') +
+      content_tag(:span, task.due_date.strftime('%b %e'))
     end
   end
 end
