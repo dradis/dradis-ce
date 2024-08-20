54533ca7cec7b99a76fe9f2bd02209ec01e5cda1
Matt Budz <matt.budz@protonmail.com>
2024-08-20 13:47:00 +0200

    improve parity with pro

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,43 @@
 class TasksController < AuthenticatedController
   include ProjectScoped
-  layout 'tylium'
-  def index
-    project_cards = current_project.methodology_library.boards.map(&:cards)[0]
 
-    @tasks = if project_cards
-      project_cards.filter_map { |card| card if card.assignees.include? current_user }
+  layout :set_layout
+  def index
+    @default_columns = ['Title', 'Methodology', 'Due Date', 'Assigned']
+
+    if params[:project_id].present?
+      cards = current_project.methodology_library.boards.map(&:cards)[0]
+      @local_storage_key = "project.ce.project_#{current_project.id}.tasks_datatable"
+    else
+      cards = current_user.cards
+      @default_columns.insert(1, 'Project')
+      @local_storage_key = 'project.pro.tasks_datatable'
+    end
+
+    @tasks = if cards
+      cards.filter_map { |card| card if card.assignees.include? current_user }
     else
       []
     end
+  end
+
+  protected
+
+  # ProjectScoped always calls current_project and set_nodes. We are overwriting
+  # it here to prevent errors in Pro when viewing tasks outside of projects.
+  def current_project
+    return if params[:project_id].blank?
+
+    super
+  end
+
+  def set_nodes
+    return if params[:project_id].blank?
+
+    super
+  end
+
+  def set_layout
+    params[:project_id].present? ? 'tylium' : 'mintcreek'
   end
 end
