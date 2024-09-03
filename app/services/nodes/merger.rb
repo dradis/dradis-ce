b0c114736e0c9bdd16ce50ca47beca004b811d9c
Caitlin <caitlinhenry.ss@gmail.com>
2024-09-03 11:02:55 -0400

    touch evidence on node merge & add affected_ids to issues#index table

--- app/services/nodes/merger.rb
+++ app/services/nodes/merger.rb
@@ -31,66 +31,70 @@ class Nodes::Merger
 
   private
 
-    attr_accessor :target_node, :source_node, :copied_attachments
+  attr_accessor :target_node, :source_node, :copied_attachments
 
-    DESCENDENT_RELATIONSHIPS = {
-      activities: :trackable_id,
-      children: :parent_id,
-      evidence: :node_id,
-      notes: :node_id
-    }.freeze
+  DESCENDENT_RELATIONSHIPS = {
+    activities: :trackable_id,
+    children: :parent_id,
+    evidence: :node_id,
+    notes: :node_id
+  }.freeze
 
-    def move_descendents
-      DESCENDENT_RELATIONSHIPS.each do |relation, column|
-        source_node.send(relation).update_all(column => target_node.id)
-      end
+  def move_descendents
+    DESCENDENT_RELATIONSHIPS.each do |relation, column|
+      # update_all doesn't update timestamps so we need to do it manually
+      source_node.send(relation).update_all(
+        column => target_node.id,
+        :updated_at => Time.current
+      )
     end
+  end
 
-    def reset_counter_caches
-      Node.reset_counters target_node.id, :children_count
-    end
+  def reset_counter_caches
+    Node.reset_counters target_node.id, :children_count
+  end
 
-    def update_properties
-      source_node.properties.each do |key, value|
-        case key.to_sym
-        when :services
-          value.each do |service|
-            data = service.merge(source: :merge)
+  def update_properties
+    source_node.properties.each do |key, value|
+      case key.to_sym
+      when :services
+        value.each do |service|
+          data = service.merge(source: :merge)
+          target_node.set_service data
+        end
+      when :services_extras
+        value.each do |protocol_port, extras|
+          protocol, port = protocol_port.split('/')
+
+          extras.each do |extra|
+            data = {
+              extra[:id] => extra[:output],
+              source: extra[:source],
+              port: port.to_i,
+              protocol: protocol
+            }
+
             target_node.set_service data
           end
-        when :services_extras
-          value.each do |protocol_port, extras|
-            protocol, port = protocol_port.split('/')
-
-            extras.each do |extra|
-              data = {
-                extra[:id] => extra[:output],
-                source: extra[:source],
-                port: port.to_i,
-                protocol: protocol
-              }
-
-              target_node.set_service data
-            end
-          end
-        else
-          target_node.set_property key, value
         end
-      end
-
-      target_node.save
-    end
-
-    def copy_attachments
-      self.copied_attachments = []
-
-      source_node.attachments.each do |attachment|
-        copied_attachments << attachment.copy_to(target_node)
+      else
+        target_node.set_property key, value
       end
     end
 
-    def undo_attachments_copy
-      return unless copied_attachments&.any?
-      copied_attachments.each(&:delete)
+    target_node.save
+  end
+
+  def copy_attachments
+    self.copied_attachments = []
+
+    source_node.attachments.each do |attachment|
+      copied_attachments << attachment.copy_to(target_node)
     end
+  end
+
+  def undo_attachments_copy
+    return unless copied_attachments&.any?
+    copied_attachments.each(&:delete)
+  end
 end
