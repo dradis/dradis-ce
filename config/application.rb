31657f82d19dc445fcbdaaafdadac734b9d4d410
Caitlin <caitlinhenry.ss@gmail.com>
2024-10-29 11:36:02 -0400

    rails 7.2 upgrade script config changes & test 7.1 framework defaults

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,11 @@ module Dradis
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -52,6 +57,9 @@ module Dradis
     # Override the default credentials lookup paths. See bin/rails credentials:help
     config.credentials.content_path = Rails.root.join('config', 'shared', 'credentials.yml.enc')
     config.credentials.key_path = Rails.root.join('config', 'shared', 'master.key')
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,11 +12,15 @@ Rails.application.configure do
   config.cache_classes = false
   config.action_view.cache_template_loading = true
 
-  # Eager loading loads your whole application. When running a single test locally,
-  # this probably isn't necessary. It's a good idea to do in a continuous integration
-  # system, or in some way before deploying your code.
+  # Eager loading loads your entire application. When running a single test locally,
+  # this is usually not necessary, and can slow down your test suite. However, it's
+  # recommended that you enable it in continuous integration systems to ensure eager
+  # loading is working properly before deploying your code.
   # config.eager_load = ENV["CI"].present?
   config.eager_load = false
+
+  # While tests run files are not watched, reloading is not necessary.
+  config.enable_reloading = false
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
@@ -25,7 +29,7 @@ Rails.application.configure do
   }
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
@@ -41,12 +45,18 @@ Rails.application.configure do
   # Disable active storage routes
   config.active_storage.draw_routes = false
 
+  # Disable caching for Action Mailer templates even if Action Controller
+  # caching is enabled.
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+
+  # Unlike controllers, the mailer instance doesn't have any context about the
+  # incoming request so you'll need to provide the :host parameter yourself.
+  # config.action_mailer.default_url_options = { host: 'www.example.com' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
@@ -64,4 +74,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Raise error when a before_action's only/except options reference missing actions.
+  config.action_controller.raise_on_missing_callback_actions = true
 end
