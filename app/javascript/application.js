db4b08c6f3fff87ac8b3ac72fb1878299c3ed60c
Matt Budz <matt.budz@protonmail.com>
2024-11-08 17:37:44 +0100

    update DT.js so we can add `rowReorder` extension

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import 'datatables'
-import 'stupidtable'
+import 'datatables/jquery.dataTables.min';
+import 'datatables/dataTables.bootstrap4.min';
+import 'datatables/dataTables.buttons.min';
+import 'datatables/buttons.colVis.min';
+import 'datatables/buttons.bootstrap4.min';
+import 'datatables/dataTables.fixedColumns.min';
+import 'datatables/fixedColumns.bootstrap4.min';
+import 'datatables/dataTables.fixedHeader.min';
+import 'datatables/fixedHeader.bootstrap4.min';
+import 'datatables/dataTables.select.min';
+import 'datatables/select.bootstrap4.min';
+import 'stupidtable';
