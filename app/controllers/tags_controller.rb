7e8021bb12457ea6af301dcf21634fcfe74d5e06
Matt Budz <matt.budz@protonmail.com>
2024-11-15 12:28:19 +0100

    render tags#index with correct tag order

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,7 +7,7 @@ class TagsController < AuthenticatedController
   load_and_authorize_resource
 
   def index
-    @tags = current_project.tags
+    @tags = current_project.tags.order(:position)
   end
 
   def new; end
