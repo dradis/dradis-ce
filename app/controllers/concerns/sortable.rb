6485af166524777b0f5ed4405fafa63d2e7963cc
Caitlin <caitlinhenry.ss@gmail.com>
2024-12-05 10:49:06 -0500

    Update existing tags with position value in migration

--- app/controllers/concerns/sortable.rb
+++ app/controllers/concerns/sortable.rb
@@ -5,11 +5,11 @@ module Sortable
     klass = sortable_records[:klass]
 
     ActiveRecord::Base.transaction do
-      sort_params[:sorted_ids].each_with_index do |id, index|
+      sort_params[:sorted_ids].each_with_index(1) do |id, index|
         id = id.to_i
 
         if sortable_records[:ids].include?(id)
-          klass.update(id, position: index + 1)
+          klass.update(id, position: index)
         end
       end
     end
--- db/migrate/20241114154140_add_position_to_tags.rb
+++ db/migrate/20241114154140_add_position_to_tags.rb
@@ -1,5 +1,11 @@
 class AddPositionToTags < ActiveRecord::Migration[7.0]
   def change
     add_column :tags, :position, :integer
+
+    Tag.transaction do
+      Tag.unscoped.each_with_index(1) do |tag, index|
+        tag.update_attribute(:position, index)
+      end
+    end
   end
 end
