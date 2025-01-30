f25925ebe8f533122f5c73e2332f403990bee742
Aaron <mail@aapomm.io>
2025-01-30 17:06:03 +0800

    Use params :qa instead

--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -7,14 +7,14 @@ class RevisionsController < AuthenticatedController
   before_action :load_record, except: [ :trash, :recover ]
 
   def index
-    redirect_to action: :show, id: @record.versions.last.try(:id) || 0, return_to: params[:return_to]
+    redirect_to action: :show, id: @record.versions.last.try(:id) || 0, qa: params[:qa] == 'true'
   end
 
   def show
     # Use `reorder`, not `order`, to override Paper Trail's default scope
     @revisions = @record.versions.includes(:item).reorder('created_at DESC')
     @revision  = @revisions.find(params[:id])
-    @return_to_qa = params[:return_to] == 'qa'
+    @qa = params[:qa] == 'true'
 
     if @revision.event == 'update'
       @diffed_revision = DiffedRevision.new(@revision, @record)
