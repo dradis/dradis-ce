aaa8f885906bff539187e5bedf0f15a87aa1e2ad
Caitlin <caitlinhenry.ss@gmail.com>
2025-02-05 13:57:31 -0500

    set user login path in controller helper for parity with pro

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,8 +4,9 @@ class NotificationMailer < ApplicationMailer
   before_action :set_inline_attachments
 
   def digest
-    @user = params[:user]
     @notifications = params[:notifications]
+    @user = params[:user]
+    set_login_path_for_user
     @type = params[:type]
 
     mail to: @user.email, subject: 'You have unread notifications.'
@@ -18,5 +19,9 @@ class NotificationMailer < ApplicationMailer
       File.read(
         Rails.root.join('app', 'assets', 'images', 'logo_full_small.png')
       )
+  end
+
+  def set_login_path_for_user # careful not to override login_path route helper
+    login_url
   end
 end
