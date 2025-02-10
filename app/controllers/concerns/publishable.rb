3db4c90b7685f81049551d88311be6c0ac990dfe
Aaron <mail@aapomm.io>
2025-02-10 19:14:56 +0800

    Fix publishable concern parity

--- app/controllers/concerns/publishable.rb
+++ app/controllers/concerns/publishable.rb
@@ -8,17 +8,28 @@ module Publishable
   private
 
   def ensure_reviewer
-    params_method = "#{controller_name.singularize}_params"
-    return if can?(:publish, current_project) || !self.respond_to?(params_method)
+    return if can?(:publish, current_project)
 
-    record_params = self.send(params_method)
-    current_state =
-      self.get_instance_variable("@#{controller_name.singularize}")&.state
+    record_params = self.send("#{controller_name.singularize}_params")
+    current_state = record&.state
 
-    if (record_params[:state] == 'published' && current_state != 'published') ||
+    if (current_state != 'published' && record_params[:state] == 'published') ||
         params[:state] == 'published'
 
       redirect_to project_issues_path(current_project), alert: 'Unable to publish record!'
+    elsif current_state == 'published' && record_params[:state] == ''
+      # If the user is not a reviewer, and the record is currently set as
+      # 'Published', the radio button will be disabled and record_params[:state]
+      # will be '' (empty string). Since an empty state is invalid, we delete
+      # the param here instead.
+      params[controller_name.singularize].delete(:state)
     end
+  end
+
+  def record
+    return unless params[:id]
+
+    klass = controller_name.classify.constantize
+    klass.find(params[:id])
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,9 +6,8 @@ class Ability
 
     can [:read, :use, :update], Project, authors: { id: [user.id] }
     can [:publish], Project do |project|
-      project.reviewers.include?(user) ||
-        # Allow all to publish if there are no reviewers
-        project.reviewers.count == 0
+      # Allow all to publish if there are no reviewers
+      project.reviewers.include?(user) || project.reviewers.count == 0
     end
     can [:create, :read], Comment
     can [:update, :destroy], Comment, user_id: user.id
