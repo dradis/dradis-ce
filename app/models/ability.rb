2a84b71b38469e6eb398da4a1647ef0733d2e877
Aaron <mail@aapomm.io>
2025-02-20 20:03:55 +0800

    Remove publishing with 0 reviewers

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,10 +5,7 @@ class Ability
     user ||= User.new # guest user (not logged in)
 
     can [:read, :use, :update], Project, authors: { id: [user.id] }
-    can [:publish], Project do |project|
-      # Allow all to publish if there are no reviewers
-      project.reviewers.include?(user) || project.reviewers.count == 0
-    end
+    can [:publish], Project, reviewers: { id: [user.id] }
     can [:create, :read], Comment
     can [:update, :destroy], Comment, user_id: user.id
     can :manage, Tag
