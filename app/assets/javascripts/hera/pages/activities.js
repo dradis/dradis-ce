59b3268a5eed26cd76df7a43a8800025359c802b
New Aguda <codesinpencil@gmail.com>
2025-03-28 22:28:24 +0800

    [Activities] Using filters for infinite scroll feature

--- app/assets/javascripts/hera/pages/activities.js
+++ app/assets/javascripts/hera/pages/activities.js
@@ -20,12 +20,18 @@ document.addEventListener('turbo:load', function () {
       ) {
         var page = $infiniteScrollContainer.data('page') + 1;
         var url = $infiniteScrollContainer.data('url');
+        var user_id = $infiniteScrollContainer.data('userId');
+        var trackable_type = $infiniteScrollContainer.data('trackableType');
+        var start_date = $infiniteScrollContainer.data('startDate');
+        var end_date = $infiniteScrollContainer.data('endDate');
+        var specific_date = $infiniteScrollContainer.data('specificDate');
+
         loading = true;
         $('[data-behavior="activities-spinner"]').show();
 
         $.ajax({
           url: url,
-          data: { page: page },
+          data: { page: page, user_id: user_id, trackable_type: trackable_type, start_date: start_date, end_date: end_date, specific_date: specific_date },
           success: function (data) {
             loading = false;
             $('[data-behavior="activities-spinner"]').hide();
