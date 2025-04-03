1794408e3cca5658f07e20bfc4de171d37b06824
Caitlin <caitlinhenry.ss@gmail.com>
2025-04-03 13:55:15 -0400

    disable reloading of code in development environment so that initializer settings are preserved

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,10 +3,11 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded any time
+  # In the development environment your application's code can be reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  # Set enable_reloading to true to turn on this setting
+  config.enable_reloading = false
 
   # Do not eager load code on boot.
   config.eager_load = false
