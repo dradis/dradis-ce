244377b7bf55ab7f09e1b2d256ab2efd9462950d
Aaron <mail@aapomm.io>
2025-05-27 15:27:34 +0800

    Add ValidationContext to serializers

--- config/initializers/yaml_serializers.rb
+++ config/initializers/yaml_serializers.rb
@@ -1,6 +1,7 @@
 Rails.application.reloader.to_prepare do
   ActiveRecord.yaml_column_permitted_classes = [
     ActiveModel::Errors,
+    ActiveModel::ValidationContext,
     ActiveSupport::TimeWithZone,
     ActiveSupport::TimeZone,
     Date,
