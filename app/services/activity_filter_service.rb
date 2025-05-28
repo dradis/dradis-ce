3316b2640b9725f604ccc21358ae91c417b1d59b
zanetagebka <zanetagebka@gmail.com>
2025-05-28 15:25:52 +0200

    feat: ActivityFilterService - add possibility to filter activities per user, date, date range and type of activity - add service spec

---
+++ app/services/activity_filter_service.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+class ActivityFilterService
+  def initialize(activities, params)
+    @activities = activities
+    @params = params
+  end
+
+  def call
+    query = @activities
+    query = filter_by_user(query) if @params[:user_id].present?
+    query = filter_by_trackable_type(query) if @params[:trackable_type].present?
+    query = filter_by_specific_date(query) if @params[:date].present?
+    query = filter_by_date_range(query) if @params[:start_date].present? && @params[:end_date].present?
+    query
+  end
+
+  private
+
+  def filter_by_user(query)
+    return query unless @params[:user_id].present?
+
+    query.where(user_id: @params[:user_id])
+  end
+
+  def filter_by_trackable_type(query)
+    return query unless @params[:trackable_type].present?
+
+    query.where(trackable_type: @params[:trackable_type])
+  end
+
+  def filter_by_specific_date(query)
+    return query unless @params[:date].present?
+
+    date = Date.parse(@params[:date]) rescue nil
+    return query if date.nil?
+
+    query.where(created_at: date.beginning_of_day..date.end_of_day)
+  end
+
+  def filter_by_date_range(query)
+    return query unless @params[:start_date].present? && @params[:end_date].present?
+
+    start_date = Date.parse(@params[:start_date]) rescue nil
+    end_date = Date.parse(@params[:end_date]) rescue nil
+    return query if start_date.nil? || end_date.nil?
+
+    query.where(created_at: start_date.beginning_of_day..end_date.end_of_day)
+  end
+end
---
+++ spec/services/activity_filter_service_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ActivityFilterService do
+  let(:project) { create(:project) }
+  let(:activities) { project.activities.includes(:trackable) }
+  let(:filter_params) { {} }
+
+  subject { described_class.new(activities, filter_params).call }
+
+  describe '#call' do
+    context 'when no filters are applied' do
+      it 'returns all activities' do
+        expect(subject).to eq(activities)
+      end
+    end
+
+    context 'when filtering by user_id' do
+      let(:user) { create(:user) }
+      let!(:activity) { create(:activity, project: project, user: user) }
+      let(:filter_params) { { user_id: user.id } }
+
+      it 'returns activities for the specified user' do
+        expect(subject).to eq([activity])
+      end
+    end
+
+    context 'when filtering by trackable_type' do
+      let!(:activity1) { create(:activity, project: project, trackable_type: 'Issue') }
+      let!(:activity2) { create(:activity, project: project, trackable_type: 'Comment') }
+      let(:filter_params) { { trackable_type: 'Issue' } }
+
+      it 'returns activities of the specified trackable type' do
+        expect(subject).to eq([activity1])
+      end
+    end
+
+    context 'when filtering by specific date' do
+      let!(:date_activity) { create(:activity, project: project, created_at: Date.yesterday) }
+      let(:filter_params) { { date: Date.yesterday.to_s } }
+
+      it 'returns activities created on the specified date' do
+        expect(subject).to eq([date_activity])
+      end
+    end
+
+    context 'when filtering by date range' do
+      let!(:start_date_activity) { create(:activity, project: project, created_at: Date.yesterday) }
+      let!(:end_date_activity) { create(:activity, project: project, created_at: Date.tomorrow) }
+      let(:filter_params) { { start_date: Date.yesterday.to_s, end_date: Date.tomorrow.to_s } }
+
+      it 'returns activities within the specified date range' do
+        expect(subject).to contain_exactly(start_date_activity, end_date_activity)
+      end
+    end
+
+    context 'when invalid date formats are provided' do
+      let!(:activity) { create(:activity, project: project, created_at: Date.yesterday) }
+      let(:filter_params) { { date: 'invalid-date' } }
+
+      it 'ignores the invalid date filter and returns all activities' do
+        expect(subject).to eq(activities)
+      end
+    end
+  end
+end
