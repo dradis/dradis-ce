28f737e8297b911838edaca88f9e8515298bbc2f
Aaron <mail@aapomm.io>
2025-07-25 15:13:14 +0800

    Use array enum and remove unused binary files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,9 +67,9 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   if ENV['RAILS_LOG_TO_STDOUT'].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Prevent health checks from clogging up the logs.
