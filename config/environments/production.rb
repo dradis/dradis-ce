270f32a925745eb2818d13c2e64a24cf6692f3cd
caitmich <caitlinhenry.ss@gmail.com>
2025-08-27 09:51:38 -0400

    remove x_sendfile_header config since it will be handled by Thruster

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
 
-  # Specifies the header that your server uses for sending files.
-  config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache or Thruster
+  # Specifies the header that your server uses for sending files. (This can be set by the server using X-Sendfile-Type header)
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache or Thruster
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
