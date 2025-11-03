e4c92ac7c265a96005e48e8cc9d7e3d9de95af6c
nicolachr <nicola.christou@gmail.com>
2025-11-03 11:22:17 +0200

    Use existing stylesheet

--- app/helpers/styles_helper.rb
+++ app/helpers/styles_helper.rb
@@ -4,6 +4,6 @@ module StylesHelper
   def highlight_code(code, language = 'html')
     lexer = Rouge::Lexer.find(language)
     formatter = Rouge::Formatters::HTML.new
-    "<pre class='highlight'>#{formatter.format(lexer.lex(code))}</pre>"
+    "<pre class='style-code'>#{formatter.format(lexer.lex(code))}</pre>"
   end
 end
