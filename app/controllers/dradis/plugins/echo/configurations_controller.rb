910e717ff706cf2d9a05e52720f6258d2741acff
Aaron <mail@aapomm.io>
2025-11-24 14:34:41 +0800

    Add configurations#update action

--- app/controllers/dradis/plugins/echo/configurations_controller.rb
+++ app/controllers/dradis/plugins/echo/configurations_controller.rb
@@ -5,6 +5,20 @@ module Dradis::Plugins::Echo
     def index
     end
 
+    def update
+      if Engine.settings.update_settings(configuration_params)
+        redirect_to configurations_path, notice: 'Updated configuration successfully'
+      else
+        render :index
+      end
+    end
+
+    private
+
+    def configuration_params
+      params.permit(:address, :model)
+    end
+
     def set_configurations
       @configurations = Engine.settings.all
     end
