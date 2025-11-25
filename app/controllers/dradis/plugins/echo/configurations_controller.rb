92e4fa2478bdc1fc782b93565b3a2545db6eed59
Aaron <mail@aapomm.io>
2025-11-25 20:50:56 +0800

    Add configuration form

--- app/controllers/dradis/plugins/echo/configurations_controller.rb
+++ app/controllers/dradis/plugins/echo/configurations_controller.rb
@@ -1,37 +1,26 @@
 module Dradis::Plugins::Echo
   class ConfigurationsController < AuthenticatedController
-    before_action :set_configurations
-
     def index
-      @is_default_model = @configurations.find { |c| c[:name] == :model }[:default]
+      @configuration_form = ConfigurationForm.from_storage
+
+      @is_default_model = Engine.settings.all.find { |c| c[:name] == :model }[:default]
     end
 
     def update
-      if validate_address(configuration_params[:address]) &&
-          Engine.settings.update_settings(configuration_params)
+      @configuration_form = ConfigurationForm.new(configuration_params)
 
+      if @configuration_form.save
         redirect_to configurations_path, notice: 'Updated configuration successfully'
       else
         flash[:alert] = 'Invalid settings'
-        render :index
+        render action: :index
       end
     end
 
     private
 
     def configuration_params
-      params.permit(:address, :model)
-    end
-
-    def set_configurations
-      @configurations = Engine.settings.all
-    end
-
-    def validate_address(string)
-      uri = URI.parse(string)
-      uri.host.present?
-    rescue URI::InvalidURIError
-      false
+      params.require(:configuration_form).permit(:address, :model)
     end
   end
 end
