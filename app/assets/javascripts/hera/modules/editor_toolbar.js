7e04e03bba55f2f1bdd281226a5f1f067e710dbc
nicolachr <nicola.christou@gmail.com>
2025-11-25 09:49:10 +0200

    Add inline-code

--- app/assets/javascripts/hera/modules/editor_toolbar.js
+++ app/assets/javascripts/hera/modules/editor_toolbar.js
@@ -289,7 +289,7 @@ class EditorToolbar {
       //'highlight':          new Affix('$${{', 'Highlighted text', '}}$$'),
       image: new Affix('\n!', 'https://', '!\n'),
       'image-placeholder': new Affix('\n!', 'https://', ' uploading...!\n'),
-      //'inline-code': new Affix('@', 'Inline code', '@'),
+      'inline-code': new Affix('@', 'Inline code', '@'),
       italic: new Affix('_', 'Italic text', '_'),
       link: new Affix('"', 'Link text', '":https://'),
       'list-ol': new Affix('# ', 'Ordered item'),
@@ -310,68 +310,74 @@ class EditorToolbar {
     if (include.includes('field'))
       str +=
         '<div class="editor-btn" data-btn="field" aria-tooltip="add new field">\
-      <i class="fa-solid fa-plus"></i>\
-    </div>\
-    <div class="divider-vertical"></div>';
+          <i class="fa-solid fa-plus"></i>\
+        </div>\
+        <div class="divider-vertical"></div>';
 
     if (include.includes('bold'))
       str +=
         '<div class="editor-btn" data-btn="bold" aria-tooltip="bold text">\
-      <i class="fa-solid fa-bold"></i>\
-    </div>';
+          <i class="fa-solid fa-bold"></i>\
+        </div>';
 
     if (include.includes('italic'))
       str +=
         '<div class="editor-btn px-2" data-btn="italic" aria-tooltip="italic text">\
-      <i class="fa-solid fa-italic"></i>\
-    </div>';
+          <i class="fa-solid fa-italic"></i>\
+        </div>';
 
     str += '<div class="divider-vertical"></div>';
+
+    if (include.includes('inline-code'))
+      str +=
+        '<div class="editor-btn" data-btn="inline-code" aria-tooltip="inline code">\
+          <i class="fa-solid fa-code"></i>\
+        </div>';
 
     if (include.includes('block-code'))
       str +=
         '<div class="editor-btn" data-btn="block-code" aria-tooltip="code block">\
-      <i class="fa-solid fa-code"></i>\
-    </div>';
+          <i class="fa-solid fa-code"></i>\
+        </div>';
 
     if (include.includes('link'))
       str +=
         '<div class="editor-btn" data-btn="link" aria-tooltip="link">\
-      <i class="fa-solid fa-link"></i>\
-    </div>';
+          <i class="fa-solid fa-link"></i>\
+        </div>';
 
     if (include.includes('quote'))
       str +=
         '<div class="editor-btn" data-btn="quote" aria-tooltip="quote block">\
-      <i class="fa-solid fa-quote-left"></i>\
-    </div>';
+          <i class="fa-solid fa-quote-left"></i>\
+        </div>';
 
     if (include.includes('table'))
       str +=
         '<div class="editor-btn" data-btn="table" aria-tooltip="table">\
-      <i class="fa-solid fa-table"></i>\
-    </div>';
+          <i class="fa-solid fa-table"></i>\
+        </div>';
 
     str += '<div class="divider-vertical"></div>';
 
     if (include.includes('list-ul'))
       str +=
         '<div class="editor-btn" data-btn="list-ul" aria-tooltip="unordered list">\
-      <i class="fa-solid fa-list-ul"></i>\
-    </div>';
+          <i class="fa-solid fa-list-ul"></i>\
+        </div>';
     if (include.includes('list-ol'))
       str +=
         '<div class="editor-btn" data-btn="list-ol" aria-tooltip="ordered list">\
-      <i class="fa-solid fa-list-ol"></i>\
-    </div>';
+          <i class="fa-solid fa-list-ol"></i>\
+        </div>';
 
     str += '<div class="divider-vertical"></div>';
 
     if (include.includes('image'))
       str +=
         '<div class="editor-btn image-btn" data-btn="image" aria-tooltip="image">\
-      <i class="fa-regular fa-image"></i>\
-    </div>';
+          <i class="fa-regular fa-image"></i>\
+        </div>';
 
     /* Additional buttons for future use
 
