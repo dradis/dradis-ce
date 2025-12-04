c431820ea617d639292e8bd89c22aa1390f1b7a4
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2025-12-04 09:12:56 +0800

    Setup::Kits - lint

--- spec/features/setup/kits_spec.rb
+++ spec/features/setup/kits_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'Setup::Kits', skip_setup_mock: true do
 
-  context "when shared password is already set" do
-    it "enqueues a KitImport job if a valid kit is passed" do
+  context 'when shared password is already set' do
+    it 'enqueues a KitImport job if a valid kit is passed' do
 
       ActiveJob::Base.queue_adapter = :test
       ActiveJob::Base.queue_adapter.perform_enqueued_jobs = false
@@ -17,7 +17,7 @@ describe 'Setup::Kits', skip_setup_mock: true do
       end.to have_enqueued_job(KitImportJob)
     end
 
-    it "doesn't enque a KitImport if :none is passed" do
+    it 'doesn\'t enque a KitImport if :none is passed' do
       visit new_setup_kit_path
       expect do
         # We'd need JS to be able to click in the link and send a POST
@@ -25,7 +25,7 @@ describe 'Setup::Kits', skip_setup_mock: true do
       end.to_not have_enqueued_job(KitImportJob)
     end
 
-    it "doesn't enque a KitImport if invalid kit is passed" do
+    it 'doesn\'t enque a KitImport if invalid kit is passed' do
       visit new_setup_kit_path
       expect do
         # We'd need JS to be able to click in the link and send a POST
