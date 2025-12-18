99cbe48ff6a42e2e523a861785d52f9cd6d168ad
Gerald <geraldcelanji@gmail.com>
2025-12-18 12:39:09 +0000

    Resolve db seeds conflict

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
-# Create a few default tags.
-Tag::DEFAULT_TAGS.each do |name|
-  Tag.create!(name: name)
+unless defined?(Dradis::Pro)
+  # Create a few default tags.
+  Tag::DEFAULT_TAGS.each do |name|
+    Tag.find_or_create_by(name: name)
+  end
+end
+
+# Seed Pro-specific engines if available
+if defined?(Dradis::Pro)
+  # Load engine seeds
+  Dradis::Pro::BI::Engine.load_seed
+  Dradis::Pro::Rules::Engine.load_seed
 end
