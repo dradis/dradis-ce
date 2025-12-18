7e439156c77fd01be1c78d3a9e304048e86e64cf
caitmich <caitlinhenry.ss@gmail.com>
2025-12-18 18:34:09 +0000

    no need to ensure top level activityservice

--- config/initializers/activity_service.rb
+++ config/initializers/activity_service.rb
@@ -1,5 +1,5 @@
 Rails.application.reloader.to_prepare do
-  ::ActivityService.configure do |activity_service|
+  ActivityService.configure do |activity_service|
     activity_service.subscribe_namespace 'issue'
   end
 end
