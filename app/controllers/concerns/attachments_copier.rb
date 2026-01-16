8c45153f8477805e7795f45a83239533fc9bbe31
caitmich <caitlinhenry.ss@gmail.com>
2026-01-16 13:58:43 -0500

    use source_node_id as param name instead of previous_node_id

--- app/controllers/concerns/attachments_copier.rb
+++ app/controllers/concerns/attachments_copier.rb
@@ -3,13 +3,13 @@ module AttachmentsCopier
   # 1. Copies the attachment to the new node
   # 2. Updates the content to point to the new attachment
   # Returns an array of copied attachments for that record
-  def copy_attachments(record, previous_node_id = nil)
+  def copy_attachments(record, source_node_id = nil)
     copied_attachments = []
     record.content.scan(Attachment::SCREENSHOT_REGEX).each do |screenshot_path|
       full_screenshot_path, _, _, _, project_id, node_id, filename, _ = screenshot_path
-      previous_node_id ||= record.node_id_was
+      source_node_id ||= record.node_id_was
 
-      attachment = Attachment.find_by(filename: CGI::unescape(filename), node_id: previous_node_id)
+      attachment = Attachment.find_by(filename: CGI::unescape(filename), node_id: source_node_id)
 
       if attachment
         new_attachment = attachment.copy_to(record.node)
