02fe3432be76ca68de77bb5188304a7da6d1127e
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2026-01-19 15:23:01 +0800

    Sandbox - add Engine initializers

--- engines/dradis-sandbox/lib/dradis/sandbox/engine.rb
+++ engines/dradis-sandbox/lib/dradis/sandbox/engine.rb
@@ -6,5 +6,26 @@ module Dradis::Sandbox
     include ::Dradis::Plugins::Base
     provides :addon
     description 'Dradis CE Sandbox'
+
+    initializer 'dradis-sandbox.mount_engine' do |app|
+      app.routes.append do
+        mount Dradis::Sandbox::Engine => '/', as: 'sandbox'
+      end
+    end
+
+    initializer 'dradis-sandbox.sentry' do
+      if !Rails.env.local? && ENV['SKIP_TELEMETRY'].blank?
+        Sentry.init do |config|
+          config.dsn = ENV['SENTRY_DSN']
+          config.breadcrumbs_logger = %i[ active_support_logger http_logger ]
+          config.send_default_pii = false
+          # config.release = ENV["KAMAL_VERSION"]
+
+          # Receive Rails.error.report and retry_on/discard_on report: true
+          config.rails.register_error_subscriber = true
+        end
+      end
+    end
+
   end
 end
