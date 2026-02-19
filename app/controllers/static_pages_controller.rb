9fec10e26cdc657475ac1a8bdcb9c0ff005580d5
MattBudz <matt.budz@protonmail.com>
2026-02-19 18:33:54 +0100

    move tickets assingment out of the view + add more states and categories

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,5 +5,40 @@ class StaticPagesController < AuthenticatedController
 
   def issuelib_import; end
 
-  def remediationtracker_index; end
+  def remediationtracker_index
+    @tickets = [
+      {
+        title:    'SQL Injection in Login Form',
+        category: 'Security',
+        state:    'Open',
+        assignee: 'James T. Kirk',
+        due_at:   Date.today - 1.week,
+        overdue:  true
+      },
+      {
+        title:    'Outdated SSL Certificate',
+        category: 'Infrastructure',
+        state:    'In Progress',
+        assignee: 'Nyota Uhura',
+        due_at:   Date.today,
+        overdue:  true
+      },
+      {
+        title:    'Cross-Site Scripting in Search Bar',
+        category: 'Application',
+        state:    'On Hold',
+        assignee: 'Montgomery Scott',
+        due_at:   Date.today + 1.week,
+        overdue:  false
+      },
+      {
+        title:    'Missing HTTP Security Headers',
+        category: 'Compliance',
+        state:    'Closed',
+        assignee: nil,
+        due_at:   Date.today + 1.month,
+        overdue:  false
+      }
+    ]
+  end
 end
