d906e42f4c1e46454169dbc9d709a1785b8a7095
caitmich <caitlinhenry.ss@gmail.com>
2026-02-20 14:43:18 -0500

    add default from address

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -48,6 +48,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
   # Set host to be used by links generated in mailer templates.
+  config.action_mailer.default_options = { from: 'from@dradisframework.dev' }
   config.action_mailer.default_url_options = { host: 'dradisframework.dev' }
 
   # Print deprecation notices to the stderr.
