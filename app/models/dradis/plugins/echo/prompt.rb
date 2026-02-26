e225a3e693518c9368b96e60ef03e60e8ed4ab9b
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2026-02-26 16:00:33 +0800

    Prompt - add validation and scoping

--- app/models/dradis/plugins/echo/prompt.rb
+++ app/models/dradis/plugins/echo/prompt.rb
@@ -4,14 +4,37 @@ module Dradis::Plugins::Echo
 
     SCOPES = [ :issue ].freeze
 
-    enum :visibility, [ :personal, :shared ]
+    enum :visibility, [ :user, :team ]
 
     # -- Relationships ----------------------------------------------------------
     belongs_to :user
 
     # -- Callbacks --------------------------------------------------------------
+    before_validation :set_defaults, on: :create
+
     # -- Validations ------------------------------------------------------------
+    normalizes :scope, with: ->(type) { type.to_s }
+
+    validates :title,
+      length: { maximum: DB_MAX_STRING_LENGTH },
+      presence: true,
+      uniqueness: { scope: :user_id }
+
+    validates :prompt, presence: true
+    validates :scope, inclusion: SCOPES, presence: true
+    validates :user, presence: true, associated: true
+    validates :visibility, presence: true
+
     # -- Scopes -----------------------------------------------------------------
+    scope :for, ->(type) { where(scope: type) }
+
     # -- Class Methods ----------------------------------------------------------
+
+    # -- Instance Methods -------------------------------------------------------
+    private
+    def set_defaults
+      self.icon ||= 'fa-star-of-life'
+    end
+
   end
 end
