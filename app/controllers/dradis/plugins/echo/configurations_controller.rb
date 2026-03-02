67f904a5349e8f1c90e23e8a7ac86b7b8a4d5c19
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2026-03-02 09:13:06 +0800

    ApplicationController - stub :current_project to avoid ProjectScope overwriting the sub_nav layout

--- app/controllers/dradis/plugins/echo/configurations_controller.rb
+++ app/controllers/dradis/plugins/echo/configurations_controller.rb
@@ -1,8 +1,5 @@
 module Dradis::Plugins::Echo
   class ConfigurationsController < ApplicationController
-    # FIXME: this is caused by CE's navbar_brand requiring :current_project
-    include ProjectScoped if !defined?(Dradis::Pro)
-
     def index
       @configuration_form = ConfigurationForm.from_storage
     end
--- app/controllers/dradis/plugins/echo/prompts_controller.rb
+++ app/controllers/dradis/plugins/echo/prompts_controller.rb
@@ -43,18 +43,6 @@ module Dradis::Plugins::Echo
 
 
     private
-
-    # The current layout in CE requires a project context. In that case, we're
-    # defining the current_project here to fit the requirements of the layout.
-    # This can be removed once the authenticated, outside-of-a-project layout is
-    # available.
-    unless defined?(Dradis::Pro)
-      def current_project
-        Project.new
-      end
-      helper_method :current_project
-    end
-
     def prompt_params
       params.require(:prompt).permit(:title, :icon, :prompt, :scope)
     end
