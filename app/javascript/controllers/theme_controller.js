52b476931629c966696b425e91eacf3988da88c1
nicolachr <nicola.christou@gmail.com>
2026-03-09 12:43:09 +0200

    Fix theme persistence by passing preferences path to Stimulus controller

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["autoIcon", "lightIcon", "darkIcon", "autoItem", "lightItem", "darkItem"]
+  static values = { preferencesPath: String }
 
   connect() {
     this.preference = this.element.dataset.theme || "auto"
