f5eb8c790a11e01b7ab40d241af86d05167caa02
MattBudz <matt.budz@protonmail.com>
2026-04-27 18:42:31 +0200

    Revert "apply Prettier formatting to liquid_async.js"
    This reverts commit d43b1d3e38ff1d95ae0e0f59987c74a8548e5c8c.

--- app/assets/javascripts/hera/modules/liquid_async.js
+++ app/assets/javascripts/hera/modules/liquid_async.js
@@ -1,22 +1,22 @@
 function updateLiquidSortValue(element, html) {
-  const liquidText = $("<div>").html(html).text().trim();
-  $(element).attr("data-sort", liquidText);
+  const liquidText = $('<div>').html(html).text().trim();
+  $(element).attr('data-sort', liquidText);
 }
 
 function liquidAsync() {
-  $("[data-behavior~=liquid-async]").each(function () {
+  $('[data-behavior~=liquid-async]').each(function () {
     const that = this,
-      data = { text: $(that).attr("data-content") },
-      $spinner = $(that).prev().find("[data-behavior~=liquid-spinner");
+      data = { text: $(that).attr('data-content') },
+      $spinner = $(that).prev().find('[data-behavior~=liquid-spinner');
 
-    const requiresSortUpdate = $(that).is("[data-sort]");
+    const requiresSortUpdate = $(that).is('[data-sort]');
 
-    fetch($(that).attr("data-path"), {
-      method: "POST",
+    fetch($(that).attr('data-path'), {
+      method: 'POST',
       headers: {
-        Accept: "text/html",
-        "Content-Type": "application/json",
-        "X-CSRF-Token": $('meta[name="csrf-token"]').attr("content"),
+        Accept: 'text/html',
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content'),
       },
       body: JSON.stringify(data),
     })
@@ -28,13 +28,13 @@ function liquidAsync() {
           updateLiquidSortValue(that, html);
         }
 
-        $(that).trigger("dradis:liquid-rendered");
-        $spinner.addClass("d-none");
+        $(that).trigger('dradis:liquid-rendered');
+        $spinner.addClass('d-none');
       });
   });
 }
 
-document.addEventListener("turbo:load", liquidAsync);
-$(document).on("dradis:fetch", function () {
+document.addEventListener('turbo:load', liquidAsync);
+$(document).on('dradis:fetch', function () {
   liquidAsync();
 });
