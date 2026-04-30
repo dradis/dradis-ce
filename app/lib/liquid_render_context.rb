06d26d5ba28873ebd3606a8beb52ebc02c57c1ba
MattBudz <matt.budz@protonmail.com>
2026-04-30 18:11:38 +0200

    sort methods alphabetically within their sections

--- app/lib/liquid_render_context.rb
+++ app/lib/liquid_render_context.rb
@@ -1,13 +1,13 @@
 module LiquidRenderContext
+  def self.clear
+    Thread.current[:liquid_render_context] = nil
+  end
+
   def self.current
     Thread.current[:liquid_render_context]&.call
   end
 
   def self.set(assigns_provider)
     Thread.current[:liquid_render_context] = assigns_provider
-  end
-
-  def self.clear
-    Thread.current[:liquid_render_context] = nil
   end
 end
