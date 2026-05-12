746bb5141764a1abb3cc3cc2d95ae6cd3e59720e
nicolachr <nicola.christou@gmail.com>
2026-05-12 15:14:23 +0300

    track card list moves in the activity feed
    Activities were silently dropped because 'move_to_list' was not in
    Activity::VALID_ACTIONS, causing ActivityTrackingJob to fail validation.

--- app/presenters/activity_presenter.rb
+++ app/presenters/activity_presenter.rb
@@ -83,6 +83,8 @@ class ActivityPresenter < BasePresenter
     case activity.action
     when 'destroy'
       'deleted'
+    when 'move_to_list'
+      'moved'
     when 'state_change'
       'updated'
     else
