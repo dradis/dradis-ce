597b8c8c561fb4c44d284d74267d76da5691d010
nicolachr <nicola.christou@gmail.com>
2026-05-12 11:41:41 +0300

    Apply review suggestions

--- config/routes.rb
+++ config/routes.rb
@@ -167,7 +167,7 @@ Rails.application.routes.draw do
   if defined?(Dradis::Pro)
   else
     # Static pages
-    get '/projects', to: 'static_pages#projects_index', as: :projects
+    get 'projects', to: 'static_pages#projects_index', as: :static_projects
     get 'projects/1/addons/issuelib', to: 'static_pages#issuelib_index', as: :static_issuelib
     get 'projects/1/addons/issuelib/import', to: 'static_pages#issuelib_import', as: :static_issuelib_import
     get 'projects/1/addons/remediationtracker', to: 'static_pages#remediationtracker_index', as: :static_remediationtracker
