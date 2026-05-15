a002eaf35dbca9ec0262bc761ff1d90e31e74476
MattBudz <matt.budz@protonmail.com>
2026-05-15 12:13:19 -0400

    allow OpenAI address to be left blank in the form
    The form copy indicates the field is optional, but the presence validation
    rejected blank values. Set the standard endpoint as default so leaving the
    field blank is valid and behaves as described.

--- engines/dradis-echo/app/models/dradis/plugins/echo/provider/open_ai.rb
+++ engines/dradis-echo/app/models/dradis/plugins/echo/provider/open_ai.rb
@@ -1,5 +1,9 @@
 module Dradis::Plugins::Echo
   class Provider::OpenAI < Provider
+    DEFAULT_ADDRESS = 'https://api.openai.com/v1/'.freeze
+
+    before_validation :set_default_address
+
     validates :address, :api_key, presence: true
 
     def generate(prompt:, model: nil, &block)
@@ -7,6 +11,10 @@ module Dradis::Plugins::Echo
     end
 
     private
+
+    def set_default_address
+      self.address = DEFAULT_ADDRESS if address.blank?
+    end
 
     def client
       #@client ||= ::OpenAI::Client.new(access_token: api_key, uri_base: address)
