0e7ef0163f26fb5f90949546ee243751ac319fc3
MattBudz <matt.budz@protonmail.com>
2026-07-14 18:05:05 +0200

    back the editing_session factory default with a real issue
    FactoryBot.lint build a session with no overrides, and the default
    record_id was an arbitrary sequence with nothing behind it. That was
    harmless while the record association was optional, but now that it's
    required (see 34a95bb3), the default build fails presence validation.

--- spec/factories/editing_sessions.rb
+++ spec/factories/editing_sessions.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :editing_session do
     association :user
     record_type { 'Issue' }
-    sequence(:record_id) { |n| n }
+    record_id { create(:issue).id }
   end
 end
