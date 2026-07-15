52eac9f83440ebeec183b78b30deba7efe05fae2
MattBudz <matt.budz@protonmail.com>
2026-07-15 13:27:32 +0200

    extract tag grouping into a shared IssuesGrouping concern
    Pro needs this same grouping logic from more than one controller, so it
    already lives in a concern there. Duplicating it inline here meant
    ProjectsController#show diverged structurally between the two repos.
    Extracting it in CE keeps both controllers aligned and lets Pro's version
    of the concern sync in as an addition rather than a rewrite.

---
+++ app/controllers/concerns/issues_grouping.rb
@@ -0,0 +1,30 @@
+module IssuesGrouping
+  private
+
+  def build_all_tags_grouping
+    @count_by_tag = { unassigned: 0 }
+    @issues_by_tag = Hash.new { |h, k| h[k] = [] }
+    @tag_names = @tags.map do |tag|
+      @count_by_tag[tag.name] = 0
+      [tag.name, [tag.display_name, tag.color]]
+    end.to_h
+
+    @issues.each do |issue|
+      if issue.tags.empty?
+        @issues_by_tag[:unassigned] << issue
+        @count_by_tag[:unassigned] += 1
+      else
+        issue.tags.each do |tag|
+          @issues_by_tag[tag.name] << issue
+          @count_by_tag[tag.name] += 1
+        end
+      end
+    end
+
+    @chart_data = {
+      dimension: 'tags',
+      tags: @tag_names.to_json,
+      issues_count: @count_by_tag.to_json
+    }
+  end
+end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < AuthenticatedController
+  include IssuesGrouping
   include NotificationsReader
 
   before_action :set_project
@@ -19,31 +20,7 @@ class ProjectsController < AuthenticatedController
     @nodes = current_project.nodes.in_tree
     @tags = current_project.tags
 
-    @count_by_tag = { unassigned: 0 }
-    @issues_by_tag = Hash.new { |h, k| h[k] = [] }
-
-    @tag_names = @tags.map do |tag|
-      @count_by_tag[tag.name] = 0
-      [tag.name, [tag.display_name, tag.color]]
-    end.to_h
-
-    @issues.each do |issue|
-      if issue.tags.empty?
-        @issues_by_tag[:unassigned] << issue
-        @count_by_tag[:unassigned] += 1
-      else
-        issue.tags.each do |tag|
-          @issues_by_tag[tag.name] << issue
-          @count_by_tag[tag.name] += 1
-        end
-      end
-    end
-
-    @chart_data = {
-      dimension: 'tags',
-      tags: @tag_names.to_json,
-      issues_count: @count_by_tag.to_json
-    }
+    build_all_tags_grouping
 
     respond_to do |format|
       format.html { render layout: 'hera/project' if !request.xhr? }
