5f13e76996d96799af7f7f28f222a23fcbfa1698
MattBudz <matt.budz@protonmail.com>
2026-07-17 14:39:06 +0200

    break out of the issues-summary frame when opening an issue
    The accordion now renders inside the async-loaded issues-summary
    frame, so clicking an issue link navigated within that small frame
    instead of the full page. Added a spec covering the click-through,
    since the existing chart-rendering spec didn't exercise this path.

--- spec/features/projects/issues_summary_spec.rb
+++ spec/features/projects/issues_summary_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 describe 'Issues Summary', js: true do
   subject { page }
 
+  let(:issue) { create(:issue, node: current_project.issue_library) }
+
   before do
     login_to_project_as_user
 
     tag = create(:tag)
-    issue = create(:issue, node: current_project.issue_library)
     issue.tags << tag
   end
 
@@ -16,6 +17,14 @@ describe 'Issues Summary', js: true do
       visit project_path(current_project)
 
       expect(page).to have_selector('#issue-chart svg')
+    end
+
+    it 'navigates to the full issue page when an accordion link is clicked' do
+      visit project_path(current_project)
+
+      click_link issue.title
+
+      expect(page).to have_current_path(project_issue_path(current_project, issue))
     end
   end
 end
