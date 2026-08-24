484462f5e04a039208d4ac58d3788e12a932c4af
MattBudz <matt.budz@protonmail.com>
2026-08-24 16:37:54 +0200

    Revert "stop eagerly computing issue grouping in ProjectsController#show"
    This reverts commit 7f05c3424699d8fcb16ff6fb31050b3ce16fb62c.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < AuthenticatedController
+  include IssuesDimensionGrouping
   include NotificationsReader
 
   before_action :set_project
@@ -14,8 +15,12 @@ class ProjectsController < AuthenticatedController
     @activities = Activity.latest
     @authors = [current_user]
     @boards = current_project.methodology_library.boards
+    @issues = current_project.issues.includes(:tags).sort
     @methodologies = current_project.methodology_library.notes.map { |n| Methodology.new(filename: n.id, content: n.text) }
     @nodes = current_project.nodes.in_tree
+    @tags = current_project.tags
+
+    build_all_tags_grouping
 
     respond_to do |format|
       format.html { render layout: 'hera/project' if !request.xhr? }
