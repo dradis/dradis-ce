5159196c17324b4ada6f71b57700c6f3b5f20f29
MattBudz <matt.budz@protonmail.com>
2026-08-24 18:34:02 +0200

    Revert "Revert "add request spec for Projects::IssuesSummaryController""
    This reverts commit cf994d17cd2e430228bf32163792505e8ccd8796.

---
+++ spec/requests/projects/issues_summary_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'Projects::IssuesSummary' do
+  before { login_to_project_as_user }
+
+  describe 'GET #show' do
+    it 'groups issues by tag' do
+      tag = create(:tag)
+      issue = create(:issue, node: current_project.issue_library)
+      issue.tags << tag
+
+      get project_issues_summary_path(current_project)
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include(tag.display_name)
+    end
+
+    it 'renders the empty state when there are no issues' do
+      get project_issues_summary_path(current_project)
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include('Use issues to represent vulnerabilities or findings.')
+    end
+  end
+end
