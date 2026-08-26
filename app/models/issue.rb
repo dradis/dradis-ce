e727ad390b6c16df4d3d1a930d318431ed994282
caitmich <caitlinhenry.ss@gmail.com>
2026-08-26 18:43:51 +0000

    scope Issue.evidence_by_node by state so exports can specify whether they need published or all evidence for a given issue

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -107,8 +107,9 @@ class Issue < Note
     self.title <=> other.title
   end
 
-  # This method groups all the available evidence associated with this Issue
-  # into a Hash where the keys are the nodes. E.g.:
+  # This method groups the evidence associated with this Issue (optionally scoped by state)
+  # into a Hash where the keys are
+  # the nodes. E.g.:
   # {
   #   <node 1> => [<evidence 1.1>, <evidence 1.2>],
   #   <node 2> => [<evidence 2.1>]
@@ -116,10 +117,13 @@ class Issue < Note
   #
   # This is useful in a number of views to present or hide information about
   # all the instances for a given issue and node/host.
-  def evidence_by_node()
+  def evidence_by_node(scope = :all)
+    evidence_collection = scope.to_sym == :published ? evidence.published : evidence
+    evidence_collection = evidence_collection.includes(:node)
+
     results = Hash.new { |h, k| h[k] = [] }
 
-    self.evidence.includes(:node).each do |evidence|
+    evidence_collection.each do |evidence|
       results[evidence.node] << evidence
     end
 
