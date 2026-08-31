6b55ea88ccbb670f556f9b35c816b93fbbd77df2
MattBudz <matt.budz@protonmail.com>
2026-08-31 12:55:32 +0200

    cover issues summary tag grouping

--- spec/controllers/projects/issues_summary_grouping_spec.rb
+++ spec/controllers/projects/issues_summary_grouping_spec.rb
@@ -2,12 +2,29 @@ require 'rails_helper'
 
 describe Projects::IssuesSummaryGrouping do
   describe '#build_tags_grouping' do
-    it 'provides the project issues summary tag grouping' do
+    it 'groups tagged and unassigned issues and builds chart data' do
       host = Class.new do
         include Projects::IssuesSummaryGrouping
       end.new
+      tag = Struct.new(:name, :display_name, :color).new('critical', 'Critical', '#dc3545')
+      tagged_issue = Struct.new(:tags).new([tag])
+      unassigned_issue = Struct.new(:tags).new([])
 
-      expect(host.send(:respond_to?, :build_tags_grouping, true)).to be(true)
+      host.instance_variable_set(:@issues, [tagged_issue, unassigned_issue])
+      host.instance_variable_set(:@tags, [tag])
+
+      host.send(:build_tags_grouping)
+
+      expect(host.instance_variable_get(:@issues_by_tag)).to eq({
+        'critical' => [tagged_issue],
+        unassigned: [unassigned_issue]
+      })
+      expect(host.instance_variable_get(:@count_by_tag)).to eq({ 'critical' => 1, unassigned: 1 })
+      expect(host.instance_variable_get(:@chart_data)).to eq({
+        dimension: 'tags',
+        issues_count: '{"critical":1,"unassigned":1}',
+        tags: '{"critical":["Critical","#dc3545"]}'
+      })
     end
   end
 end
