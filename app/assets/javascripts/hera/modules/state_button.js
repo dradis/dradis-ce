fde78ca32bc76e6b34748021a97364860f8f378b
MattBudz <matt.budz@protonmail.com>
2026-09-01 13:42:03 +0200

    Modernize state button initialization

--- app/assets/javascripts/hera/modules/state_button.js
+++ app/assets/javascripts/hera/modules/state_button.js
@@ -1,26 +1,25 @@
 // Called from initBehaviors() since Turbo Frame swaps don't fire turbo:load.
-(function ($, window) {
-  function updateBtn($selectedRadio) {
-    var selectedState = $selectedRadio
-      .parent()
-      .find('[data-behavior~=state-label]');
+const updateBtn = ($selectedRadio) => {
+  const selectedState = $selectedRadio
+    .parent()
+    .find('[data-behavior~=state-label]');
 
-    var $stateBtn = $selectedRadio
-      .closest('[data-behavior~=btn-states]')
-      .find('[data-behavior~=state-button]');
+  const $stateBtn = $selectedRadio
+    .closest('[data-behavior~=btn-states]')
+    .find('[data-behavior~=state-button]');
 
-    $stateBtn.text(selectedState.text());
-    $stateBtn.parent().attr('data-state', $selectedRadio.val());
-  }
+  $stateBtn.text(selectedState.text());
+  $stateBtn.parent().attr('data-state', $selectedRadio.val());
+};
 
-  function initStateButton(parentElement) {
-    $(parentElement)
-      .find('[data-behavior~=state-radio]')
-      .off('change.stateButton')
-      .on('change.stateButton', function () {
-        updateBtn($(this));
-      });
-  }
+window.initStateButton = (parentElement) => {
+  const $stateRadios = $(parentElement).find(
+    '[data-behavior~=state-radio]'
+  );
 
-  window.initStateButton = initStateButton;
-})(jQuery, window);
+  if (!$stateRadios.length) return;
+
+  $stateRadios.off('change.stateButton').on('change.stateButton', (event) => {
+    updateBtn($(event.currentTarget));
+  });
+};
