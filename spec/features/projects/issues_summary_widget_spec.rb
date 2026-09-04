08a54ebda9ff3ae56cfd872befaf554d1db9e755
MattBudz <matt.budz@protonmail.com>
2026-09-04 19:17:36 +0200

    allow empty issues action to leave summary frame

--- spec/features/projects/issues_summary_widget_spec.rb
+++ spec/features/projects/issues_summary_widget_spec.rb
@@ -1,45 +1,57 @@
 require 'rails_helper'
 
 describe 'Issues Summary widget', js: true do
-  before do
-    login_to_project_as_user
+  before { login_to_project_as_user }
 
-    tag = create(:tag)
-    issue = create(:issue, node: current_project.issue_library)
-    issue.tags << tag
+  context 'with issues' do
+    before do
+      tag = create(:tag)
+      issue = create(:issue, node: current_project.issue_library)
+      issue.tags << tag
+    end
+
+    it 'initializes the chart once across repeated lifecycle events' do
+      visit project_path(current_project)
+
+      expect(page).to have_css('[data-behavior="issue-chart"] svg')
+    end
+
+    it 'initializes the chart once when called repeatedly' do
+      visit project_path(current_project)
+
+      expect(page).to have_css('[data-behavior="issue-chart"] svg')
+
+      page.execute_script('initIssuesChart(); initIssuesChart();')
+
+      expect(page).to have_css('[data-behavior="issue-chart"] svg', count: 1)
+    end
+
+    it 'updates the accordion caret after the frame loads' do
+      visit project_path(current_project)
+
+      expect(page).to have_css('[data-behavior="caret-icon"].fa-caret-up')
+
+      page.execute_script(<<~JS)
+        const frame = document.querySelector('turbo-frame#issues-summary');
+        frame.innerHTML = '';
+        frame.reload();
+      JS
+
+      expect(page).to have_css('[data-behavior="caret-icon"].fa-caret-up')
+
+      first('[data-behavior="card-header"]').click
+
+      expect(page).to have_css('[data-behavior="caret-icon"].fa-caret-down')
+    end
   end
 
-  it 'initializes the chart once across repeated lifecycle events' do
-    visit project_path(current_project)
+  context 'without issues' do
+    it 'navigates to the issues page from the empty state' do
+      visit project_path(current_project)
 
-    expect(page).to have_css('[data-behavior="issue-chart"] svg')
-  end
+      click_on 'Go To Issues'
 
-  it 'initializes the chart once when called repeatedly' do
-    visit project_path(current_project)
-
-    expect(page).to have_css('[data-behavior="issue-chart"] svg')
-
-    page.execute_script('initIssuesChart(); initIssuesChart();')
-
-    expect(page).to have_css('[data-behavior="issue-chart"] svg', count: 1)
-  end
-
-  it 'updates the accordion caret after the frame loads' do
-    visit project_path(current_project)
-
-    expect(page).to have_css('[data-behavior="caret-icon"].fa-caret-up')
-
-    page.execute_script(<<~JS)
-      const frame = document.querySelector('turbo-frame#issues-summary');
-      frame.innerHTML = '';
-      frame.reload();
-    JS
-
-    expect(page).to have_css('[data-behavior="caret-icon"].fa-caret-up')
-
-    first('[data-behavior="card-header"]').click
-
-    expect(page).to have_css('[data-behavior="caret-icon"].fa-caret-down')
+      expect(page).to have_current_path(project_issues_path(current_project))
+    end
   end
 end
