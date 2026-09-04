d4e381e697e4e7488c45d9582bf1c446233d1b9f
MattBudz <matt.budz@protonmail.com>
2026-09-04 18:35:51 +0200

    delegate the issues-summary accordion caret-icon click handler

--- spec/features/projects/issues_summary_widget_spec.rb
+++ spec/features/projects/issues_summary_widget_spec.rb
@@ -24,4 +24,22 @@ describe 'Issues Summary widget', js: true do
 
     expect(page).to have_css('[data-behavior="issue-chart"] svg', count: 1)
   end
+
+  it 'updates the accordion caret after the frame loads' do
+    visit project_path(current_project)
+
+    expect(page).to have_css('[data-behavior="caret-icon"].fa-caret-up')
+
+    page.execute_script(<<~JS)
+      const frame = document.querySelector('turbo-frame#issues-summary');
+      frame.innerHTML = '';
+      frame.reload();
+    JS
+
+    expect(page).to have_css('[data-behavior="caret-icon"].fa-caret-up')
+
+    first('[data-behavior="card-header"]').click
+
+    expect(page).to have_css('[data-behavior="caret-icon"].fa-caret-down')
+  end
 end
