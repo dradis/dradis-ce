94d25a3e17d7bf053c9b47c39397848bd7e8cebb
MattBudz <matt.budz@protonmail.com>
2026-09-10 17:23:39 +0200

    extract editing-session release logic into a reusable controller concern

--- app/controllers/issues/editing_sessions_controller.rb
+++ app/controllers/issues/editing_sessions_controller.rb
@@ -1,14 +1,14 @@
 class Issues::EditingSessionsController < AuthenticatedController
+  include EditingSessionsActions
   include ProjectScoped
 
   before_action :set_issue
 
-  def destroy
-    @issue.release_edit_session(current_user)
-    head :no_content
-  end
-
   private
+
+  def editing_session_record
+    @issue
+  end
 
   def set_issue
     @issue = current_project.issues.find(params[:issue_id])
